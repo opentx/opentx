997d623e1f46759c8c1e30142c4a7e095973e176
bsongis <bsongis@8db61ce9-5e8f-94c8-fa6b-1d1c8bcca7d0>
2011-10-10 08:42:30 +0000

    Trims2Offsets feature is back

--- myeeprom.h
+++ myeeprom.h
@@ -158,8 +158,9 @@ typedef struct t_SafetySwData { // Safety Switches data
 #define FUNC_TRAINER_THR    4
 #define FUNC_TRAINER_AIL    5
 #define FUNC_INSTANT_TRIM   6
-#define FUNC_VIEW_TELEMETRY 7
-#define FUNC_LAST           7
+#define FUNC_TRIMS_2_OFS    7
+#define FUNC_VIEW_TELEMETRY 8
+#define FUNC_LAST           8
 
 typedef struct t_FuncSwData { // Function Switches data
   int8_t  swtch; //input
