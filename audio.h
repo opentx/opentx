5f9ff56490462aa4a45b2a9ece18df50d834d294
rob@marotori.com <rob@marotori.com@8db61ce9-5e8f-94c8-fa6b-1d1c8bcca7d0>
2012-02-01 15:04:20 +0000

    Author name change

--- audio.h
+++ audio.h
@@ -1,5 +1,5 @@
 /*
- * Author - Rob Thomson <rob@marotori.com>
+ * Author - Rob Thomson & Bertrand Songis
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
