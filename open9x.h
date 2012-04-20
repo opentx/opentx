acbc0a1802bf5d12b3883a75d7077200716d79e3
bsongis <bsongis@8db61ce9-5e8f-94c8-fa6b-1d1c8bcca7d0>
2012-04-20 18:22:34 +0000

    Compilation issue on companion9x

--- open9x.h
+++ open9x.h
@@ -514,6 +514,9 @@ extern void setTrimValue(uint8_t phase, uint8_t idx, int16_t trim);
 
 #if defined(PCBV4)
 extern uint8_t s_perOut_flight_phase;
+#endif
+
+#if define(ROTARY_ENCODERS)
 int16_t getRotaryEncoder(uint8_t idx);
 void incRotaryEncoder(uint8_t idx, int8_t inc);
 inline bool navigationRotaryEncoder(uint8_t event)
