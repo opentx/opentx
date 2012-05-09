10f017275ddab9de9ef0d4b720d2045bfd038be5
maxim.karban@gmail.com <maxim.karban@gmail.com@8db61ce9-5e8f-94c8-fa6b-1d1c8bcca7d0>
2012-05-09 12:35:26 +0000

    commented lines removed

--- src/frsky.cpp
+++ src/frsky.cpp
@@ -395,8 +395,6 @@ void processFrskyPacket(uint8_t *packet)
       frskyRSSI[1].set(packet[4] / 2);
       frskyStreaming = FRSKY_TIMEOUT10ms; // reset counter only if valid frsky packets are being detected
       if (g_model.varioSource >= VARIO_SOURCE_A1) {
-        //frskyHubData.varioSpeed = applyChannelRatio(frskyTelemetry[g_model.varioSource - VARIO_SOURCE_A1].value, g_model.varioSource - VARIO_SOURCE_A1);
-		    //frskyHubData.varioSpeed = frskyTelemetry[g_model.varioSource - VARIO_SOURCE_A1].value;
         frskyHubData.varioSpeed = applyChannelRatio(g_model.varioSource - VARIO_SOURCE_A1, frskyTelemetry[g_model.varioSource - VARIO_SOURCE_A1].value);
       }
       break;
