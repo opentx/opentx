68d8fa7794f7fbeb7491b3496aa44ca68b6daa02
bsongis <bsongis@8db61ce9-5e8f-94c8-fa6b-1d1c8bcca7d0>
2012-05-30 07:47:12 +0000

    img2lbm script restored

--- util/img2lbm.py
+++ util/img2lbm.py
@@ -12,7 +12,7 @@ if sys.argv[3] == "img":
     rows = 1
     if len(sys.argv) > 4:
         rows = int(sys.argv[4])
-    f.write("%d,%d,0,\n" % (width, height/rows))
+    f.write("%d,%d,\n" % (width, height/rows))
     for y in range(0, height, 8):
         for x in range(width):
             value = 0
@@ -21,42 +21,6 @@ if sys.argv[3] == "img":
                     value += 1 << z
             f.write("0x%02x," % value)
         f.write("\n")
-
-elif sys.argv[3] == "imgc":
-    rows = 1
-    if len(sys.argv) > 4:
-        rows = int(sys.argv[4])
-    f.write("%d,%d,1,\n" % (width, height/rows))
-    val=0
-    ocur=0
-    for y in range(0, height, 8):
-        for x in range(width):
-            value = 0
-            for z in range(8):
-                if image.pixel(x, y+z) == Qt.qRgb(0, 0, 0):
-                    value += 1 << z
-            if (val==value) & (ocur<255):
-		ocur+=1
-	    else:
-		if ocur>1:
-		    if ocur>2:
-			f.write("0xf0,0x%02x,0x%02x," % (val,ocur))
-		    else:
-			f.write("0x%02x,0x%02x," % (val,val))
-		else:
-		    if val!=0xf0:
-			f.write("0x%02x," % val)
-		    else:
-			f.write("0xf0,0xf0,0x01,")
-		val=value
-		ocur=1
-        f.write("\n")
-    if ocur>0:
-	if ocur>1: f.write("0xf0,0x%02x,0x%02x," % (val,ocur))
-	else:
-	    if val!=0xf0: f.write("0x%02x," % val)
-	    else: f.write("0xf0,0xf0,0x01,")
-        
 elif sys.argv[3] == "char":
     for y in range(0, height, 8):
         for x in range(width):
