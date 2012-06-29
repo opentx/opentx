221826f457515c71871aa7c377de72b731533f7c
bsongis <bsongis@8db61ce9-5e8f-94c8-fa6b-1d1c8bcca7d0>
2012-06-29 20:28:32 +0000

    TWO_DOTS option was wrong as well

--- src/lcd.cpp
+++ src/lcd.cpp
@@ -469,8 +469,8 @@ void putsStrIdx(uint8_t x, uint8_t y, const pm_char *str, uint8_t idx, uint8_t a
 {
   lcd_putsAtt(x, y, str, att & ~BSS); // TODO use something else than BSS for LEADING0
   lcd_outdezNAtt(lcdLastPos, y, idx, att|LEFT, 2);
-  lcd_putsAtt(x, y, str, att & ~BSS); // TODO use something else than BSS for LEADING0
   if (att&TWO_DOTS) lcd_putc(lcdLastPos, y, ':');
+  lcd_putsAtt(x, y, str, att & ~BSS); // TODO use something else than BSS for LEADING0
 }
 
 void putsChnRaw(uint8_t x, uint8_t y, uint8_t idx, uint8_t att)
