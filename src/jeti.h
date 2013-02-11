983a7d8c287ef61754315e3360894a624bb02049
bsongis <bsongis@8db61ce9-5e8f-94c8-fa6b-1d1c8bcca7d0>
2013-02-11 06:16:17 +0000

    Jeti compilation error

--- src/jeti.h
+++ src/jeti.h
@@ -35,28 +35,17 @@
 #ifndef jeti_h
 #define jeti_h
 
-
 #define JETI_KEY_LEFT		0x70
 #define JETI_KEY_RIGHT		0xe0
 #define JETI_KEY_UP		0xd0
 #define JETI_KEY_DOWN		0xb0
 #define JETI_KEY_NOCHANGE	0xf0
 
-extern uint16_t jeti_keys;
-extern uint8_t JetiBuffer[32]; // 32 characters
-extern uint8_t JetiBufferReady;
-
 void JETI_Init(void);
 void JETI_DisableTXD (void);
 void JETI_EnableTXD (void);
 void JETI_DisableRXD (void);
 void JETI_EnableRXD (void);
-
-void JETI_putw (uint16_t c);
-void JETI_putc (uint8_t c);
-void JETI_puts (char *s);
-void JETI_put_start (void);
-void JETI_put_stop (void);
 
 void menuTelemetryJeti(uint8_t event);
 
