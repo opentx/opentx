4d03134e9e808e2ca8ef51588975fba7955df887
bsongis@gmail.com <bsongis@gmail.com@8db61ce9-5e8f-94c8-fa6b-1d1c8bcca7d0>
2013-04-08 19:12:38 +0000

    [stock128] Less RAM used!!!

--- src/translations/it.h
+++ src/translations/it.h
@@ -284,7 +284,7 @@
 
 #if defined(CPUARM)
   #define TR_CUSTOMSW          "SW1""SW2""SW3""SW4""SW5""SW6""SW7""SW8""SW9""SWA""SWB""SWC""SWD""SWE""SWF""SWG""SWH""SWI""SWJ""SWK""SWL""SWM""SWN""SWO""SWP""SWQ""SWR""SWS""SWT""SWU""SWV""SWW"
-#elif defined(PCBGRUVIN9X) || defined(CPUM128)
+#elif defined(PCBGRUVIN9X)
   #define TR_CUSTOMSW          "SW1""SW2""SW3""SW4""SW5""SW6""SW7""SW8""SW9""SWA""SWB""SWC""SWD""SWE""SWF"
 #else
   #define TR_CUSTOMSW          "SW1""SW2""SW3""SW4""SW5""SW6""SW7""SW8""SW9""SWA""SWB""SWC"
