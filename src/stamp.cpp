fd39a178bbfc579e00d9f90f41deeb1ec4a5d9a6
bernet.andre@gmail.com <bernet.andre@gmail.com@8db61ce9-5e8f-94c8-fa6b-1d1c8bcca7d0>
2013-04-15 10:37:23 +0000

    [M128] Variant missing in stamp

--- src/stamp.cpp
+++ src/stamp.cpp
@@ -43,7 +43,7 @@
 const pm_char stamp1[] PROGMEM = " SVN: " SVN_STR;
 const pm_char stamp2[] PROGMEM = "DATE: " DATE_STR;
 const pm_char stamp3[] PROGMEM = "TIME: " TIME_STR;
-#if defined(CPUM64)
+#if defined(PCBSTD)
 const pm_char eeprom_stamp[] PROGMEM = "EEPR: " DEFNUMSTR(EEPROM_VER) "-" DEFNUMSTR(EEPROM_VARIANT);
 #else
 const pm_char eeprom_stamp[] PROGMEM = "EEPR: " DEFNUMSTR(EEPROM_VER);
