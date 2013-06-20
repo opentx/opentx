b7be29b9ca463d1f8c28450f84a3bad727c5b5b5
bernet.andre@gmail.com <bernet.andre@gmail.com@8db61ce9-5e8f-94c8-fa6b-1d1c8bcca7d0>
2013-06-20 09:28:54 +0000

    Servo test template fixed

--- src/templates.cpp
+++ src/templates.cpp
@@ -236,7 +236,7 @@ void applyTemplate(uint8_t idx)
 
       // Servo Test
       case TMPL_SERVO_TEST:
-        md=setDest(15, MIXSRC_SW1, true); md->weight=110; md->mltpx=MLTPX_ADD; md->delayUp = 6; md->delayDown = 6; md->speedUp = 8; md->speedDown = 8;
+        md=setDest(NUM_CHNOUT-1, MIXSRC_SW1, true); md->weight=110; md->mltpx=MLTPX_ADD; md->delayUp = 6; md->delayDown = 6; md->speedUp = 8; md->speedDown = 8;
         setSwitch(1, CS_VNEG, MIXSRC_LAST_CH, 0);
         break;
 
