6955a492d7fc9756f1d3ba8e301e29fcee5d8718
Bertrand Songis <bsongis@gmail.com>
2014-02-21 11:52:02 +0100

    A new set of amplitudes for the sine wave ...

--- radio/util/sinus.py
+++ radio/util/sinus.py
@@ -3,14 +3,27 @@
 import math
 
 samples = 1024
-amplitudes = (28000, 0, 28000, 0, 28000)
+amplitudes = (49000, 0, 25000, 0, 25000)
+
+max = 0
+min = 0
 
 for i in range(samples):
-  sample = 0.0
-  for harmonic, amplitude in enumerate(amplitudes):    
-      sample += math.sin(math.pi*2*i*(harmonic+1)/samples) * amplitude / 2
-      if sample > 32767 or sample < -32768:
-        print "erreur"
-  print "%d," % int(sample),
-  if i % 10 == 9:
-    print
+    sample = 0.0
+    for harmonic, amplitude in enumerate(amplitudes):    
+        sample += math.sin(math.pi*2*i*(harmonic+1)/samples) * amplitude / 2
+    sample = int(sample)
+    if sample > max:
+        max = sample
+    elif sample < min:
+        min = sample
+    print "%d," % sample,
+    if i % 10 == 9:
+        print
+
+print
+print 'Range is:', min, max
+
+if max > 32767 or min < -32768:
+    print "Invalid range!"
+        
