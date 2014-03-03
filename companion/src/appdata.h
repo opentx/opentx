5b5aac9d167b3060df5df0c3c4b23222acb099e7
Kjell Kernen <github@kernen.se>
2014-03-03 16:54:45 +0100

    Added GPLv2 licence texts to new files

--- companion/src/appdata.h
+++ companion/src/appdata.h
@@ -1,5 +1,16 @@
-// Companion Application Data Class Declaration.
-// Author Kjell Kernen
+/*
+ * Author - Kjell Kernen
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
 
 // All temporary and permanent global variables are defined here to make 
 // initialization and storage safe and visible.
