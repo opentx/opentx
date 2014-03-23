32090b8bbe28b65ccfea9971f89392c98ad8c1e9
Damjan Adamic <projectkk2glider@gmail.com>
2014-03-23 20:38:42 +0100

    Compiler warning removed for gcc 4.8.3

--- radio/src/targets/taranis/usb_conf.h
+++ radio/src/targets/taranis/usb_conf.h
@@ -154,15 +154,17 @@
 #endif /* USB_OTG_HS_INTERNAL_DMA_ENABLED */
 
 /* __packed keyword used to decrease the data type alignment to 1-byte */
-#if defined (__CC_ARM)         /* ARM Compiler */
-  #define __packed    __packed
-#elif defined (__ICCARM__)     /* IAR Compiler */
-  #define __packed    __packed
-#elif defined   ( __GNUC__ )   /* GNU Compiler */                        
-  #define __packed    __attribute__ ((__packed__))
-#elif defined   (__TASKING__)  /* TASKING Compiler */
-  #define __packed    __unaligned
-#endif /* __CC_ARM */
+#if !defined(__packed)
+  #if defined (__CC_ARM)         /* ARM Compiler */
+    #define __packed    __packed
+  #elif defined (__ICCARM__)     /* IAR Compiler */
+    #define __packed    __packed
+  #elif defined   ( __GNUC__ )   /* GNU Compiler */                        
+    #define __packed    __attribute__ ((__packed__))
+  #elif defined   (__TASKING__)  /* TASKING Compiler */
+    #define __packed    __unaligned
+  #endif /* __CC_ARM */
+#endif
 
 #endif //__USB_CONF__H__
 
