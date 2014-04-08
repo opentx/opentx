692b13bba5b31d19502e9cc9630942386ba32b77
bsongis <bsongis@gmail.com>
2014-04-08 15:55:46 +0200

    FIRMWARE.BIN now exported in Taranis massstorage

--- radio/src/targets/sky9x/board_sky9x.h
+++ radio/src/targets/sky9x/board_sky9x.h
@@ -43,6 +43,10 @@
 
 extern uint16_t ResetReason;
 
+#define BOOTLOADER_SIZE                 0x8000
+#define FIRMWARE_SIZE                   (256*1024)
+#define FIRMWARE_ADDRESS                0x00400000
+
 #if defined(REVA)
 #define GPIO_BUTTON_MENU                PIOB->PIO_PDSR
 #define GPIO_BUTTON_EXIT                PIOA->PIO_PDSR
