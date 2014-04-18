f3678b2002f6afc599e393a59bcfd00d77f0a971
Kjell Kernen <kjell.kernen@gmail.com>
2014-04-19 00:21:29 +0200

    All firmware selections have been removed from the download dialog. A consequence is that it is always possible to try to download voice files. Even for those boards that have no voice capability. I do not think that is a big problem. Anyone trying to download voice files for a unmodified 9x should not be surprised to get a "file not found" browser message.

--- companion/src/fwpreferencesdialog.h
+++ companion/src/fwpreferencesdialog.h
@@ -21,25 +21,9 @@ public:
 
 private:
     Ui::fwPreferencesDialog *ui;
-    QList<QCheckBox *> optionsCheckBoxes;
-    bool updateLock;
-
-    void showVoice(bool);
-    void showVoice();
-    void hideVoice();
-    void populateLocale();
-    void populateFirmwareOptions(const FirmwareInfo *);
-    FirmwareVariant getFirmwareVariant();
     void initSettings();
-    QCheckBox * voice;
 
 private slots:
-    void shrink();
-    void writeValues();
-    void firmwareLangChanged();
-    void baseFirmwareChanged();
-    void firmwareOptionChanged(bool state);
-    void firmwareChanged();
     void on_fw_dnld_clicked();
     void on_voice_dnld_clicked();
     void on_checkFWUpdates_clicked();
