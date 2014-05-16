9bbe3236abdf915272cc3f6bf082d9972df99d82
openfsguruh <open.20.fsguruh@xoxy.net>
2014-05-16 16:48:07 +0200

    a special volatile saves 40 bytes if FRSKY_STICKS used

--- radio/src/opentx.cpp
+++ radio/src/opentx.cpp
@@ -1375,7 +1375,7 @@ uint16_t anaIn(uint8_t chan)
 #else
   static const pm_char crossAna[] PROGMEM = {3,1,2,0,4,5,6,7};
 #if defined(FRSKY_STICKS)
-  uint16_t temp = s_anaFilt[pgm_read_byte(crossAna+chan)];
+  volatile uint16_t temp = s_anaFilt[pgm_read_byte(crossAna+chan)];  // volatile saves here 40 bytes; maybe removed for newer AVR when available
   if (chan < NUM_STICKS && (g_eeGeneral.stickReverse & (1 << chan))) {
     temp = 2048 - temp;
   }
