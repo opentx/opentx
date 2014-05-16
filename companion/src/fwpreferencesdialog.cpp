b4d99acea9bdc06ffd8e2f0f35b415af3a785f30
Kjell Kernen <github@kernen.se>
2014-05-16 10:18:02 +0200

    Companion voice downloads switched to http://voice.open-tx.org/. By using separate download areas for each Companion version it is possible to change the format and content of the voice downloads between versions. For Companion2 some download alternatives have been removed and some have been added.

--- companion/src/fwpreferencesdialog.cpp
+++ companion/src/fwpreferencesdialog.cpp
@@ -46,7 +46,7 @@ void FirmwarePreferencesDialog::on_fw_dnld_clicked()
 
 void FirmwarePreferencesDialog::on_voice_dnld_clicked()
 {
-  QString url="http://fw.opentx.it/voices/";
+  QString url="http://voice.open-tx.org/";
   QString fwType = g.profile[g.id()].fwType();
   QStringList list = fwType.split("-");
   QString firmware = QString("%1-%2").arg(list[0]).arg(list[1]);
