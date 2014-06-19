2a277e930cb8e598c46aa24a5bf29056420cad6a
Damjan Adamic <projectkk2glider@gmail.com>
2014-06-19 16:44:24 +0200

    Correct minimum value for speed is 0

--- radio/src/opentx.cpp
+++ radio/src/opentx.cpp
@@ -716,6 +716,11 @@ ls_telemetry_value_t minTelemValue(uint8_t channel)
     case TELEM_HDG:
       return 0;
 #if defined(CPUARM)
+    case TELEM_SPEED:
+    case TELEM_MAX_SPEED:
+    case TELEM_ASPEED:
+    case TELEM_MAX_ASPEED:
+      return 0;
     default:
       return -30000;
 #else
