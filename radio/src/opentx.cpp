c9875525a42734f0572c02b835c274df555037a8
Damjan Adamic <projectkk2glider@gmail.com>
2014-09-26 11:31:16 +0200

    Re #1610: * sport.log only active if SPORT_FILE_LOG=YES * audio concurrent access fixes * added IO mutex * added Event Trace Buffer * fixed SD card error

--- radio/src/opentx.cpp
+++ radio/src/opentx.cpp
@@ -2524,6 +2524,10 @@ void opentxStart()
 {
   doSplash();
 
+#if defined(DEBUG_TRACE_BUFFER)
+    trace_event(trace_start, 0x12345678); 
+#endif 
+
 #if defined(PCBSKY9X) && defined(SDCARD) && !defined(SIMU)
   for (int i=0; i<500 && !Card_initialized; i++) {
     CoTickDelay(1);  // 2ms
