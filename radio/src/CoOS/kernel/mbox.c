315934f8d0174596f061e9101c7c7e16ff3a30b0
bsongis <bsongis@gmail.com>
2015-02-01 22:05:28 +0100

    Move to latest CoOS

--- radio/src/CoOS/kernel/mbox.c
+++ radio/src/CoOS/kernel/mbox.c
@@ -1,15 +1,38 @@
 /**
  *******************************************************************************
  * @file       mbox.c
- * @version   V1.1.4    
- * @date      2011.04.20
+ * @version    V1.1.6    
+ * @date       2014.05.23
  * @brief      Mailbox management implementation code of CooCox CoOS kernel.	
  *******************************************************************************
  * @copy
  *
- * INTERNAL FILE,DON'T PUBLIC.
+ *  Redistribution and use in source and binary forms, with or without 
+ *  modification, are permitted provided that the following conditions 
+ *  are met: 
+ *  
+ *      * Redistributions of source code must retain the above copyright 
+ *  notice, this list of conditions and the following disclaimer. 
+ *      * Redistributions in binary form must reproduce the above copyright
+ *  notice, this list of conditions and the following disclaimer in the
+ *  documentation and/or other materials provided with the distribution. 
+ *      * Neither the name of the <ORGANIZATION> nor the names of its 
+ *  contributors may be used to endorse or promote products derived 
+ *  from this software without specific prior written permission. 
+ *  
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+ *  THE POSSIBILITY OF SUCH DAMAGE.
  * 
- * <h2><center>&copy; COPYRIGHT 2009 CooCox </center></h2>
+ * <h2><center>&copy; COPYRIGHT 2014 CooCox </center></h2>
  *******************************************************************************
  */ 
 
@@ -208,8 +231,11 @@ void* CoPendMail(OS_EventID id,U32 timeout,StatusType* perr)
             *perr = E_OK;
             
             /* Have recived a message or the mailbox have been deleted        */
-            pmail = curTCB->pmail;          
+            OsSchedLock();
+            pmail = curTCB->pmail;    
+            pecb->eventCounter = 0;               
             curTCB->pmail = Co_NULL;
+            OsSchedUnlock(); 
             return pmail;               /* Return received message or Co_NULL    */
         }
         else                            /* If time-out is configured          */
@@ -228,8 +254,13 @@ void* CoPendMail(OS_EventID id,U32 timeout,StatusType* perr)
             else    /* Have recived a message or the mailbox have been deleted*/
             {
                 *perr = E_OK;
-                pmail = curTCB->pmail;
+                
+                OsSchedLock();
+                pmail = curTCB->pmail;            
+                pecb->eventCounter = 0;   
                 curTCB->pmail = Co_NULL;
+                OsSchedUnlock(); 
+                
                 return pmail;           /* Return received message or Co_NULL    */
             }			
         }	
--- radio/src/CoOS/portable/arch.c
+++ radio/src/CoOS/portable/arch.c
@@ -1,14 +1,37 @@
 /**
  *******************************************************************************
  * @file      arch.c
- * @version   V1.1.4    
- * @date      2011.04.20
+ * @version    V1.1.6    
+ * @date       2014.05.23
  * @brief     This file provides InitTaskContext() and SysTick_Handler().
  *******************************************************************************
  * @copy
- *	WRITE COPY INFORMATION USE CAPITAL LETTER
- *
- * <h2><center>&copy; COPYRIGHT 2009 CooCox </center></h2>
+ *  Redistribution and use in source and binary forms, with or without 
+ *  modification, are permitted provided that the following conditions 
+ *  are met: 
+ *  
+ *      * Redistributions of source code must retain the above copyright 
+ *  notice, this list of conditions and the following disclaimer. 
+ *      * Redistributions in binary form must reproduce the above copyright
+ *  notice, this list of conditions and the following disclaimer in the
+ *  documentation and/or other materials provided with the distribution. 
+ *      * Neither the name of the <ORGANIZATION> nor the names of its 
+ *  contributors may be used to endorse or promote products derived 
+ *  from this software without specific prior written permission. 
+ *  
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+ *  THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * <h2><center>&copy; COPYRIGHT 2014 CooCox </center></h2>
  *******************************************************************************
  */  
 
@@ -34,12 +57,19 @@ OS_STK *InitTaskContext(FUNCPtr task,void *param,OS_STK *pstk)
 {
     OS_STK *context;
 	context  = pstk;
+#if CFG_CHIP_TYPE == 3
+	context      = context - 18;
+#endif
     *(context--) = (U32)0x01000000L;      /* xPSR	        */
 	*(context--) = (U32)task;             /* Entry point of task.                         */
 	*(context)   = (U32)0xFFFFFFFEL;
     context      = context - 5;
 	*(context)   = (U32)param;            /* R0: argument */
 	context      = context - 8;
+#if CFG_CHIP_TYPE == 3
+	context      = context - 16;
+#endif
+//    *(--context) = 0xfffffffd;          /* LR */
   	
     return (context);                   /* Returns location of new stack top. */
 }
