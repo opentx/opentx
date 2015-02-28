opentx/opentx

commit 92ceaa948c1f6ead565e7f1e3edd72fa13a1c8c2

MEGA2560 update
Build option for Companion
Default LCD = ST7565P and not ST7565R

companion/src/firmwares/opentx/opentxinterface.cpp
@@ -1243,6 +1243,29 @@ void registerOpenTxFirmwares()
 
   /* MEGA2560 board */
   openTx = new OpenTxFirmware("opentx-mega2560", QObject::tr("OpenTX for MEGA2560 board"), BOARD_MEGA2560);
+  Option LCD_options[] = { { "ST7565P LCD", QObject::tr("ST7565P LCD or compatible"), 0 }, { "ST7565R LCD", QObject::tr("ST7565R LCD or compatible"), 0 }, { NULL } };
+  openTx->addOptions(LCD_options);
+  openTx->addOption("heli", QObject::tr("Enable heli menu and cyclic mix support"));
+  openTx->addOption("templates", QObject::tr("Enable TEMPLATES menu"));
+  openTx->addOption("nofp", QObject::tr("No flight modes"));
+  openTx->addOption("nocurves", QObject::tr("Disable curves menus"));
+  openTx->addOption("sdcard", QObject::tr("Support for SD memory card"));
+  openTx->addOption("voice", QObject::tr("Used if you have voice module"));
+  openTx->addOption("PXX", QObject::tr("Support of FrSky PXX protocol"));
+  Option dsm2_options[] = { { "DSM2", QObject::tr("Support for DSM2 modules"), 0 }, { "DSM2PPM", QObject::tr("Support for DSM2 modules using ppm instead of true serial"), 0 }, { NULL } };
+  openTx->addOptions(dsm2_options);
+  openTx->addOption("ppmca", QObject::tr("PPM center adjustment in limits"));
+  openTx->addOption("gvars", QObject::tr("Global variables"), GVARS_VARIANT);
+  openTx->addOption("symlimits", QObject::tr("Symetrical Limits"));
+  openTx->addOption("mixersmon", QObject::tr("Adds mixers output view to the CHANNELS MONITOR screen, pressing [ENT] switches between the views"));
+  openTx->addOption("autosource", QObject::tr("In model setup menus automatically set source by moving the control"));
+  openTx->addOption("autoswitch", QObject::tr("In model setup menus automatically set switch by moving the control"));
+  openTx->addOption("dblkeys", QObject::tr("Enable resetting values by pressing up and down at the same time"));
+  openTx->addOption("nographics", QObject::tr("No graphical check boxes and sliders"));
+  openTx->addOption("battgraph", QObject::tr("Battery graph"));
+  openTx->addOption("nobold", QObject::tr("Don't use bold font for highlighting active items"));
+  openTx->addOption("pgbar", QObject::tr("EEprom write Progress bar"));
+  openTx->addOption("imperial", QObject::tr("Imperial units"));
   addOpenTxCommonOptions(openTx);
   firmwares.push_back(openTx);
 

radio/src/Makefile
@@ -657,7 +657,7 @@ ifeq ($(PCB), MEGA2560)
   MCU = atmega2560
   OPT = s
   CPPDEFS = -DF_CPU=$(F_CPU)UL -DPCBMEGA2560 -DCPUM2560 -DEEPROM_VARIANT=0 -DAUDIO
-  SDCARD = YES
+  SDCARD = NO
   BUZZER = YES
   THR_TRACE = YES
   INCDIRS += targets/mega2560 targets/stock FatFs FatFs/option
@@ -668,10 +668,10 @@ ifeq ($(PCB), MEGA2560)
   BITMAPS += bitmaps/9X/splash.lbm bitmaps/9X/asterisk.lbm bitmaps/9X/about.lbm
   GUIGENERALSRC += gui/$(GUIDIRECTORY)/menu_general_diagkeys.cpp gui/$(GUIDIRECTORY)/menu_general_diaganas.cpp
 
-  ifeq ($(PCBREV), ST7565P)
-    EXTRABOARDSRC = targets/stock/lcd_driver.cpp targets/common_avr/telemetry_driver.cpp
+  ifeq ($(PCBREV), ST7565R)
+    EXTRABOARDSRC = targets/mega2560/lcd_driver_ST7565R.cpp targets/common_avr/telemetry_driver.cpp 
   else
-    EXTRABOARDSRC = targets/mega2560/lcd_driver_ST7565R.cpp targets/common_avr/telemetry_driver.cpp
+    EXTRABOARDSRC = targets/stock/lcd_driver.cpp targets/common_avr/telemetry_driver.cpp
   endif
 
   ifeq ($(SDCARD), YES)
@@ -692,7 +692,6 @@ ifeq ($(PCB), MEGA2560)
   endif
 
   # Enable extra rotary encoders
-  # Values = YES, NO
   ROTARY_ENCODERS = 2
   CPPDEFS += -DROTARY_ENCODERS=${ROTARY_ENCODERS}
 endif

commit 04acd54a443c8f5bda46ceb0409640f2b8458b04

Fixed compiler error introduced in 92ceaa948c1f6ead565e7f1e3edd72fa13a1c8c2

companion/src/firmwares/opentx/opentxinterface.cpp
@@ -1087,6 +1087,7 @@ void registerOpenTxFirmwares()
   Option ext_options[] = { { "frsky", QObject::tr("Support for frsky telemetry mod"), FRSKY_VARIANT }, { "telemetrez", QObject::tr("Support for telemetry easy board"), FRSKY_VARIANT }, { "jeti", QObject::tr("Support for jeti telemetry mod"), 0 }, { "ardupilot", QObject::tr("Support for receiving ardupilot data"), 0 }, { "nmea", QObject::tr("Support for receiving NMEA data"), 0 }, { "mavlink", QObject::tr("Support for MAVLINK devices"), MAVLINK_VARIANT }, { NULL } };
   Option nav_options[] = { { "rotenc", QObject::tr("Rotary Encoder use in menus navigation") }, { "potscroll", QObject::tr("Pots use in menus navigation") }, { NULL } };
   Option extr_options[] = { { "frsky", QObject::tr("Support for frsky telemetry mod"), FRSKY_VARIANT }, { "jeti", QObject::tr("Support for jeti telemetry mod"), 0 }, { "ardupilot", QObject::tr("Support for receiving ardupilot data"), 0 }, { "nmea", QObject::tr("Support for receiving NMEA data"), 0 }, { "mavlink", QObject::tr("Support for MAVLINK devices"), MAVLINK_VARIANT }, { NULL } };
+  Option dsm2_options[] = { { "DSM2", QObject::tr("Support for DSM2 modules"), 0 }, { "DSM2PPM", QObject::tr("Support for DSM2 modules using ppm instead of true serial"), 0 }, { NULL } };
 
   /* 9x board */
   openTx = new OpenTxFirmware("opentx-9x", QObject::tr("OpenTX for 9X board"), BOARD_STOCK);
@@ -1224,7 +1225,6 @@ void registerOpenTxFirmwares()
   openTx->addOption("sdcard", QObject::tr("Support for SD memory card"));
   openTx->addOption("voice", QObject::tr("Used if you have modified your radio with voice mode"));
   openTx->addOption("PXX", QObject::tr("Support of FrSky PXX protocol"));
-  Option dsm2_options[] = { { "DSM2", QObject::tr("Support for DSM2 modules"), 0 }, { "DSM2PPM", QObject::tr("Support for DSM2 modules using ppm instead of true serial"), 0 }, { NULL } };
   openTx->addOptions(dsm2_options);
   openTx->addOption("ppmca", QObject::tr("PPM center adjustment in limits"));
   openTx->addOption("gvars", QObject::tr("Global variables"), GVARS_VARIANT);
@@ -1252,7 +1252,6 @@ void registerOpenTxFirmwares()
   openTx->addOption("sdcard", QObject::tr("Support for SD memory card"));
   openTx->addOption("voice", QObject::tr("Used if you have voice module"));
   openTx->addOption("PXX", QObject::tr("Support of FrSky PXX protocol"));
-  Option dsm2_options[] = { { "DSM2", QObject::tr("Support for DSM2 modules"), 0 }, { "DSM2PPM", QObject::tr("Support for DSM2 modules using ppm instead of true serial"), 0 }, { NULL } };
   openTx->addOptions(dsm2_options);
   openTx->addOption("ppmca", QObject::tr("PPM center adjustment in limits"));
   openTx->addOption("gvars", QObject::tr("Global variables"), GVARS_VARIANT);