730bc8c608529fdd1f6ce6b092c4334c2741538b
jtaylor2 <jhtaylor20@gmail.com>
2015-03-30 14:22:23 -0400

    Fix for special function play icon Issue 2090

--- companion/src/modeledit/customfunctions.cpp
+++ companion/src/modeledit/customfunctions.cpp
@@ -258,6 +258,7 @@ void CustomFunctionsPanel::playMusic()
       phononCurrent = -1;
     }
     else {
+      playBT[phononCurrent]->setIcon(CompanionIcon("play.png"));
       phononCurrent = index;
       clickObject->clear();
 #ifdef __APPLE__
