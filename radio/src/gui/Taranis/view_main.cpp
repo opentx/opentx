82d11abb2e58416c57fb82a80c7c95203941133e
Andre Bernet <andre@andrebernet.ch>
2015-03-31 19:21:11 +0400

    Display trims problem (Fix #2157)

--- radio/src/gui/Taranis/view_main.cpp
+++ radio/src/gui/Taranis/view_main.cpp
@@ -117,7 +117,7 @@ void doMainScreenGraphics()
 
 void displayTrims(uint8_t phase)
 {
-  for (unsigned int i=0; i<NUM_STICKS; ++i) {
+  for (unsigned int i=0; i<NUM_STICKS; i++) {
     coord_t x[4] = { TRIM_LH_X, TRIM_LV_X, TRIM_RV_X, TRIM_RH_X };
     uint8_t vert[4] = { 0, 1, 1, 0 };
     coord_t xm, ym;
@@ -159,9 +159,9 @@ void displayTrims(uint8_t phase)
       if (exttrim) {
         lcd_hline(xm-1, ym,  3);
       }
-      if (g_model.displayTrims != DISPLAY_TRIMS_NEVER) {
-        if ((g_model.displayTrims == DISPLAY_TRIMS_ALWAYS && dir != 0) || (trimsDisplayTimer > 0 && (trimsDisplayMask & (1<<stickIndex)))) {
-          lcd_outdezAtt(dir>0 ? 25 : 57, xm-2, -abs(dir), TINSIZE|VERTICAL);
+      if (g_model.displayTrims != DISPLAY_TRIMS_NEVER && dir != 0) {
+        if (g_model.displayTrims == DISPLAY_TRIMS_ALWAYS || (trimsDisplayTimer > 0 && (trimsDisplayMask & (1<<i)))) {
+          lcd_outdezAtt(dir>0 ? 22 : 54, xm-2, -abs(dir/5), TINSIZE|VERTICAL);
         }
       }
     }
@@ -182,8 +182,8 @@ void displayTrims(uint8_t phase)
         lcd_vline(xm, ym-1,  3);
       }
       if (g_model.displayTrims != DISPLAY_TRIMS_NEVER && dir != 0) {
-        if (g_model.displayTrims == DISPLAY_TRIMS_ALWAYS || (trimsDisplayTimer > 0 && (trimsDisplayMask & (1<<stickIndex)))) {
-          lcd_outdezAtt((stickIndex==0 ? TRIM_LH_X : TRIM_RH_X)+(dir>0 ? -9 : 22), ym-2, -abs(dir), TINSIZE);
+        if (g_model.displayTrims == DISPLAY_TRIMS_ALWAYS || (trimsDisplayTimer > 0 && (trimsDisplayMask & (1<<i)))) {
+          lcd_outdezAtt((stickIndex==0 ? TRIM_LH_X : TRIM_RH_X)+(dir>0 ? -11 : 20), ym-2, -abs(dir/5), TINSIZE);
         }
       }
     }
