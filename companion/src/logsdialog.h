46ccd1592414b7c49951c2fd5893160ec0d3a9d9
Grieb <grieb.dev@gmail.com>
2015-05-09 15:39:07 -0300

    Log Viewer multi-axes. Fixes #2267.

--- companion/src/logsdialog.h
+++ companion/src/logsdialog.h
@@ -5,24 +5,35 @@
 #include <QtGui>
 #include "qcustomplot/qcustomplot.h"
 
+#define INVALID_MIN 999999
+#define INVALID_MAX -999999
+
+enum yaxes_t {
+  firstLeft = 0,
+  firstRight,
+  secondLeft,
+  secondRight,
+  AXES_LIMIT // = 4
+};
+
 struct coords {
   QVector<double> x, y;
   double min_y;
   double max_y;
-  bool secondRange;
+  yaxes_t yaxis;
   QString name;
+};
+
+struct minMax {
+  double min;
+  double max;
 };
 
 struct plotsCollection {
   QVarLengthArray<struct coords> coords;
   double min_x;
   double max_x;
-  bool twoRanges;
   bool tooManyRanges;
-  double rangeOneMin;
-  double rangeOneMax;
-  double rangeTwoMin;
-  double rangeTwoMax;
 };
 
 namespace Ui {
@@ -50,7 +61,7 @@ private slots:
   void on_fileOpen_BT_clicked();
   void on_sessions_CB_currentIndexChanged(int index);
   void on_mapsButton_clicked();
-  void setRangeyAxis2(QCPRange range);
+  void yAxisChangeRanges(QCPRange range);
 
 private:
   QList<QStringList> csvlog;
@@ -63,12 +74,8 @@ private:
   QVarLengthArray<Qt::GlobalColor> colors;
   QPen pen;
 
-  bool hasyAxis2;
-  double rangeRatio;
-  double rangeyAxisMin;
-  double rangeyAxisMax;
-  double rangeyAxis2Min;
-  double rangeyAxis2Max;
+  double yAxesRatios[AXES_LIMIT];
+  minMax yAxesRanges[AXES_LIMIT];
 };
 
 #endif // LOGSDIALOG_H
