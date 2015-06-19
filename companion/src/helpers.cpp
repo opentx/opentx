c0afcb51f7af76530c637656d44402a1dd4f7009
Damjan Adamic <projectkk2glider@gmail.com>
2015-06-19 18:39:45 +0200

    Fixes #2326: Google Earth export GPS glitch filtering and lat/lon synchronization (ported from master)

--- companion/src/helpers.cpp
+++ companion/src/helpers.cpp
@@ -1060,3 +1060,55 @@ bool caseInsensitiveLessThan(const QString &s1, const QString &s2)
 {
   return s1.toLower() < s2.toLower();
 }
+
+bool GpsGlitchFilter::isGlitch(double latitude, double longitude) 
+{
+  if ((fabs(latitude) < 0.1) && (fabs(longitude) < 0.1)) {
+    return true;
+  }
+
+  if (lastValid) {
+    if (fabs(latitude - lastLat) > 0.01) {
+      // qDebug() << "GpsGlitchFilter(): latitude glitch " << latitude << lastLat;
+      if ( ++glitchCount < 10) {
+        return true; 
+      }
+    }
+    if (fabs(longitude - lastLon) > 0.01) {
+      // qDebug() << "GpsGlitchFilter(): longitude glitch " << longitude << lastLon;
+      if ( ++glitchCount < 10) {
+        return true; 
+      }
+    }
+  }
+  lastLat = latitude;
+  lastLon = longitude;
+  lastValid = true;
+  glitchCount = 0;
+  return false;
+}
+
+bool GpsLatLonFilter::isValid(const QString & latitude, const QString & longitude)
+{
+  if (lastLat == latitude) {
+    return false;
+  }
+  if (lastLon == longitude) {
+    return false;
+  }
+  lastLat = latitude;
+  lastLon = longitude;
+  return true;
+}
+
+double toDecimalCoordinate(const QString & value) 
+{
+  if (value.isEmpty()) return 0.0;
+  double temp = int(value.left(value.length()-1).toDouble() / 100);
+  double result = temp + (value.left(value.length() - 1).toDouble() - temp * 100) / 60.0;
+  QChar direction = value.at(value.size()-1);
+  if ((direction == 'S') || (direction == 'W')) {
+    result = -result;
+  }
+  return result;
+}
