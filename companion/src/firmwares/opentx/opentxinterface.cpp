a8890b11e0ad9f527078e4d66865847a04f66cbb
Bertrand Songis <bsongis@gmail.com>
2015-09-07 21:54:07 +0200

    [Companion] CLI option added

--- companion/src/firmwares/opentx/opentxinterface.cpp
+++ companion/src/firmwares/opentx/opentxinterface.cpp
@@ -1116,7 +1116,9 @@ void addOpenTxTaranisOptions(OpenTxFirmware * firmware)
   firmware->addOption("noheli", QObject::tr("Disable HELI menu and cyclic mix support"));
   firmware->addOption("nogvars", QObject::tr("Disable Global variables"));
   firmware->addOption("lua", QObject::tr("Support for Lua model scripts"));
-  firmware->addOption("nojoystick", QObject::tr("No Joystick emulation inside the FW (only Mass Storage as in the Bootloader)"));
+  Option usb_options[] = { { "massstorage", QObject::tr("Instead of Joystick emulation, USB connection is Mass Storage (as in the Bootloader)") }, { "cli", QObject::tr("Instead of Joystick emulation, USB connection is Command Line Interface") }, { NULL } };
+  firmware->addOptions(usb_options);
+  firmware->addOption("massstorage", QObject::tr("No Joystick emulation inside the FW (only Mass Storage as in the Bootloader)"));
   firmware->addOption("mixersmon", QObject::tr("Adds mixers output view to the CHANNELS MONITOR screen, pressing [ENT] switches between the views"));
   firmware->addOption("eu", QObject::tr("Removes D8 and LR12 FrSky protocols that are not legal for use in the EU on radios sold after Jan 1st, 2015"));
   firmware->addOption("internalppm", QObject::tr("Support for PPM internal module hack"));
