518864531346cbc3a93d82877b72f80ce1af5161
Andre Bernet <andre@andrebernet.ch>
2015-09-07 22:28:29 +0200

    Remove duplicate option

--- companion/src/firmwares/opentx/opentxinterface.cpp
+++ companion/src/firmwares/opentx/opentxinterface.cpp
@@ -1118,7 +1118,6 @@ void addOpenTxTaranisOptions(OpenTxFirmware * firmware)
   firmware->addOption("lua", QObject::tr("Support for Lua model scripts"));
   Option usb_options[] = { { "massstorage", QObject::tr("Instead of Joystick emulation, USB connection is Mass Storage (as in the Bootloader)") }, { "cli", QObject::tr("Instead of Joystick emulation, USB connection is Command Line Interface") }, { NULL } };
   firmware->addOptions(usb_options);
-  firmware->addOption("massstorage", QObject::tr("No Joystick emulation inside the FW (only Mass Storage as in the Bootloader)"));
   firmware->addOption("mixersmon", QObject::tr("Adds mixers output view to the CHANNELS MONITOR screen, pressing [ENT] switches between the views"));
   firmware->addOption("eu", QObject::tr("Removes D8 and LR12 FrSky protocols that are not legal for use in the EU on radios sold after Jan 1st, 2015"));
   firmware->addOption("internalppm", QObject::tr("Support for PPM internal module hack"));
