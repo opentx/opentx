9d13484291f84751ba7f197173544ae8df8be4b3
Sean Vig <sean.v.775@gmail.com>
2015-10-08 23:40:59 -0500

    PEP8 radio/util/copyright.py
    Remove unused imports, fix whitespace, fix comments

--- radio/util/copyright.py
+++ radio/util/copyright.py
@@ -2,13 +2,12 @@
 
 from __future__ import print_function
 
-import os
-import re
 import sys
 
-def writeheader(filename,header):
+
+def writeheader(filename, header):
     """
-    write a header to filename, 
+    write a header to filename,
     skip files where first line after optional shebang matches the skip regex
     filename should be the name of the file to write to
     header should be a list of strings
@@ -19,30 +18,31 @@ def writeheader(filename,header):
     f.close()
     output = []
 
-    #comment out the next 3 lines if you don't wish to preserve shebangs
-    if len(inpt) > 0 and inpt[0].startswith("#!"): 
+    # comment out the next 3 lines if you don't wish to preserve shebangs
+    if len(inpt) > 0 and inpt[0].startswith("#!"):
         output.append(inpt[0])
         inpt = inpt[1:]
 
     if inpt[0].strip().startswith("/*"):
         for i, line in enumerate(inpt):
             if line.strip().endswith("*/"):
-                inpt = inpt[i+1:]
+                inpt = inpt[i + 1:]
                 break
 
     while not inpt[0].strip():
         inpt = inpt[1:]
 
-    output.extend(header) #add the header
+    output.extend(header)  # add the header
     for line in inpt:
         output.append(line)
     try:
         f = open(filename,'w')
         f.writelines(output)
         f.close()
-        print("added header to %s" %filename)
+        print("added header to %s" % filename)
     except IOError as err:
-        print("something went wrong trying to add header to %s: %s" % (filename,err))
+        print("something went wrong trying to add header to %s: %s" % (filename, err))
+
 
 def main(args=sys.argv):
     headerfile = open(args[1])
@@ -50,5 +50,7 @@ def main(args=sys.argv):
     headerfile.close()
     writeheader(args[2], header)
 
-#call the main method
-main()
+
+if __name__ == '__main__':
+    # call the main method
+    main()
