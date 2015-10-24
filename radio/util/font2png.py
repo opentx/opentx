9cd0cbf417de2d26c04d9eedf2d157802434f101
Bertrand Songis <bsongis@gmail.com>
2015-10-24 20:17:41 +0200

    Test for the Travis server ...

--- radio/util/font2png.py
+++ radio/util/font2png.py
@@ -6,9 +6,10 @@ from PyQt4 import Qt, QtCore, QtGui
 import glob
 
 try:
-    app = Qt.QApplication(sys.argv)
-except:
     app = Qt.QCoreApplication(sys.argv)
+except:
+    pass
+    # app = Qt.QCoreApplication(sys.argv)
 
 for f in glob.glob("fonts/*.ttf"):
     QtGui.QFontDatabase.addApplicationFont(f)
