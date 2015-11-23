7ef790d3ef3ae561d2bccedd6ce4cc921b560b3e
Damjan Adamic <projectkk2glider@gmail.com>
2015-11-23 19:51:40 +0100

    Re #3095: luaGetVersion() also returns version as numbers

--- radio/src/lua/api_general.cpp
+++ radio/src/lua/api_general.cpp
@@ -72,22 +72,28 @@ Return OpenTX version
 
 @retval string OpenTX version (ie "2.1.5")
 
-@retval list (available since 2.1.7) returns two values:
- * `string` OpenTX version (ie "2.1.5")
- * `string` radio version: `taranisx9e`, `taranisplus` or `taranis`. 
+@retval multiple (available since 2.1.7) returns 5 values:
+ * (string) OpenTX version (ie "2.1.5")
+ * (string) radio version: `taranisx9e`, `taranisplus` or `taranis`. 
 If running in simulator the "-simu" is added
+ * (number) major version (ie 2 if version 2.1.5)
+ * (number) minor version (ie 1 if version 2.1.5)
+ * (number) revison number (ie 5 if version 2.1.5)
 
 @status current Introduced in 2.0.0, expanded in 2.1.7
 
 ### Example
 
-This example also runs in OpenTX versions where the radio version was not available:
+This example also runs in OpenTX versions where the function returned only one value:
 
 ```lua
 local function run(event)
-  local ver, radio = getVersion()
+  local ver, radio, maj, minor, rev = getVersion()
   print("version: "..ver)
   if radio then print ("radio: "..radio) end
+  if maj then print ("maj: "..maj) end
+  if minor then print ("minor: "..minor) end
+  if rev then print ("rev: "..rev) end
   return 1
 end
 
@@ -97,14 +103,19 @@ Output of the above script in simulator:
 ```
 version: 2.1.7
 radio: taranis-simu
-Script finished with status 1
+maj: 2
+minor: 1
+rev: 7
 ```
 */
 static int luaGetVersion(lua_State *L)
 {
   lua_pushstring(L, VERS_STR);
   lua_pushstring(L, RADIO_VERSION);
-  return 2;
+  lua_pushnumber(L, VERSION_MAJOR);
+  lua_pushnumber(L, VERSION_MINOR);
+  lua_pushnumber(L, VERSION_REVISION);
+  return 5;
 }
 
 /*luadoc
