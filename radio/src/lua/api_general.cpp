69594ba38b39ef5e7a28058f192283cd9819542b
Damjan Adamic <projectkk2glider@gmail.com>
2015-11-23 22:41:05 +0100

    Fixes #2921: getValue() now also returns pilot position

--- radio/src/lua/api_general.cpp
+++ radio/src/lua/api_general.cpp
@@ -170,20 +170,15 @@ static int luaGetDateTime(lua_State *L)
 static void luaPushLatLon(TelemetrySensor & telemetrySensor, TelemetryItem & telemetryItem)
 /* result is lua table containing members ["lat"] and ["lon"] as lua_Number (doubles) in decimal degrees */
 {
-  lua_Number lat = 0.0;
-  lua_Number lon = 0.0;
   uint32_t gpsLat = 0;
   uint32_t gpsLon = 0;
-
   telemetryItem.gps.extractLatitudeLongitude(&gpsLat, &gpsLon); /* close, but not the format we want */
-  lat = gpsLat / 1000000.0;
-  if (telemetryItem.gps.latitudeNS == 'S') lat = -lat;
-  lon = gpsLon / 1000000.0;
-  if (telemetryItem.gps.longitudeEW == 'W') lon = -lon;
 
-  lua_createtable(L, 0, 2);
-  lua_pushtablenumber(L, "lat", lat);
-  lua_pushtablenumber(L, "lon", lon);
+  lua_createtable(L, 0, 4);
+  lua_pushtablenumber(L, "lat", gpsLat / ((telemetryItem.gps.latitudeNS == 'S') ? -1000000.0 : 1000000.0));
+  lua_pushtablenumber(L, "pilot-lat", telemetryItem.pilotLatitude / ((telemetryItem.gps.latitudeNS == 'S') ? -1000000.0 : 1000000.0));
+  lua_pushtablenumber(L, "lon", gpsLon / ((telemetryItem.gps.longitudeEW == 'W') ? -1000000.0 : 1000000.0));
+  lua_pushtablenumber(L, "pilot-lon", telemetryItem.pilotLongitude / ((telemetryItem.gps.longitudeEW == 'W') ? -1000000.0 : 1000000.0));
 }
 
 static void luaPushTelemetryDateTime(TelemetrySensor & telemetrySensor, TelemetryItem & telemetryItem)
@@ -381,6 +376,8 @@ or a name (string) of the source.
 @retval table GPS position is returned in a table:
  * `lat` (number) latitude, positive is North 
  * `lon` (number) longitude, positive is East
+ * `pilot-lat` (number) pilot latitude, positive is North 
+ * `pilot-lon` (number) pilot longitude, positive is East
 
 @retval table GPS date/time, see getDateTime()
 
