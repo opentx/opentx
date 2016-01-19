bc9ae7a1a129c3dce5b4fa814b85732733b4fd33
Damjan Adamic <projectkk2glider@gmail.com>
2016-01-19 19:22:34 +0100

    Calibration state packed in enum

--- radio/src/gui/Taranis/menu_general_calib.cpp
+++ radio/src/gui/Taranis/menu_general_calib.cpp
@@ -41,6 +41,14 @@
 #define BAR_SPACING   12
 #define BAR_HEIGHT    22
 
+enum CalibrationState {
+  CALIB_START = 0,
+  CALIB_SET_MIDPOINT,
+  CALIB_MOVE_STICKS,
+  CALIB_STORE,
+  CALIB_FINISHED
+};
+
 void drawPotsBars()
 {
   // Optimization by Mike Blandford
@@ -101,7 +109,7 @@ void menuCommonCalib(uint8_t event)
   {
     case EVT_ENTRY:
     case EVT_KEY_BREAK(KEY_EXIT):
-      reusableBuffer.calib.state = 0;
+      reusableBuffer.calib.state = CALIB_START;
       break;
 
     case EVT_KEY_BREAK(KEY_ENTER):
@@ -110,14 +118,14 @@ void menuCommonCalib(uint8_t event)
   }
 
   switch (reusableBuffer.calib.state) {
-    case 0:
+    case CALIB_START:
       // START CALIBRATION
       if (!READ_ONLY()) {
         lcd_putsLeft(MENU_HEADER_HEIGHT+2*FH, STR_MENUTOSTART);
       }
       break;
 
-    case 1:
+    case CALIB_SET_MIDPOINT:
       // SET MIDPOINT
       lcd_putsAtt(0*FW, MENU_HEADER_HEIGHT+FH, STR_SETMIDPOINT, INVERS);
       lcd_putsLeft(MENU_HEADER_HEIGHT+2*FH, STR_MENUWHENDONE);
@@ -133,7 +141,7 @@ void menuCommonCalib(uint8_t event)
       }
       break;
 
-    case 2:
+    case CALIB_MOVE_STICKS:
       // MOVE STICKS/POTS
       STICK_SCROLL_DISABLE();
       lcd_putsAtt(0*FW, MENU_HEADER_HEIGHT+FH, STR_MOVESTICKSPOTS, INVERS);
@@ -150,7 +158,7 @@ void menuCommonCalib(uint8_t event)
       }
       break;
 
-    case 3:
+    case CALIB_STORE:
       for (uint8_t i=POT1; i<=POT_LAST; i++) {
         int idx = i - POT1;
         int count = reusableBuffer.calib.xpotsCalib[idx].stepsCount;
@@ -176,11 +184,11 @@ void menuCommonCalib(uint8_t event)
       }
       g_eeGeneral.chkSum = evalChkSum();
       eeDirty(EE_GENERAL);
-      reusableBuffer.calib.state = 4;
+      reusableBuffer.calib.state = CALIB_FINISHED;
       break;
 
     default:
-      reusableBuffer.calib.state = 0;
+      reusableBuffer.calib.state = CALIB_START;
       break;
   }
 
@@ -190,7 +198,7 @@ void menuCommonCalib(uint8_t event)
 #if 0
   for (int i=POT1; i<=POT_LAST; i++) {
     uint8_t steps = 0;
-    if (reusableBuffer.calib.state == 2) {
+    if (reusableBuffer.calib.state == CALIB_MOVE_STICKS) {
       steps = reusableBuffer.calib.xpotsCalib[i-POT1].stepsCount;
     }
     else if (IS_POT_MULTIPOS(i)) {
@@ -209,14 +217,14 @@ void menuGeneralCalib(uint8_t event)
   check_simple(STR_MENUCALIBRATION, event, e_Calib, menuTabGeneral, DIM(menuTabGeneral), 0);
   menuCommonCalib(READ_ONLY() ? 0 : event);
   if (menuEvent) {
-    calibrationState = 0;
+    calibrationState = CALIB_START;
   }
 }
 
 void menuFirstCalib(uint8_t event)
 {
-  if (event == EVT_KEY_BREAK(KEY_EXIT) || reusableBuffer.calib.state == 4) {
-    calibrationState = 0;
+  if (event == EVT_KEY_BREAK(KEY_EXIT) || reusableBuffer.calib.state == CALIB_FINISHED) {
+    calibrationState = CALIB_START;
     chainMenu(menuMainView);
   }
   else {
