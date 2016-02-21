22a8a5cc80c052051341d707362c64796e3dd425
Andre Bernet <andre@andrebernet.ch>
2016-02-21 17:38:49 +0400

    [Horus] Fix LED off

--- radio/src/targets/horus/led_driver.cpp
+++ radio/src/targets/horus/led_driver.cpp
@@ -35,7 +35,15 @@ void ledInit()
 
 void ledOff()
 {
-  RCC_AHB1PeriphClockCmd(LED_RCC_AHB1Periph, DISABLE);
+  RCC_AHB1PeriphClockCmd(LED_RCC_AHB1Periph, ENABLE);
+
+  GPIO_InitTypeDef GPIO_InitStructure;
+  GPIO_InitStructure.GPIO_Pin = LED_GPIO_PIN;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+  GPIO_Init(LED_GPIO, &GPIO_InitStructure);
 }
 
 void ledRed()
