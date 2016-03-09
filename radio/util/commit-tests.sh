be531b77f054f868db1f1da8ba5a991b5a52479f
Bertrand Songis <bsongis@gmail.com>
2016-03-09 20:34:34 +0100

    Commit tests may now me executed from everywhere (not always inside sources)

--- radio/util/commit-tests.sh
+++ radio/util/commit-tests.sh
@@ -9,73 +9,74 @@ cd build
 
 # Companion
 rm -rf *
-cmake -DCMAKE_BUILD_TYPE=Debug -DPCB=TARANIS ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DPCB=TARANIS $1
 make -j2
 
 # OpenTX on 9X stock with FrSky telemetry
 rm -rf *
-cmake -DCMAKE_BUILD_TYPE=Debug -DPCB=9X -DHELI=YES -DEXT=FRSKY ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DPCB=9X -DHELI=YES -DEXT=FRSKY $1
 make -j2 firmware
 make -j2 simu
 make -j2 gtests ; ./gtests
 
 # OpenTX on 9X stock with Mavlink telemetry
 rm -rf *
-cmake -DCMAKE_BUILD_TYPE=Debug -DPCB=9X -DHELI=YES -DEXT=MAVLINK ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DPCB=9X -DHELI=YES -DEXT=MAVLINK $1
 make -j2 firmware
 
 # OpenTX on Mega2560
 rm -rf *
-cmake -DCMAKE_BUILD_TYPE=Debug -DPCB=MEGA2560 -DHELI=YES ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DPCB=MEGA2560 -DHELI=YES $1
 make -j2 firmware
 make -j2 simu
 make -j2 gtests ; ./gtests
 
 # OpenTX on Mega2560 with Mavlink telemetry
 rm -rf *
-cmake -DCMAKE_BUILD_TYPE=Debug -DPCB=MEGA2560 -DEXT=MAVLINK -DHELI=YES ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DPCB=MEGA2560 -DEXT=MAVLINK -DHELI=YES $1
 make -j2 firmware
 make -j2 simu
 make -j2 gtests ; ./gtests
 
 # OpenTX on gruvin9x board
 rm -rf *
-cmake -DCMAKE_BUILD_TYPE=Debug -DPCB=GRUVIN9X -DHELI=YES ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DPCB=GRUVIN9X -DHELI=YES $1 
 make -j2 firmware
 make -j2 simu
 make -j2 gtests ; ./gtests
 
 # OpenTX on Sky9x
 rm -rf *
-cmake -DCMAKE_BUILD_TYPE=Debug -DPCB=SKY9X -DHELI=YES ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DPCB=SKY9X -DHELI=YES $1 
 make -j2 firmware
 make -j2 simu
 make -j2 gtests ; ./gtests
 
 # OpenTX on Taranis
 rm -rf *
-cmake -DCMAKE_BUILD_TYPE=Debug -DPCB=TARANIS -DHELI=YES -DLUA=YES -DWARNINGS_AS_ERRORS=YES ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DPCB=TARANIS -DHELI=YES -DLUA=YES -DWARNINGS_AS_ERRORS=YES $1
 make -j2 firmware
 make -j2 simu
 make -j2 gtests ; ./gtests
 
 # OpenTX on Taranis X9E
 rm -rf *
-cmake -DCMAKE_BUILD_TYPE=Debug -DPCB=TARANIS -DPCBREV=REV9E -DHELI=YES -DLUA=YES -DWARNINGS_AS_ERRORS=YES .. 
+cmake -DCMAKE_BUILD_TYPE=Debug -DPCB=TARANIS -DPCBREV=REV9E -DHELI=YES -DLUA=YES -DWARNINGS_AS_ERRORS=YES $1 
 make -j2 firmware
 make -j2 simu
 make -j2 gtests ; ./gtests
 
 # OpenTX on Taranis Plus
 rm -rf *
-cmake -DCMAKE_BUILD_TYPE=Debug -DPCB=TARANIS -DPCBREV=REVPLUS -DHELI=YES -DLUA=YES -DWARNINGS_AS_ERRORS=YES .. 
+cmake -DCMAKE_BUILD_TYPE=Debug -DPCB=TARANIS -DPCBREV=REVPLUS -DHELI=YES -DLUA=YES -DWARNINGS_AS_ERRORS=YES $1 
 make -j2 firmware
 make -j2 simu
 make -j2 gtests ; ./gtests
 
 # OpenTX on Horus
 rm -rf *
-cmake -DCMAKE_BUILD_TYPE=Debug -DPCB=HORUS -DHELI=NO -DUSB=SERIAL -DCLI=YES -DDEBUG=YES .. 
+cmake -DCMAKE_BUILD_TYPE=Debug -DPCB=HORUS -DHELI=NO -DUSB=SERIAL -DCLI=YES -DDEBUG=YES $1 
 make -j2 firmware
 make -j2 simu
 #make -j2 gtests ; ./gtests
+
