ddac3ea5e0c65893e03d7d21db6b580e78f41eb3
Bertrand Songis <bsongis@gmail.com>
2016-03-12 19:18:32 +0100

    More simulators in Companion

--- compile.bat
+++ compile.bat
@@ -17,7 +17,22 @@ cmake -DCMAKE_PREFIX_PATH=C:\Qt\5.6\msvc2015 -DPCB=9X %CODE_DIR%
 cmake -DCMAKE_PREFIX_PATH=C:\Qt\5.6\msvc2015 -DPCB=GRUVIN9X %CODE_DIR%
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" radio\src\targets\simu\opentx-simulator.vcxproj /t:Rebuild /p:Configuration=Release
 
+cmake -DCMAKE_PREFIX_PATH=C:\Qt\5.6\msvc2015 -DPCB=MEGA2560 %CODE_DIR%
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" radio\src\targets\simu\opentx-simulator.vcxproj /t:Rebuild /p:Configuration=Release
+
+cmake -DCMAKE_PREFIX_PATH=C:\Qt\5.6\msvc2015 -DPCB=SKY9X %CODE_DIR%
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" radio\src\targets\simu\opentx-simulator.vcxproj /t:Rebuild /p:Configuration=Release
+
+cmake -DCMAKE_PREFIX_PATH=C:\Qt\5.6\msvc2015 -DPCB=9XRPRO %CODE_DIR%
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" radio\src\targets\simu\opentx-simulator.vcxproj /t:Rebuild /p:Configuration=Release
+
 cmake -DCMAKE_PREFIX_PATH=C:\Qt\5.6\msvc2015 -DPCB=TARANIS %CODE_DIR%
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" radio\src\targets\simu\opentx-simulator.vcxproj /t:Rebuild /p:Configuration=Release
+
+cmake -DCMAKE_PREFIX_PATH=C:\Qt\5.6\msvc2015 -DPCB=TARANIS -DPCBREV=REVPLUS %CODE_DIR%
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" radio\src\targets\simu\opentx-simulator.vcxproj /t:Rebuild /p:Configuration=Release
+
+cmake -DCMAKE_PREFIX_PATH=C:\Qt\5.6\msvc2015 -DPCB=TARANIS -DPCBREV=REV9E %CODE_DIR%
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" radio\src\targets\simu\opentx-simulator.vcxproj /t:Rebuild /p:Configuration=Release
 
 cmake -DCMAKE_PREFIX_PATH=C:\Qt\5.6\msvc2015 -DPCB=HORUS %CODE_DIR%
