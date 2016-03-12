f4174e675680cd94aea614e86adbaed13dcd2b12
Bertrand Songis <bsongis@gmail.com>
2016-03-12 18:58:32 +0100

    Windows compilation script added to git

---
+++ compile.bat
@@ -0,0 +1,28 @@
+SET CODE_DIR=%~dp0%
+
+cd %CODE_DIR%
+git fetch
+git checkout next
+git reset --hard origin/next
+
+cd ..
+rmdir build /s /q
+mkdir build
+cd build
+
+
+cmake -DCMAKE_PREFIX_PATH=C:\Qt\5.6\msvc2015 -DPCB=9X %CODE_DIR%
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" radio\src\targets\simu\opentx-simulator.vcxproj /t:Rebuild /p:Configuration=Release
+
+cmake -DCMAKE_PREFIX_PATH=C:\Qt\5.6\msvc2015 -DPCB=GRUVIN9X %CODE_DIR%
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" radio\src\targets\simu\opentx-simulator.vcxproj /t:Rebuild /p:Configuration=Release
+
+cmake -DCMAKE_PREFIX_PATH=C:\Qt\5.6\msvc2015 -DPCB=TARANIS %CODE_DIR%
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" radio\src\targets\simu\opentx-simulator.vcxproj /t:Rebuild /p:Configuration=Release
+
+cmake -DCMAKE_PREFIX_PATH=C:\Qt\5.6\msvc2015 -DPCB=HORUS %CODE_DIR%
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" radio\src\targets\simu\opentx-simulator.vcxproj /t:Rebuild /p:Configuration=Release
+
+
+cmake -DCMAKE_PREFIX_PATH=C:\Qt\5.6\msvc2015 -DPCB=HORUS %CODE_DIR%
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" companion\src\installer.vcxproj /t:Rebuild /p:Configuration=Release
