e0ead516f6db0f694aa664c2a9daa07b9feb38b1
Bertrand Songis <bsongis@gmail.com>
2016-03-16 07:32:19 +0100

    Wrong command

--- compile.bat
+++ compile.bat
@@ -10,30 +10,32 @@ rmdir build /s /q
 mkdir build
 cd build
 
-cmake -DCMAKE_PREFIX_PATH=C:\Qt\5.6\msvc2015 -DALLOW_NIGHTLY_BUILDS -DVERSION_SUFFIX=%1 -DPCB=9X %CODE_DIR%
+SET COMMON_OPTIONS=-DCMAKE_PREFIX_PATH=C:\Qt\5.6\msvc2015 -DALLOW_NIGHTLY_BUILDS=YES -DVERSION_SUFFIX=%1
+
+cmake %COMMON_OPTIONS% -DPCB=9X %CODE_DIR%
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" radio\src\targets\simu\opentx-simulator.vcxproj /t:Rebuild /p:Configuration=Release
 
-cmake -DCMAKE_PREFIX_PATH=C:\Qt\5.6\msvc2015 -DALLOW_NIGHTLY_BUILDS -DVERSION_SUFFIX=%1 -DPCB=GRUVIN9X %CODE_DIR%
+cmake %COMMON_OPTIONS% -DPCB=GRUVIN9X %CODE_DIR%
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" radio\src\targets\simu\opentx-simulator.vcxproj /t:Rebuild /p:Configuration=Release
 
-cmake -DCMAKE_PREFIX_PATH=C:\Qt\5.6\msvc2015 -DALLOW_NIGHTLY_BUILDS -DVERSION_SUFFIX=%1 -DPCB=MEGA2560 %CODE_DIR%
+cmake %COMMON_OPTIONS% -DPCB=MEGA2560 %CODE_DIR%
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" radio\src\targets\simu\opentx-simulator.vcxproj /t:Rebuild /p:Configuration=Release
 
-cmake -DCMAKE_PREFIX_PATH=C:\Qt\5.6\msvc2015 -DALLOW_NIGHTLY_BUILDS -DVERSION_SUFFIX=%1 -DPCB=SKY9X %CODE_DIR%
+cmake %COMMON_OPTIONS% -DPCB=SKY9X %CODE_DIR%
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" radio\src\targets\simu\opentx-simulator.vcxproj /t:Rebuild /p:Configuration=Release
 
-cmake -DCMAKE_PREFIX_PATH=C:\Qt\5.6\msvc2015 -DALLOW_NIGHTLY_BUILDS -DVERSION_SUFFIX=%1 -DPCB=9XRPRO %CODE_DIR%
+cmake %COMMON_OPTIONS% -DPCB=9XRPRO %CODE_DIR%
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" radio\src\targets\simu\opentx-simulator.vcxproj /t:Rebuild /p:Configuration=Release
 
-cmake -DCMAKE_PREFIX_PATH=C:\Qt\5.6\msvc2015 -DALLOW_NIGHTLY_BUILDS -DVERSION_SUFFIX=%1 -DPCB=TARANIS %CODE_DIR%
+cmake %COMMON_OPTIONS% -DPCB=TARANIS %CODE_DIR%
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" radio\src\targets\simu\opentx-simulator.vcxproj /t:Rebuild /p:Configuration=Release
 
-cmake -DCMAKE_PREFIX_PATH=C:\Qt\5.6\msvc2015 -DALLOW_NIGHTLY_BUILDS -DVERSION_SUFFIX=%1 -DPCB=TARANIS -DPCBREV=REVPLUS %CODE_DIR%
+cmake %COMMON_OPTIONS% -DPCB=TARANIS -DPCBREV=REVPLUS %CODE_DIR%
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" radio\src\targets\simu\opentx-simulator.vcxproj /t:Rebuild /p:Configuration=Release
 
-cmake -DCMAKE_PREFIX_PATH=C:\Qt\5.6\msvc2015 -DALLOW_NIGHTLY_BUILDS -DVERSION_SUFFIX=%1 -DPCB=TARANIS -DPCBREV=REV9E %CODE_DIR%
+cmake %COMMON_OPTIONS% -DPCB=TARANIS -DPCBREV=REV9E %CODE_DIR%
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" radio\src\targets\simu\opentx-simulator.vcxproj /t:Rebuild /p:Configuration=Release
 
-cmake -DCMAKE_PREFIX_PATH=C:\Qt\5.6\msvc2015 -DALLOW_NIGHTLY_BUILDS -DVERSION_SUFFIX=%1 -DPCB=HORUS %CODE_DIR%
+cmake %COMMON_OPTIONS% -DPCB=HORUS %CODE_DIR%
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" radio\src\targets\simu\opentx-simulator.vcxproj /t:Rebuild /p:Configuration=Release
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" companion\src\installer.vcxproj /t:Rebuild /p:Configuration=Release
