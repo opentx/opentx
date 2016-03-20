5e6288b7d1402b656b6852c4072956787622684a
Andre Bernet <andre@andrebernet.ch>
2016-03-20 22:53:19 +0400

    [Horus] LS editing bug

--- radio/src/gui/horus/menu_model_logical_switches.cpp
+++ radio/src/gui/horus/menu_model_logical_switches.cpp
@@ -173,7 +173,7 @@ bool menuModelLogicalSwitches(evt_t event)
         INCDEC_ENABLE_CHECK(NULL);
       }
       v2_max = getMaximumValue(v1_val);
-      v2_min = - v2_min;
+      v2_min = - v2_max;
       putsChannelValue(CSW_3RD_COLUMN, y, v1_val, v1_val <= MIXSRC_LAST_CH ? calc100toRESX(cs->v2) : cs->v2, LEFT|attr2);
     }
 
