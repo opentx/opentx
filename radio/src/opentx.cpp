3c65c32d4586d3e93879f5c95a0851e3eb658505
Andre Bernet <andre@andrebernet.ch>
2016-03-23 16:56:12 +0400

    Wasted flash

--- radio/src/opentx.cpp
+++ radio/src/opentx.cpp
@@ -2525,7 +2525,6 @@ int main(void)
   MCUCR = 0x80 ;   // Must be done twice
 #elif defined(PCBSTD)
   uint8_t mcusr = MCUCSR;
-  MCUCSR = 0;
   MCUCSR = 0x80 ;   // Disable JTAG port that can interfere with POT3
   MCUCSR = 0x80 ;   // Must be done twice
 #endif
