a03f85b0838ffe542ae988efd8ffb6bf6e643c50
Andre Bernet <andre@andrebernet.ch>
2016-04-02 11:06:10 +0400

    [Horus] Switch menu pages on key press

--- radio/src/gui/horus/navigation.cpp
+++ radio/src/gui/horus/navigation.cpp
@@ -412,13 +412,13 @@ bool check(check_event_t event, uint8_t curr, const MenuHandlerFunc * menuTab, u
   if (menuTab && !calibrationState) {
     int cc = curr;
     switch (event) {
-      case EVT_KEY_BREAK(KEY_PGDN):
+      case EVT_KEY_FIRST(KEY_PGDN):
         if (++cc == menuTabSize)
           cc = 0;
         break;
 
 
-      case EVT_KEY_BREAK(KEY_PGUP):
+      case EVT_KEY_FIRST(KEY_PGUP):
         if (cc-- == 0)
           cc = menuTabSize-1;
         break;
