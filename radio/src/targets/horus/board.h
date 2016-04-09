fe9a52779d062e3f24639479273c906abe6e440b
Andre Bernet <andre@andrebernet.ch>
2016-04-09 13:51:28 +0400

    [Horus] Reboot protection reworked to also handle non-WDT events

--- radio/src/targets/horus/board.h
+++ radio/src/targets/horus/board.h
@@ -186,18 +186,30 @@ void checkRotaryEncoder(void);
 
 // WDT driver
 #define WDTO_500MS                            500
-#define wdt_disable()
-void watchdogInit(unsigned int duration);
-#if defined(WATCHDOG_DISABLED) || defined(SIMU)
-  #define wdt_enable(x)
-  #define wdt_reset()
+extern uint32_t powerupReason;
+#define SHUTDOWN_REQUEST                      0xDEADBEEF
+#define NO_SHUTDOWN_REQUEST                   ~SHUTDOWN_REQUEST
+#define DIRTY_SHUTDOWN                        0xCAFEDEAD
+#define NORMAL_POWER_OFF                      ~DIRTY_SHUTDOWN
+#if defined(SIMU)
+  #define WAS_RESET_BY_WATCHDOG()               (false)
+  #define WAS_RESET_BY_SOFTWARE()               (false)
+  #define WAS_RESET_BY_WATCHDOG_OR_SOFTWARE()   (false)
 #else
-  #define wdt_enable(x)                       watchdogInit(x)
-  #define wdt_reset()                         IWDG->KR = 0xAAAA
+  #define wdt_disable()
+  void watchdogInit(unsigned int duration);
+  #if defined(WATCHDOG_DISABLED)
+    #define wdt_enable(x)
+    #define wdt_reset()
+  #else
+    #define wdt_enable(x)                       watchdogInit(x)
+    #define wdt_reset()                         IWDG->KR = 0xAAAA
+  #endif
+  #define WAS_RESET_BY_WATCHDOG()               (RCC->CSR & (RCC_CSR_WDGRSTF | RCC_CSR_WWDGRSTF))
+  #define WAS_RESET_BY_SOFTWARE()               (RCC->CSR & RCC_CSR_SFTRSTF)
+  #define UNREQUESTED_SHUTDOWN()                (shutdownRequest != SHUTDOWN_REQUEST)
+  #define WAS_RESET_BY_WATCHDOG_OR_SOFTWARE()   (RCC->CSR & (RCC_CSR_WDGRSTF | RCC_CSR_WWDGRSTF | RCC_CSR_SFTRSTF))
 #endif
-#define WAS_RESET_BY_WATCHDOG()               (RCC->CSR & (RCC_CSR_WDGRSTF | RCC_CSR_WWDGRSTF))
-#define WAS_RESET_BY_SOFTWARE()               (RCC->CSR & RCC_CSR_SFTRSTF)
-#define WAS_RESET_BY_WATCHDOG_OR_SOFTWARE()   (RCC->CSR & (RCC_CSR_WDGRSTF | RCC_CSR_WWDGRSTF | RCC_CSR_SFTRSTF))
 
 // ADC driver
 enum Analogs {
@@ -237,7 +249,11 @@ void pwrResetHandler(void);
 uint32_t pwrPressed(void);
 uint32_t pwrPressedDuration(void);
 #define pwroffPressed()         pwrPressed()
-#define UNEXPECTED_SHUTDOWN()   (WAS_RESET_BY_WATCHDOG())
+#if defined(SIMU)
+  #define UNEXPECTED_SHUTDOWN()                 (false)
+#else
+  #define UNEXPECTED_SHUTDOWN()                 (powerupReason == DIRTY_SHUTDOWN)
+#endif
 
 // Led driver
 void ledOff(void);
