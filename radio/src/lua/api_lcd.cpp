b06612e0521abca3db67b9d27c97ffa940c809df
3djc <lesitewebdejc@hotmail.com>
2016-05-14 05:12:08 -0700

    Document setColor

--- radio/src/lua/api_lcd.cpp
+++ radio/src/lua/api_lcd.cpp
@@ -561,6 +561,56 @@ static int luaLcdDrawCombobox(lua_State *L)
 #endif
 
 #if defined(COLORLCD)
+/*luadoc
+@function lcd.setColor(area, color)
+
+Set a color for specific area
+
+@param area (unsigned number) specific screen area in the list bellow
+
+ * `CUSTOM_COLOR`
+ * `TEXT_COLOR`
+ * `TEXT_BGCOLOR`
+ * `TEXT_INVERTED_COLOR`
+ * `TEXT_INVERTED_BGCOLOR`
+ * `LINE_COLOR`
+ * `SCROLLBOX_COLOR`
+ * `MENU_TITLE_BGCOLOR`
+ * `MENU_TITLE_COLOR`
+ * `MENU_TITLE_DISABLE_COLOR`
+ * `HEADER_COLOR`
+ * `ALARM_COLOR`
+ * `WARNING_COLOR`
+ * `TEXT_DISABLE_COLOR`
+ * `HEADER_COLOR`
+ * `CURVE_AXIS_COLOR`
+ * `CURVE_CURSOR_COLOR`
+ * `TITLE_BGCOLOR`
+ * `TRIM_BGCOLOR`
+ * `TRIM_SHADOW_COLOR`
+ * `MAINVIEW_PANES_COLOR`
+ * `MAINVIEW_GRAPHICS_COLOR`
+ * `HEADER_BGCOLOR`
+ * `HEADER_ICON_BGCOLOR`
+ * `HEADER_CURRENT_BGCOLOR`
+ * `OVERLAY_COLOR`
+
+@param color (number) color in 5/6/5 rgb format. The following prefined colors are available
+
+ * `WHITE`
+ * `GREY`
+ * `LIGHTGREY`
+ * `DARKGREY`
+ * `BLACK`
+ * `YELLOW`
+ * `BLUE`
+ * `RED`
+ * `DARKRED`
+
+@notice Only available on Horus
+
+@status current Introduced in 2.2.0
+*/
 static int luaLcdSetColor(lua_State *L)
 {
   if (!luaLcdAllowed) return 0;
