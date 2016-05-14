6527ff5036876aae9dbab5dbbb43bd649f0b7555
Bertrand Songis <bsongis@gmail.com>
2016-05-14 10:56:18 +0200

    Travis install time reduced

--- radio/util/bin2lbm.py
+++ radio/util/bin2lbm.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from __future__ import division
 import sys
@@ -9,12 +9,13 @@ fileout = sys.argv[2]
 # Read entire file
 with open(filename, "rb") as fr:
     sts = fr.read()
+
 # Parse into chunks of 16 bytes
 sts = [sts[i:i+16] for i in range(0, len(sts), 16)]
 
 with open(fileout, "w") as fw:
     for st in sts:
         for b in st:
-            fw.write("0x%02x," % ord(b))
+            fw.write("0x%02x," % b)
         fw.write("\n")
     fw.write("\n")
