32576b6f72e0af3d57735bd713116720b6356857
Bertrand Songis <bsongis@gmail.com>
2016-05-19 07:00:27 +0200

    LUA option added to automatic builds

--- radio/util/build-companion.bat
+++ radio/util/build-companion.bat
@@ -10,7 +10,8 @@ rmdir build /s /q
 mkdir build
 cd build
 
-SET COMMON_OPTIONS=-DCMAKE_PREFIX_PATH=C:\Qt\5.6\msvc2015 -DALLOW_NIGHTLY_BUILDS=YES -DVERSION_SUFFIX=%1
+SET COMMON_OPTIONS=-DCMAKE_PREFIX_PATH=C:\Qt\5.6\msvc2015 -DALLOW_NIGHTLY_BUILDS=YES -DVERSION_SUFFIX=%1 -DGVARS=YES -DHELI=YES
+SET STM32_OPTIONS=%COMMON_OPTIONS% -DLUA=YES
 
 cmake %COMMON_OPTIONS% -DPCB=9X %SRCDIR%
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" radio\src\targets\simu\libsimulator.vcxproj /t:Rebuild /p:Configuration=Release
@@ -27,15 +28,15 @@ cmake %COMMON_OPTIONS% -DPCB=SKY9X %SRCDIR%
 cmake %COMMON_OPTIONS% -DPCB=9XRPRO %SRCDIR%
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" radio\src\targets\simu\libsimulator.vcxproj /t:Rebuild /p:Configuration=Release
 
-cmake %COMMON_OPTIONS% -DPCB=TARANIS %SRCDIR%
+cmake %STM32_OPTIONS% -DPCB=TARANIS %SRCDIR%
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" radio\src\targets\simu\libsimulator.vcxproj /t:Rebuild /p:Configuration=Release
 
-cmake %COMMON_OPTIONS% -DPCB=TARANIS -DPCBREV=REVPLUS %SRCDIR%
+cmake %STM32_OPTIONS% -DPCB=TARANIS -DPCBREV=REVPLUS %SRCDIR%
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" radio\src\targets\simu\libsimulator.vcxproj /t:Rebuild /p:Configuration=Release
 
-cmake %COMMON_OPTIONS% -DPCB=TARANIS -DPCBREV=REV9E %SRCDIR%
+cmake %STM32_OPTIONS% -DPCB=TARANIS -DPCBREV=REV9E %SRCDIR%
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" radio\src\targets\simu\libsimulator.vcxproj /t:Rebuild /p:Configuration=Release
 
-cmake %COMMON_OPTIONS% -DPCB=HORUS %SRCDIR%
+cmake %STM32_OPTIONS% -DPCB=HORUS %SRCDIR%
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" radio\src\targets\simu\libsimulator.vcxproj /t:Rebuild /p:Configuration=Release
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" companion\src\installer.vcxproj /t:Rebuild /p:Configuration=Release
