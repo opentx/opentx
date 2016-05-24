d0b3d8f0a8d4b5da1ca075b77b7719c65ee9b6e8
3djc <lesitewebdejc@hotmail.com>
2016-05-24 10:46:08 +0200

    Initial push

--- radio/src/gui/horus/widgets/outputs.cpp
+++ radio/src/gui/horus/widgets/outputs.cpp
@@ -29,33 +29,67 @@ class OutputsWidget: public Widget
     }
 
     virtual void refresh();
+    
+    void drawChannels(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint8_t firstChan, uint8_t lastChan)
+    {
+      char chanString[]="CH32";
+      uint8_t offset =80;
+      
+      for (uint8_t curChan = firstChan; curChan < lastChan + 1; curChan++)
+      {
+        int32_t chanVal = calcRESXto1000(channelOutputs[curChan-1]) / 10;
+        
+        sprintf(chanString,"CH%02d", curChan);
+        lcdDrawText(x, y + (curChan - firstChan) * 20, chanString,SMLSIZE + TEXT_COLOR + LEFT);
+        sprintf(chanString,"%3d", chanVal);
+        lcdDrawText(x + offset - 2, y + (curChan - firstChan) * 20, chanString,SMLSIZE + TEXT_COLOR + RIGHT);
+        lcdDrawRect(x + offset, y + (curChan -firstChan) * 20, w - 90, 18);
+        lcd->drawSolidVerticalLine(x + offset + floor((w - 90) / 2), y + (curChan - firstChan) * 20, 18, MAINVIEW_GRAPHICS_COLOR);
+        if (chanVal > 0)
+        {
+          lcdDrawSolidFilledRect(x + offset + floor((w - 90) / 2),  y + (curChan -firstChan) * 20, (w - 90) * chanVal / 200 , 18, MAINVIEW_GRAPHICS_COLOR);
+        }
+        else if (chanVal < 0)
+        {
+          uint16_t endpoint = x + offset + (w - 90) / 2;
+          uint16_t startpoint = x + offset;
+          uint16_t size = floor(abs((endpoint - startpoint) * chanVal / 100));
+          lcdDrawSolidFilledRect(endpoint - size,  y + (curChan - firstChan) * 20, size, 18, MAINVIEW_GRAPHICS_COLOR);
+        }
+      }
+    };
+
+    void zoneXLarge()
+    {
+      lcd->drawSolidVerticalLine(zone.x + zone.w / 2 - 5, zone.y, zone.h, CUSTOM_COLOR);
+      drawChannels(zone.x, zone.y, floor(zone.w / 2), zone.h, 1, 8);
+      drawChannels(zone.x + floor(zone.w / 2), zone.y, floor(zone.w / 2), zone.h, 9, 16);
+    };
+    
+    void zoneLarge()
+    {
+      drawChannels(zone.x, zone.y, zone.w, zone.h, 1, 8);
+    };
+
+    void zoneSmall()
+    {
+      drawChannels(zone.x, zone.y, zone.w, zone.h, 1, 4);
+    };
+
 
     static const ZoneOption options[];
 };
 
 const ZoneOption OutputsWidget::options[] = {
-  { "Min", ZoneOption::Integer, OPTION_DEFAULT_VALUE_UNSIGNED(0) },
-  { "Max", ZoneOption::Integer, OPTION_DEFAULT_VALUE_UNSIGNED(7) },
-  { "Color", ZoneOption::Color, OPTION_DEFAULT_VALUE_UNSIGNED(RED) },
   { NULL, ZoneOption::Bool }
 };
 
+
 void OutputsWidget::refresh()
 {
-  int firstChannel = MIXSRC_CH1 + persistentData->options[0].unsignedValue;
-  int lastChannel = MIXSRC_CH1 + persistentData->options[1].unsignedValue;
-  uint16_t color = persistentData->options[2].unsignedValue;
-
-  lcdSetColor(color);
-  lcd->drawSolidHorizontalLine(zone.x, zone.y+zone.h/2, zone.w, CUSTOM_COLOR);
-
-  int count = lastChannel - firstChannel + 1;
-
-  for (int ch=firstChannel, i=0; ch<=lastChannel; ch++, i++) {
-    int32_t value = limit<int32_t>(-RESX, getValue(ch), RESX);
-    int h = divRoundClosest((zone.h-1)/2 * value, RESX);
-    lcdDrawSolidFilledRect(zone.x+(zone.w*i)/count, zone.y + zone.h/2, zone.w/count-1, -h, CUSTOM_COLOR);
-  }
+  if (zone.w > 360 and zone.h > 165) OutputsWidget::zoneXLarge();
+  else if (zone.w > 180 and zone.h > 145) OutputsWidget::zoneLarge();
+  else if (zone.w > 170 and zone.h > 65) OutputsWidget::zoneSmall();
 }
 
 BaseWidgetFactory<OutputsWidget> outputsWidget("Outputs", OutputsWidget::options);
