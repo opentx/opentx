554e31a0afa3f57a65ab9cd239c12c8819f273b6
3djc <lesitewebdejc@hotmail.com>
2016-06-21 14:41:11 +0200

    Add proper icons for channels3 and 4

--- radio/src/gui/horus/themes/darkblue.cpp
+++ radio/src/gui/horus/themes/darkblue.cpp
@@ -107,10 +107,10 @@ class DarkblueTheme: public Theme
       loadMenuIcon(ICON_THEME_VIEW5, "mask_theme_view5.png");
       loadMenuIcon(ICON_THEME_ADD_VIEW, "mask_theme_add_view.png");
       loadMenuIcon(ICON_MONITOR, "mask_monitor.png");
-      loadMenuIcon(ICON_MONITOR_OUTPUTS1, "mask_monitor_outputs1.png");
-      loadMenuIcon(ICON_MONITOR_OUTPUTS2, "mask_monitor_outputs2.png");
-      loadMenuIcon(ICON_MONITOR_MIXERS1, "mask_monitor_mixes1.png");
-      loadMenuIcon(ICON_MONITOR_MIXERS2, "mask_monitor_mixes2.png");
+      loadMenuIcon(ICON_MONITOR_CHANNELS1, "mask_monitor_outputs1.png");
+      loadMenuIcon(ICON_MONITOR_CHANNELS2, "mask_monitor_outputs2.png");
+      loadMenuIcon(ICON_MONITOR_CHANNELS3, "mask_monitor_outputs3.png");
+      loadMenuIcon(ICON_MONITOR_CHANNELS4, "mask_monitor_outputs4.png");
       loadMenuIcon(ICON_MONITOR_LOGICAL_SWITCHES, "/mask_monitor_logsw.png");
     }
 
