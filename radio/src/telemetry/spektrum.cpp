8e0aeaf6220063893557f02583d88b7294a93dfa
Arne Schwabe <arne@rfc2549.org>
2016-07-19 17:37:01 +0200

    Minor fixes for Spektrum telemetry

--- radio/src/telemetry/spektrum.cpp
+++ radio/src/telemetry/spektrum.cpp
@@ -42,9 +42,10 @@
 
 
 
-#define I2C_PSEUDO_TX 0x01
+#define I2C_PSEUDO_TX 0xf0
 #define SPEKTRUM_TELEMETRY_LENGTH 18
 
+#define I2C_HIGH_CURRENT 0x03
 #define I2C_CELLS 0x3a
 #define I2C_QOS 0x7f
 
@@ -72,25 +73,25 @@ struct SpektrumSensor {
 
 const SpektrumSensor spektrumSensors[] = {
   // High voltage internal sensor
-  {0x01,      0,  int16,     ZSTR_A1,                UNIT_VOLTS,             1},
+  {0x01,             0,  int16,     ZSTR_A1,                UNIT_VOLTS,             1},
 
   // Temperature internal sensor
-  {0x02,      0,  int16,     ZSTR_TEMP1,             UNIT_CELSIUS,           1},
+  {0x02,             0,  int16,     ZSTR_TEMP1,             UNIT_CELSIUS,           1},
 
-  // High current internal sensor, 300A/2048 resolution
-  {0x03,      0,  int16,     ZSTR_CURR,              UNIT_AMPS,              1},
+  // High current internal sensor (0x03), 300A/2048 resolution
+  {I2C_HIGH_CURRENT, 0,  int16,     ZSTR_CURR,              UNIT_AMPS,              1},
 
   //Powerbox (also mentioned as 0x7D but that is also transmitter frame data)
-  {0x0a,      0,  uint16,    ZSTR_BATT1_VOLTAGE,     UNIT_VOLTS,             2},
-  {0x0a,      2,  uint16,    ZSTR_BATT2_VOLTAGE,     UNIT_VOLTS,             2},
-  {0x0a,      4,  uint16,    ZSTR_BATT1_CONSUMPTION, UNIT_MAH,               0},
-  {0x0a,      6,  uint16,    ZSTR_BATT2_CONSUMPTION, UNIT_MAH,               0},
+  {0x0a,             0,  uint16,    ZSTR_BATT1_VOLTAGE,     UNIT_VOLTS,             2},
+  {0x0a,             2,  uint16,    ZSTR_BATT2_VOLTAGE,     UNIT_VOLTS,             2},
+  {0x0a,             4,  uint16,    ZSTR_BATT1_CONSUMPTION, UNIT_MAH,               0},
+  {0x0a,             6,  uint16,    ZSTR_BATT2_CONSUMPTION, UNIT_MAH,               0},
 
   // AirSpeed, also has max (+2, int16)
-  {0x11,      0,  int16,     ZSTR_ASPD,              UNIT_KMH,               0},
+  {0x11,             0,  int16,     ZSTR_ASPD,              UNIT_KMH,               0},
 
   // Altitude, also has max (+2, int16)
-  {0x12,      0,  int16,     ZSTR_ALT,               UNIT_METERS,            1},
+  {0x12,             0,  int16,     ZSTR_ALT,               UNIT_METERS,            1},
 
   // {0x20, esc}
   // {0x22, fuel/tank}
@@ -99,50 +100,50 @@ const SpektrumSensor spektrumSensors[] = {
   // {0x38, strain}
 
   // G-Force (+min, max)
-  {0x14,      0,  int16,     ZSTR_ACCX,              UNIT_G,                 2},
-  {0x14,      2,  int16,     ZSTR_ACCY,              UNIT_G,                 2},
-  {0x14,      4,  int16,     ZSTR_ACCZ,              UNIT_G,                 2},
+  {0x14,             0,  int16,     ZSTR_ACCX,              UNIT_G,                 2},
+  {0x14,             2,  int16,     ZSTR_ACCY,              UNIT_G,                 2},
+  {0x14,             4,  int16,     ZSTR_ACCZ,              UNIT_G,                 2},
   // 0x15,  JETCAT/TURBINE, BCD Encoded values
 
   // 0x16-0x17 GPS
-  {0x17,      0,  uint16bcd, ZSTR_GSPD,              UNIT_KTS,               1},
+  {0x17,             0,  uint16bcd, ZSTR_GSPD,              UNIT_KTS,               1},
   //{0x17, 2, uint32, ZSTR_GPSDATETIME, UNIT_DATETIME}, utc in bcd HH:MM:SS.S
-  {0x17,      6,  uint8bcd,  ZSTR_SATELLITES,        UNIT_RAW,               0},
+  {0x17,             6,  uint8bcd,  ZSTR_SATELLITES,        UNIT_RAW,               0},
   //{0x17, 7, uint8bcd, ZSTR_GPSALT, UNIT_METERS}, altitude high bits
   // 0x19 Jetcat flow rate
 
   // 0x1a Gyro
-  {0x1a,      0,  int16,     ZSTR_GYROX,             UNIT_DEGREE,            1},
-  {0x1a,      2,  int16,     ZSTR_GYROY,             UNIT_DEGREE,            1},
-  {0x1a,      4,  int16,     ZSTR_GYROZ,             UNIT_DEGREE,            1},
+  {0x1a,             0,  int16,     ZSTR_GYROX,             UNIT_DEGREE,            1},
+  {0x1a,             2,  int16,     ZSTR_GYROY,             UNIT_DEGREE,            1},
+  {0x1a,             4,  int16,     ZSTR_GYROZ,             UNIT_DEGREE,            1},
 
   // 0x1b Attitude & Mag Compass
   // mag Units are tbd so probably no sensor in existance, ignore them for now
-  {0x1b,      0,  int16,     ZSTR_ROLL,              UNIT_DEGREE,            1},
-  {0x1b,      2,  int16,     ZSTR_PITCH,             UNIT_DEGREE,            1},
-  {0x1b,      4,  int16,     ZSTR_YAW,               UNIT_DEGREE,            1},
+  {0x1b,             0,  int16,     ZSTR_ROLL,              UNIT_DEGREE,            1},
+  {0x1b,             2,  int16,     ZSTR_PITCH,             UNIT_DEGREE,            1},
+  {0x1b,             4,  int16,     ZSTR_YAW,               UNIT_DEGREE,            1},
 
   // Cells (0x3a)
-  {I2C_CELLS, 0,  uint16,    ZSTR_CELLS,             UNIT_VOLTS,             2},
-  {I2C_CELLS, 2,  uint16,    ZSTR_CELLS,             UNIT_VOLTS,             2},
-  {I2C_CELLS, 4,  uint16,    ZSTR_CELLS,             UNIT_VOLTS,             2},
-  {I2C_CELLS, 6,  uint16,    ZSTR_CELLS,             UNIT_VOLTS,             2},
-  {I2C_CELLS, 8,  uint16,    ZSTR_CELLS,             UNIT_VOLTS,             2},
-  {I2C_CELLS, 10, uint16,    ZSTR_CELLS,             UNIT_VOLTS,             2},
-  {I2C_CELLS, 12, uint16,    ZSTR_TEMP2,             UNIT_CELSIUS,           2},
+  {I2C_CELLS,        0,  uint16,    ZSTR_CELLS,             UNIT_VOLTS,             2},
+  {I2C_CELLS,        2,  uint16,    ZSTR_CELLS,             UNIT_VOLTS,             2},
+  {I2C_CELLS,        4,  uint16,    ZSTR_CELLS,             UNIT_VOLTS,             2},
+  {I2C_CELLS,        6,  uint16,    ZSTR_CELLS,             UNIT_VOLTS,             2},
+  {I2C_CELLS,        8,  uint16,    ZSTR_CELLS,             UNIT_VOLTS,             2},
+  {I2C_CELLS,        10, uint16,    ZSTR_CELLS,             UNIT_VOLTS,             2},
+  {I2C_CELLS,        12, uint16,    ZSTR_TEMP2,             UNIT_CELSIUS,           2},
 
 
   // Vario-S
-  {0x40,      0,  int16,     ZSTR_ALT,               UNIT_METERS,            1},
-  {0x40,      2,  int16,     ZSTR_VSPD,              UNIT_METERS_PER_SECOND, 1},
+  {0x40,             0,  int16,     ZSTR_ALT,               UNIT_METERS,            1},
+  {0x40,             2,  int16,     ZSTR_VSPD,              UNIT_METERS_PER_SECOND, 1},
 
   // 0x50-0x56 custom 3rd party sensors
   //{0x50, 0, int16, ZSTR_}
 
   // RPM/Volts/Temperature
-  {0x7e,      0,  uint16,    ZSTR_RPM,               UNIT_RPMS,              0},
-  {0x7e,      2,  uint16,    ZSTR_A3,                UNIT_VOLTS,             2},
-  {0x7e,      4,  int16,     ZSTR_TEMP2,             UNIT_FAHRENHEIT,        1},
+  {0x7e,             0,  uint16,    ZSTR_RPM,               UNIT_RPMS,              0},
+  {0x7e,             2,  uint16,    ZSTR_A3,                UNIT_VOLTS,             2},
+  {0x7e,             4,  int16,     ZSTR_TEMP2,             UNIT_FAHRENHEIT,        0},
 
   // 0x7f, QoS DATA, also called Flight Log,, with A, B, L, R, F, H?
   // A - Antenna Fades on Receiver A
@@ -150,25 +151,25 @@ const SpektrumSensor spektrumSensors[] = {
   // L - Antenna Fades on left Receiver
   // R - Antenna Fades on right Receiver
   // F - Frame losses
-  {I2C_QOS,      0,  uint16,    ZSTR_QOS_A,             UNIT_RAW,               0},
-  {I2C_QOS,      2,  uint16,    ZSTR_QOS_B,             UNIT_RAW,               0},
-  {I2C_QOS,      4,  uint16,    ZSTR_QOS_L,             UNIT_RAW,               0},
-  {I2C_QOS,      6,  uint16,    ZSTR_QOS_R,             UNIT_RAW,               0},
-  {I2C_QOS,      8,  uint16,    ZSTR_QOS_F,             UNIT_RAW,               0},
-  {I2C_QOS,      10, uint16,    ZSTR_QOS_H,             UNIT_RAW,               0},
-  {I2C_QOS,      12, uint16,    ZSTR_A2,                UNIT_VOLTS,             2},
+  {I2C_QOS,          0,  uint16,    ZSTR_QOS_A,             UNIT_RAW,               0},
+  {I2C_QOS,          2,  uint16,    ZSTR_QOS_B,             UNIT_RAW,               0},
+  {I2C_QOS,          4,  uint16,    ZSTR_QOS_L,             UNIT_RAW,               0},
+  {I2C_QOS,          6,  uint16,    ZSTR_QOS_R,             UNIT_RAW,               0},
+  {I2C_QOS,          8,  uint16,    ZSTR_QOS_F,             UNIT_RAW,               0},
+  {I2C_QOS,          10, uint16,    ZSTR_QOS_H,             UNIT_RAW,               0},
+  {I2C_QOS,          12, uint16,    ZSTR_A2,                UNIT_VOLTS,             2},
 
-
-  {0,         0,  int16, NULL,                       UNIT_RAW,               0} //sentinel
+  {I2C_PSEUDO_TX,    0,  uint8,     ZSTR_TX_RSSI,           UNIT_RAW,               0},
+  {0,                0,  int16,     NULL,                   UNIT_RAW,               0} //sentinel
 };
 
 // The bcd int parameter has wrong endian
-static int32_t bcdToInt16(unsigned int bcd) {
+static int32_t bcdToInt16(uint16_t bcd) {
   return (bcd & 0x0f00) + 10 * (bcd & 0xf000) + 100 * (bcd & 0x000f) + 1000 * (bcd & 0x00f0);
 }
 
 // The bcd int parameter has wrong endian
-static int32_t bcdToInt8(unsigned int bcd) {
+static int32_t bcdToInt8(uint8_t bcd) {
   return (bcd & 0xf) + 10 * (bcd & 0xf0);
 }
 
@@ -176,7 +177,6 @@ static int32_t bcdToInt8(unsigned int bcd) {
 // Spektrum uses Big Endian data types
 static int32_t spektrumGetValue(uint8_t *packet, int startByte, SpektrumDataType type) {
   uint8_t *data = packet + startByte;
-  int32_t val;
   switch (type) {
     case uint8:
       return *((uint8_t *) (data));
@@ -208,7 +208,7 @@ bool isSpektrumValidValue(int32_t value, const SpektrumDataType type) {
     case int32:
       return value != 0x7fffffff;
     case uint32:
-      return ((uint32_t )value) != 0xffffffff;
+      return ((uint32_t) value) != 0xffffffff;
     default:
       return true;
   }
@@ -216,16 +216,15 @@ bool isSpektrumValidValue(int32_t value, const SpektrumDataType type) {
 
 void processSpektrumPacket(uint8_t *packet) {
 
-  setTelemetryValue(TELEM_PROTO_SPEKTRUM, 0xf100, 0, 0, packet[1], UNIT_DB, 1);
-  uint8_t ic2address = packet[2];
+  setTelemetryValue(TELEM_PROTO_SPEKTRUM, (I2C_PSEUDO_TX << 8) + 1, 0, 0, packet[1], UNIT_RAW, 0);
+  // highest bit indicates that TM1100 is in use, ignore it
+  uint8_t i2cAddress = (packet[2] & 0x7f);
   //uint8_t instance = packet[3];
 
-  // highest bit indicates that TM1100 is in use, ignore it
-  uint8_t instance = (uint8_t) (ic2address & 0x7f);
-
+  uint8_t instance = i2cAddress;
 
   for (const SpektrumSensor *sensor = spektrumSensors; sensor->i2caddress; sensor++) {
-    if (ic2address == sensor->i2caddress) {
+    if (i2cAddress == sensor->i2caddress) {
 
       // Extract value, skip header
       int32_t value = spektrumGetValue(packet + 4, sensor->startByte, sensor->dataType);
@@ -233,15 +232,30 @@ void processSpektrumPacket(uint8_t *packet) {
       if (!isSpektrumValidValue(value, sensor->dataType))
         continue;
 
-      if (ic2address == I2C_CELLS && sensor->unit == UNIT_VOLTS) {
+      if (i2cAddress == I2C_CELLS && sensor->unit == UNIT_VOLTS) {
         // Map to FrSky style cell values
 
         int cellIndex = (sensor->startByte / 2) << 16;
         value = value | cellIndex;
       }
-      
-      if (ic2address == I2C_QOS && sensor->startByte == 0) {
-        telemetryData.rssi.set(value);
+
+      if (sensor->i2caddress == I2C_HIGH_CURRENT && sensor->unit == UNIT_AMPS)
+        // Spektrum's documents talks says: Resolution: 300A/2048 = 0.196791 A/tick
+        // Note that 300/2048 = 0,1464. DeviationTX also uses the 0.196791 figure
+        value = value * 196791 / 100000;
+
+      // Check if this looks like a LemonRX Transceiver, they use QoS Frame loss A as RSSI indicator(0-100)
+      if (i2cAddress == I2C_QOS && sensor->startByte == 0) {
+        if (spektrumGetValue(packet + 4, 2, uint16) == 0x8000 &&
+            spektrumGetValue(packet + 4, 4, uint16) == 0x8000 &&
+            spektrumGetValue(packet + 4, 6, uint16) == 0x8000 &&
+            spektrumGetValue(packet + 4, 8, uint16) == 0x8000) {
+          telemetryData.rssi.set(value);
+        } else {
+          // Otherwise use the received signal strength of the telemetry packet as indicator
+          // Range is 0-31, multiply by 3 to get an almost full reading for 0x1f, the maximum the cyrf chip reports
+          telemetryData.rssi.set(packet[1] * 3);
+        }
         telemetryStreaming = TELEMETRY_TIMEOUT10ms;
       }
 
@@ -305,6 +319,15 @@ void spektrumSetDefault(int index, uint16_t id, uint8_t subId, uint8_t instance)
     if (unit == UNIT_RPMS) {
       telemetrySensor.custom.ratio = 1;
       telemetrySensor.custom.offset = 1;
+    } else if (unit == UNIT_FAHRENHEIT) {
+      if (!IS_IMPERIAL_ENABLE()) {
+        telemetrySensor.unit = UNIT_CELSIUS;
+      }
+
+    } else if (unit == UNIT_METERS) {
+      if (IS_IMPERIAL_ENABLE()) {
+        telemetrySensor.unit = UNIT_FEET;
+      }
     }
   } else {
     telemetrySensor.init(id);
