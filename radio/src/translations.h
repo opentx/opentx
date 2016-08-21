b42d7875fe8b20f24b0da9e6d059c203ca9c39d1
3djc <lesitewebdejc@hotmail.com>
2016-08-21 13:26:55 +0200

    Strings

--- radio/src/translations.h
+++ radio/src/translations.h
@@ -706,6 +706,8 @@ extern const pm_char STR_THROTTLEWARN[];
 extern const pm_char STR_ALARMSWARN[];
 extern const pm_char STR_SWITCHWARN[];
 extern const pm_char STR_FAILSAFEWARN[];
+extern const pm_char STR_SDCARDVERSIONWARN[];
+extern const pm_char STR_WRONG_SDCARDVERSION[];
 extern const pm_char STR_NO_FAILSAFE[];
 extern const pm_char STR_KEYSTUCK[];
 
