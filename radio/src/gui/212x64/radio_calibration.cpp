76300ae92dec70f83d3fa077741b1f73fad26cd0
Bertrand Songis <bsongis@gmail.com>
2016-08-27 09:48:04 +0200

    [X9E] Calibration screen display problem

--- radio/src/gui/212x64/radio_calibration.cpp
+++ radio/src/gui/212x64/radio_calibration.cpp
@@ -36,7 +36,7 @@ enum CalibrationState {
 void drawPotsBars()
 {
   // Optimization by Mike Blandford
-  for (uint8_t x=LCD_W/2-(NUM_POTS+NUM_SLIDERS/2)*BAR_SPACING+BAR_SPACING/2, i=NUM_STICKS; i<NUM_STICKS+NUM_POTS+NUM_SLIDERS; x+=BAR_SPACING, i++) {
+  for (uint8_t x=LCD_W/2-(NUM_POTS+NUM_SLIDERS)/2*BAR_SPACING+BAR_SPACING/2, i=NUM_STICKS; i<NUM_STICKS+NUM_POTS+NUM_SLIDERS; x+=BAR_SPACING, i++) {
     if (IS_POT_OR_SLIDER_AVAILABLE(i)) {
       uint8_t len = ((calibratedStick[i]+RESX)*BAR_HEIGHT/(RESX*2))+1l;  // calculate once per loop
       V_BAR(x, LCD_H-8, len);
