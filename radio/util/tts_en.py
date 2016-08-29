39dbc70765840eb78b2bb09bfaa49c519e73a4e0
3djc <lesitewebdejc@hotmail.com>
2016-08-29 15:46:33 +0200

    Add mintrim

--- radio/util/tts_en.py
+++ radio/util/tts_en.py
@@ -47,7 +47,8 @@ for i, s in enumerate(["point zero", "point one", "point two", "point three",
                        "point seven", "point eight", "point nine"]):
     systemSounds.append((s, filename(PROMPT_SYSTEM_BASE + 167 + i)))
 for s, f, a in [(u"trim center", "midtrim", 495),
-                (u"maximum trim reached", "endtrim", NO_ALTERNATE),
+                (u"maximum trim reached", "maxtrim", NO_ALTERNATE),
+                (u"minium trim reached", "mintrim", NO_ALTERNATE),
                 (u"timer 1 elapsed", "timovr1", NO_ALTERNATE),
                 (u"timer 2 elapsed", "timovr2", NO_ALTERNATE),
                 (u"timer 3 elapsed", "timovr3", NO_ALTERNATE),
