00a756d80fbb65c72fbb3c04582e5fe779e7a92f
Bertrand Songis <bsongis@gmail.com>
2016-09-18 16:03:35 +0200

    Fixes. Still not tested

--- radio/src/targets/horus/board.cpp
+++ radio/src/targets/horus/board.cpp
@@ -89,8 +89,9 @@ void interrupt1ms()
   DEBUG_TIMER_STOP(debugTimerRotEnc);
 
 #if defined(LUA)
-  if (telemetryProtocol == PROTOCOL_FRSKY_SPORT) {
+  if (telemetryProtocol == PROTOCOL_FRSKY_SPORT && outputTelemetryBufferTrigger) {
     if (telemetryFifo.last(-2) == 0x7E && telemetryFifo.last(-1) == outputTelemetryBufferTrigger) {
+      outputTelemetryBufferTrigger = 0;
       sportSendBuffer(outputTelemetryBuffer, outputTelemetryBufferSize);
     }
   }
