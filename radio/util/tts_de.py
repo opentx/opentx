1b961e1beb154f3b8bedfe7b74c1b1ce30b97285
Benjamin Boy <rcbebo82@googlemail.com>
2016-09-30 11:58:40 +0200

    Update tts_de.py
    Comma starts at 103, Units start at 111

--- radio/util/tts_de.py
+++ radio/util/tts_de.py
@@ -14,7 +14,7 @@ systemSounds.append(("ein", filename(PROMPT_SYSTEM_BASE + 100)))
 systemSounds.append(("hundert", filename(PROMPT_SYSTEM_BASE + 101)))
 systemSounds.append(("tausend", filename(PROMPT_SYSTEM_BASE + 102)))
 for i, s in enumerate(["comma", "und", "minus", "uhr", "minute", "minuten", "sekunde", "sekunden"]):
-    systemSounds.append((s, filename(PROMPT_SYSTEM_BASE + 102 + i)))
+    systemSounds.append((s, filename(PROMPT_SYSTEM_BASE + 103 + i)))
 for i, (s, f) in enumerate([(u"Volt","volt0"),
                             (u"Ampere", "amp0"),
                             (u"MilliAmpere", "mamp0"),
@@ -42,7 +42,7 @@ for i, (s, f) in enumerate([(u"Volt","volt0"),
                             (u"Minute", "minute0"), (u"Minuten", "minute1"),
                             (u"Sekunde", "second0"), (u"Sekunden", "second1"),
                             ]):
-    systemSounds.append((s, filename(f, PROMPT_SYSTEM_BASE + 110 + i)))
+    systemSounds.append((s, filename(f, PROMPT_SYSTEM_BASE + 111 + i)))
 for s, f, a in [(u"Inaktivitätsalarm", "inactiv", 486),
                 (u"Senderspannung schwach", "lowbatt", 485),
                 (u"Gaskanal nicht Null, bitte prüfen", "thralert", 481),
