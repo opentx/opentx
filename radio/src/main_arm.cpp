opentx/opentx

commit 7fd412f7195f493935b5a55fa5051e71609e1592

Fixes #3549: disable trim processing in while shutdown is in progress, do no turn of the back-light before the shutdown (#3612)

radio/src/opentx.cpp
@@ -1944,6 +1944,11 @@ void opentxStart()
 #if defined(CPUARM) || defined(CPUM2560)
 void opentxClose()
 {
+#if defined(CPUARM)
+  watchdogSetTimeout(2000/*20s*/);
+#endif
+  pausePulses();   // stop mixer task to disable trims processing while in shutdown
+
   AUDIO_BYE();
 
 #if defined(FRSKY)

radio/src/tasks_arm.cpp
@@ -192,8 +192,6 @@ void menusTask(void * pdata)
   topLcdOff();
 #endif
 
-  BACKLIGHT_OFF();
-
 #if defined(PCBTARANIS)
   displaySleepBitmap();
 #else

commit f38963d6ba6fa2705277760fe031e9ce81a1b046

Fixes #3840: Radio was reset 20s after USB connection in MASSSTORAGE mode (broken in 7fd412f7195, fix ported from a33b1d2659)

radio/src/main_arm.cpp
@@ -64,7 +64,7 @@ void handleUsbConnection()
     */
 
 #if defined(USB_MASS_STORAGE)
-    opentxClose();
+    opentxClose(false);
     usbPluggedIn();
 #endif
   }

radio/src/opentx.cpp
@@ -1941,14 +1941,16 @@ void opentxStart()
 }
 
 #if defined(CPUARM) || defined(CPUM2560)
-void opentxClose()
+void opentxClose(uint8_t shutdown)
 {
 #if defined(CPUARM)
   watchdogSetTimeout(2000/*20s*/);
 #endif
-  pausePulses();   // stop mixer task to disable trims processing while in shutdown
 
-  AUDIO_BYE();
+  if (shutdown) {
+    pausePulses();   // stop mixer task to disable trims processing while in shutdown
+    AUDIO_BYE();
+  }
 
 #if defined(FRSKY)
   // TODO needed? telemetryEnd();

radio/src/opentx.h
@@ -1532,9 +1532,9 @@ extern uint8_t requiredSpeakerVolume;
 extern uint8_t requestScreenshot;
 #endif
 
-extern void checkBattery();
-extern void opentxClose();
-extern void opentxInit();
+void checkBattery();
+void opentxClose(uint8_t shutdown=true);
+void opentxInit();
 
 // Re-useable byte array to save having multiple buffers
 #define SD_SCREEN_FILE_LENGTH (32)