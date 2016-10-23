e580927fc1ae9eb476c884c0b1129e23a004a688
Arne Schwabe <arne@rfc2549.org>
2016-10-23 13:18:26 +0200

    Add opentx os x build vm and allow more cores to be used for fast mac… (#3947)
    * Add opentx os x build vm and allow more cores to be used for fast machines.

--- tools/build-companion.sh
+++ tools/build-companion.sh
@@ -4,11 +4,28 @@
 set -e
 set -x
 
+JOBS=2
+
+while [ $# -gt 0 ]
+do
+  case "$1" in
+    --jobs=*)
+      JOBS="${1#*=}";;
+    -j*)
+      JOBS="${1#*j}";;
+    -*)
+      echo >&2 "usage: $0 [-j<jobs>|--jobs=<jobs>] SRCDIR OUTDIR VERSION_SUFFIX"
+      exit 1;;
+    *)
+      break;;   # terminate while loop
+  esac
+  shift
+done
+
 SRCDIR=$1
 OUTDIR=$2
 
 COMMON_OPTIONS="-DALLOW_NIGHTLY_BUILDS=YES -DVERSION_SUFFIX=$3 -DGVARS=YES -DHELI=YES"
-
 if [ "$(uname)" = "Darwin" ]; then
     COMMON_OPTIONS="${COMMON_OPTIONS} -DCMAKE_PREFIX_PATH=~/Qt/5.7/clang_64/ -DCMAKE_OSX_DEPLOYMENT_TARGET='10.9'"
 fi
@@ -20,42 +37,41 @@ if [ "$3" == "" ]; then
    exit 1
 fi
 
-
 rm -rf build
 mkdir build
 cd build
 
 cmake ${COMMON_OPTIONS} -DPCB=9X ${SRCDIR}
-make -j2 libsimulator
+make -j${JOBS} libsimulator
 
 cmake ${COMMON_OPTIONS} -DPCB=GRUVIN9X ${SRCDIR}
-make -j2 libsimulator
+make -j${JOBS} libsimulator
 
 cmake ${COMMON_OPTIONS} -DPCB=MEGA2560 ${SRCDIR}
-make -j2 libsimulator
+make -j${JOBS} libsimulator
 
 cmake ${COMMON_OPTIONS} -DPCB=SKY9X ${SRCDIR}
-make -j2 libsimulator
+make -j${JOBS} libsimulator
 
 cmake ${COMMON_OPTIONS} -DPCB=9XRPRO ${SRCDIR}
-make -j2 libsimulator
+make -j${JOBS} libsimulator
 
 cmake -DALLOW_NIGHTLY_BUILDS=YES -DVERSION_SUFFIX=$3 -DGVARS=NO -DHELI=YES -DPCB=X7D ${SRCDIR}
-make -j2 libsimulator
+make -j${JOBS} libsimulator
  
 cmake ${STM32_OPTIONS} -DPCB=X9D ${SRCDIR}
-make -j2 libsimulator
+make -j${JOBS} libsimulator
 
 cmake ${STM32_OPTIONS} -DPCB=X9D+ ${SRCDIR}
-make -j2 libsimulator
+make -j${JOBS} libsimulator
 
 cmake ${STM32_OPTIONS} -DPCB=X9E ${SRCDIR}
-make -j2 libsimulator
+make -j${JOBS} libsimulator
 
 cmake ${STM32_OPTIONS} -DPCB=HORUS ${SRCDIR}
-make -j2 libsimulator
+make -j${JOBS} libsimulator
 
-make -j2 package
+make -j${JOBS} package
 
 if [ "$(uname)" = "Darwin" ]; then
     cp *.dmg ${OUTDIR}
