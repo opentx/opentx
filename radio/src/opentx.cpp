opentx/opentx

commit ed07dd76efb5e794572e0ed5f1d8396681a69645

[X7D] Audio volume initialization was not correctly done

radio/src/opentx.cpp
@@ -2456,8 +2456,11 @@ void opentxInit(OPENTX_INIT_ARGS)
   }
 #endif
 
-#if defined(VOICE) && !defined(SOFTWARE_VOLUME)
-  setScaledVolume(g_eeGeneral.speakerVolume+VOLUME_LEVEL_DEF);
+#if defined(VOICE)
+  currentSpeakerVolume = g_eeGeneral.speakerVolume + VOLUME_LEVEL_DEF;
+  #if !defined(SOFTWARE_VOLUME)
+    setScaledVolume(currentSpeakerVolume);
+  #endif
 #endif
 
 #if defined(CPUARM)

radio/src/audio_arm.cpp
@@ -23,10 +23,6 @@
 
 extern OS_MutexID audioMutex;
 
-#if defined(SOFTWARE_VOLUME)
-extern uint8_t currentSpeakerVolume;
-#endif
-
 const int16_t sineValues[] =
 {
     0, 196, 392, 588, 784, 980, 1175, 1370, 1564, 1758,

radio/src/audio_arm.h
@@ -452,6 +452,7 @@ class AudioQueue {
     AudioFragmentFifo fragmentsFifo;
 };
 
+extern uint8_t currentSpeakerVolume;
 extern AudioQueue audioQueue;
 
 enum {

commit 0eba5c4c9c5a587992f43dcc2a528bc0b805aacd

Added missing audio volume initialization (caused by ed07dd76efb5e794572e0ed5f1d8396681a69645)

radio/src/opentx.cpp
@@ -2457,7 +2457,7 @@ void opentxInit(OPENTX_INIT_ARGS)
 #endif
 
 #if defined(VOICE) && defined(CPUARM)
-  currentSpeakerVolume = g_eeGeneral.speakerVolume + VOLUME_LEVEL_DEF;
+  currentSpeakerVolume = requiredSpeakerVolume = g_eeGeneral.speakerVolume + VOLUME_LEVEL_DEF;
   #if !defined(SOFTWARE_VOLUME)
     setScaledVolume(currentSpeakerVolume);
   #endif