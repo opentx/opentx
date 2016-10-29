8f7b3f3736df2e9697f09d9b6996bb885544fb90
3djc <lesitewebdejc@hotmail.com>
2016-10-29 12:10:45 +0200

    Removing magnetic 0 that could feel unnatural to a force to 0 when changing side

--- radio/src/opentx.cpp
+++ radio/src/opentx.cpp
@@ -1300,7 +1300,7 @@ uint8_t checkTrim(event_t event)
     int16_t after = (k&1) ? before + v : before - v;   // positive = k&1
     bool beepTrim = false;
 
-    if (abs(after) <= (trimInc+1)) { // magnetic 0
+    if (abs(after+before) < (abs(after)+abs(before))) { //forcing a stop a centerered trim when changing sides
       after = 0;
     }
     if (!thro && after==0 && before!=0) {
