1b8f8683b25ceb959dc21a517aad344e9769971e
3djc <lesitewebdejc@hotmail.com>
2016-10-29 10:16:23 +0200

    Introduce 'magnetic 0' for trims

--- radio/src/opentx.cpp
+++ radio/src/opentx.cpp
@@ -1300,6 +1300,9 @@ uint8_t checkTrim(event_t event)
     int16_t after = (k&1) ? before + v : before - v;   // positive = k&1
     bool beepTrim = false;
 
+    if (abs(after) <= (trimInc+1)) { // magnetic 0
+      after = 0;
+    }
     if (!thro && after==0 && before!=0) {
       beepTrim = true;
       AUDIO_TRIM_MIDDLE();
