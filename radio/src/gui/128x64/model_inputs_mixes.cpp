1c96d827f80660561cac289c4072a6d9b7e6f9ee
Bertrand Songis <bsongis@gmail.com>
2016-11-07 22:07:22 +0100

    [X7D] Compilation fix

--- radio/src/gui/128x64/model_inputs_mixes.cpp
+++ radio/src/gui/128x64/model_inputs_mixes.cpp
@@ -380,7 +380,7 @@ void drawOffsetBar(uint8_t x, uint8_t y, MixData * md)
   if (barMin <= barMax) {
     int8_t right = (barMax * GAUGE_WIDTH) / 200;
     int8_t left = ((barMin * GAUGE_WIDTH) / 200)-1;
-    lcdDrawFilledRect(x+GAUGE_WIDTH/2+left, y+2, right-left, GAUGE_HEIGHT-3);
+    lcdDrawSolidFilledRect(x+GAUGE_WIDTH/2+left, y+2, right-left, GAUGE_HEIGHT-3);
   }
   lcdDrawSolidVerticalLine(x+GAUGE_WIDTH/2-1, y, GAUGE_HEIGHT+1);
   if (barMin == -101) {
@@ -882,7 +882,7 @@ void menuModelExpoMix(uint8_t expo, event_t event)
             }
             if (cur == sub) {
               /* invert the raw when it's the current one */
-              lcdDrawFilledRect(expo ? EXPO_LINE_SELECT_POS+1 : 23, y, expo ? (LCD_W-EXPO_LINE_SELECT_POS-2) : (LCD_W-24), 7);
+              lcdDrawSolidFilledRect(expo ? EXPO_LINE_SELECT_POS+1 : 23, y, expo ? (LCD_W-EXPO_LINE_SELECT_POS-2) : (LCD_W-24), 7);
             }
           }
         }
