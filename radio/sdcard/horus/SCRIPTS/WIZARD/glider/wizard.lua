0dc5e660de1850f52a2f916176d27d68d9d2c757
3djc <lesitewebdejc@hotmail.com>
2016-11-17 17:58:24 +0100

    cosmetics on the sample wizards

--- radio/sdcard/horus/SCRIPTS/WIZARD/glider/wizard.lua
+++ radio/sdcard/horus/SCRIPTS/WIZARD/glider/wizard.lua
@@ -9,7 +9,7 @@ end
 local function run(event)
   lcd.clear()
   lcd.drawFilledRectangle(0, 0, LCD_W, LCD_H, TEXT_BGCOLOR)
-  lcd.drawText(50, 50, "You are running dummy delta wing wizard 1", TEXT_COLOR)
+  lcd.drawText(50, 50, "You are running dummy glider wizard 1", TEXT_COLOR)
 
   return 1
 end
