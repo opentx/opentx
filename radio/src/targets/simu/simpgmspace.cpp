0b053b67bd58ec7b717a994bdfc146bfe581f7df
Thibault Jouan <tj@a13.fr>
2016-11-19 16:33:29 +0000

    Fix radio firmwares build under FreeBSD

--- radio/src/targets/simu/simpgmspace.cpp
+++ radio/src/targets/simu/simpgmspace.cpp
@@ -760,7 +760,7 @@ char *findTrueFileName(const char *path)
       for (;;) {
         struct simu::dirent * res = simu::readdir(dir);
         if (res == 0) break;
-#if defined(__APPLE__)
+#if defined(__APPLE__) || defined(__FreeBSD__)
         if ((res->d_type == DT_REG) || (res->d_type == DT_LNK)) {
 #else
         if ((res->d_type == simu::DT_REG) || (res->d_type == simu::DT_LNK)) {
@@ -911,7 +911,7 @@ FRESULT f_readdir (DIR * rep, FILINFO * fil)
   simu::dirent * ent = simu::readdir((simu::DIR *)rep->fs);
   if (!ent) return FR_NO_FILE;
 
-#if defined(WIN32) || !defined(__GNUC__) || defined(__APPLE__)
+#if defined(WIN32) || !defined(__GNUC__) || defined(__APPLE__) || defined(__FreeBSD__)
   fil->fattrib = (ent->d_type == DT_DIR ? AM_DIR : 0);
 #else
   if (ent->d_type == simu::DT_UNKNOWN) {
