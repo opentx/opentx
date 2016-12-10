86b85aabc53d14d93e8c4f0c87619c0add93f8b8
Andre Bernet <kilrah@andrebernet.ch>
2016-12-10 18:44:09 +0100

    Disable rotary encoder rotation while pressed (will hopefully prevent some of the common unwanted edits when pressing, thanks 3djc for the idea)

--- radio/src/targets/horus/keys_driver.cpp
+++ radio/src/targets/horus/keys_driver.cpp
@@ -94,7 +94,7 @@ uint8_t keyDown()
 void checkRotaryEncoder()
 {
   uint32_t newpos = ROTARY_ENCODER_POSITION();
-  if (newpos != rotencPosition) {
+  if (newpos != rotencPosition && !keyState(KEY_ENTER)) {
     if ((rotencPosition & 0x01) ^ ((newpos & 0x02) >> 1)) {
       --rotencValue[0];
     }
--- radio/src/targets/taranis/keys_driver.cpp
+++ radio/src/targets/taranis/keys_driver.cpp
@@ -89,7 +89,7 @@ uint8_t keyDown()
 void checkRotaryEncoder()
 {
   uint32_t newpos = ROTARY_ENCODER_POSITION();
-  if (newpos != rotencPosition) {
+  if (newpos != rotencPosition && !keyState(KEY_ENTER)) {
     if ((rotencPosition & 0x01) ^ ((newpos & 0x02) >> 1)) {
       --rotencValue[0];
     }
