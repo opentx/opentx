c675a0648163c0e0261d26909612cbb89b7658dc
3djc <lesitewebdejc@hotmail.com>
2017-01-01 11:46:18 +0100

    Only apply to stdsize

--- radio/util/font2png.py
+++ radio/util/font2png.py
@@ -91,12 +91,9 @@ def createFontBitmap(filename, fontname, fontsize, fontbold, foreground, backgro
             pass
         elif c == "." and fontsize <= 8:
             painter.drawPoint(width, fontsize)
-        elif c == u"↑" or c == u"↓":
+        elif (c == u"↑" or c == u"↓") and fontsize == 16:
             rect = metrics.boundingRect(c)
-            if fontsize >= 24:
-                painter.drawText(width - rect.left() + 1, fontsize - 2, c)  # fontsize-bottom+1 -17 / 7
-            else:
-                painter.drawText(width - 1, fontsize, c)  # fontsize-bottom+1 -17 / 7            
+            painter.drawText(width - 1, fontsize, c)  # fontsize-bottom+1 -17 / 7
         else:
             rect = metrics.boundingRect(c)
             if fontsize >= 24:
