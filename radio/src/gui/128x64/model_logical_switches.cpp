3afb76beda0ab50ae35ade4364f4044842ea243f
3djc <lesitewebdejc@hotmail.com>
2017-01-06 13:43:11 +0100

    X7 limits where wrong : for exemple, if you not set a compare value bigger than 15 in a logicial switch.
    This fix it and also takes into account some radio settings (like extended limits)

--- radio/src/gui/128x64/model_logical_switches.cpp
+++ radio/src/gui/128x64/model_logical_switches.cpp
@@ -2,7 +2,7 @@
  * Copyright (C) OpenTX
  *
  * Based on code named
- *   th9x - http://code.google.com/p/th9x 
+ *   th9x - http://code.google.com/p/th9x
  *   er9x - http://code.google.com/p/er9x
  *   gruvin9x - http://code.google.com/p/gruvin9x
  *
@@ -63,10 +63,10 @@ void menuModelLogicalSwitchOne(event_t event)
   TITLE(STR_MENULOGICALSWITCH);
 
   LogicalSwitchData * cs = lswAddress(s_currIdx);
-  
+
   uint8_t sw = SWSRC_SW1+s_currIdx;
   uint8_t cstate = lswFamily(cs->func);
-  
+
   drawSwitch(14*FW, 0, sw, (getSwitch(sw) ? BOLD : 0));
 
   SUBMENU_NOTITLE(LS_FIELD_COUNT, { 0, 0, uint8_t(cstate == LS_FAMILY_EDGE ? 1 : 0), 0 /*, 0...*/ });
@@ -79,7 +79,7 @@ void menuModelLogicalSwitchOne(event_t event)
     coord_t y = MENU_HEADER_HEIGHT + 1 + k*FH;
     uint8_t i = k + menuVerticalOffset;
     uint8_t attr = (sub==i ? (s_editMode>0 ? BLINK|INVERS : INVERS) : 0);
-    
+
     switch (i) {
       case LS_FIELD_FUNCTION:
         lcdDrawTextAlignedLeft(y, STR_FUNC);
@@ -100,7 +100,7 @@ void menuModelLogicalSwitchOne(event_t event)
           }
         }
         break;
-        
+
       case LS_FIELD_V1:
       {
         INCDEC_DECLARE_VARS(EE_MODEL);
@@ -128,7 +128,7 @@ void menuModelLogicalSwitchOne(event_t event)
         }
         break;
       }
-      
+
       case LS_FIELD_V2:
       {
         INCDEC_DECLARE_VARS(EE_MODEL);
@@ -183,7 +183,7 @@ void menuModelLogicalSwitchOne(event_t event)
           else
 #endif
           {
-            v2_min = -LIMIT_EXT_PERCENT; v2_max = +LIMIT_EXT_PERCENT;
+            v2_max = calc100toRESX(getMaximumValue(v1_val)); v2_min = -v2_max;
             drawSourceCustomValue(CSWONE_2ND_COLUMN, y, v1_val, cs->v2, LEFT|attr);
           }
         }
@@ -193,13 +193,13 @@ void menuModelLogicalSwitchOne(event_t event)
         }
         break;
       }
-      
+
       case LS_FIELD_ANDSW:
         lcdDrawTextAlignedLeft(y, STR_AND_SWITCH);
         drawSwitch(CSWONE_2ND_COLUMN, y, cs->andsw, attr);
         if (attr) CHECK_INCDEC_MODELVAR_CHECK(event, cs->andsw, -MAX_LS_ANDSW, MAX_LS_ANDSW, isSwitchAvailableInLogicalSwitches);
         break;
-        
+
       case LS_FIELD_DURATION:
         lcdDrawTextAlignedLeft(y, STR_DURATION);
         if (cs->duration > 0)
@@ -208,7 +208,7 @@ void menuModelLogicalSwitchOne(event_t event)
           lcdDrawTextAtIndex(CSWONE_2ND_COLUMN, y, STR_MMMINV, 0, attr);
         if (attr) CHECK_INCDEC_MODELVAR_ZERO(event, cs->duration, MAX_LS_DURATION);
         break;
-        
+
       case LS_FIELD_DELAY:
         lcdDrawTextAlignedLeft(y, STR_DELAY);
         if (cs->delay > 0)
