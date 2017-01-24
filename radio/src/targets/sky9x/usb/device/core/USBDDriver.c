8f970fdbbf6535b383453e14f0b50630e639c961
Bertrand Songis <bsongis@gmail.com>
2017-01-24 18:57:45 +0100

    [9XR-PRO] Warning fixed (ATMEL code)

--- radio/src/targets/sky9x/usb/device/core/USBDDriver.c
+++ radio/src/targets/sky9x/usb/device/core/USBDDriver.c
@@ -512,10 +512,12 @@ void USBDDriver_Initialize(
     pDriver->pDescriptors = pDescriptors;
     pDriver->pInterfaces = pInterfaces;
 
+#if 0
     // Initialize interfaces array if not null
     if (pInterfaces != 0) {
-        memset(pInterfaces, 0, sizeof(pInterfaces));
+      memset(pInterfaces, sizeof(*pInterfaces), 0); // Really wrong, but never happens :)
     }
+#endif
 }
 
 //------------------------------------------------------------------------------
