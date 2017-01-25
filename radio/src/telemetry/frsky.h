6064ea457844b6a5f805acdcd7b5aaa0abd2dcf8
Bertrand Songis <bsongis@gmail.com>
2017-01-25 08:21:33 +0100

    [S.PORT] DIY range updated. The spec should follow soon

--- radio/src/telemetry/frsky.h
+++ radio/src/telemetry/frsky.h
@@ -139,7 +139,7 @@
 #define RBOX_STATE_LAST_ID        0x0b2f
 #define RBOX_CNSP_FIRST_ID        0x0b30
 #define RBOX_CNSP_LAST_ID         0x0b3f
-#define DIY_FIRST_ID              0x5100
+#define DIY_FIRST_ID              0x5000
 #define DIY_LAST_ID               0x52ff
 #define RSSI_ID                   0xf101
 #define ADC1_ID                   0xf102
