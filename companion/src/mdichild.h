4c5ee513c1ebb867c6b506a722834aa9ca0f9ec9
kellybyrd <kellybyrd@users.noreply.github.com>
2017-02-01 09:04:01 -0800

    Issue #4224: Implement "duplicate model" in Companion
    Just what it says in the title, implement the "duplicate model" action in
    Companion's context menu.
    
    Also, fixed "modelAdd" so that it only sets the default model if it just
    added the first one. Previously, the newly added model would become the
    default, which seemed like the wrong thing.

--- companion/src/mdichild.h
+++ companion/src/mdichild.h
@@ -79,6 +79,7 @@ class MdiChild : public QWidget
     void categoryDelete();
     void modelAdd();
     void modelEdit();
+    void modelDuplicate();
     void wizardEdit();
     void openModelEditWindow();
     bool loadBackup();
