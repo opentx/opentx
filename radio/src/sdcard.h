98f6523d4af010ef9cb874d65470fd7fa1031353
3djc <lesitewebdejc@hotmail.com>
2017-02-10 22:23:52 +0100

    Bring constant in line with naming convention

--- radio/src/sdcard.h
+++ radio/src/sdcard.h
@@ -69,7 +69,7 @@ const char RADIO_SETTINGS_PATH[] = RADIO_PATH "/radio.bin";
 
 #if defined(PCBHORUS)
 #define BITMAPS_EXT         BMP_EXT JPG_EXT PNG_EXT
-#define BITMAPS_EXT_LEN     4
+#define LEN_BITMAPS_EXT     4
 #else
 #define BITMAPS_EXT         BMP_EXT
 #endif
