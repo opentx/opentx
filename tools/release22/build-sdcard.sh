52e65179a8e91c91407439110c035a56df509d3f
3djc <lesitewebdejc@hotmail.com>
2017-03-21 07:43:13 +0100

    Tools for 2.2 release handling (#4649)
    * Scripts
    
    * No suffix for docker image
    
    * generate sdcard file to subdirectories to match companion changes
    
    * Massively increase options for simus
    backup lua_fields info for lua docs
    
    * Allow both nightly and release to be built
    
    * $ ambiguity
    
    * Cleanup
    
    * Tidy up
    
    * Cosmetics

--- tools/release22/build-sdcard.sh
+++ tools/release22/build-sdcard.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-branch=next
-workdir=/home/opentx/nightly22
-output=/var/www/html/2.2/nightly
+branch=master
+workdir=/home/opentx/release22
+output=/var/www/html/2.2
 
 # Handle opentx.sdcard.version
 sdcard_version="2.2V"$(grep 'set(SDCARD_REVISION' ${workdir}/code/CMakeLists.txt | grep -o '".*"' | sed 's/"//g')
@@ -28,11 +28,11 @@ else
   cp /home/opentx/horus-bitmaps/* ${workdir}/sdcard/horus/IMAGES/
 
   # Request sound pack generation
-  python3 -B ${workdir}/code/tools/nightly22/tts.py en csv files
-  python3 -B ${workdir}/code/tools/nightly22/tts.py fr csv files
-  python3 -B ${workdir}/code/tools/nightly22/tts.py es csv files
-  python3 -B ${workdir}/code/tools/nightly22/tts.py it csv files
-  python3 -B ${workdir}/code/tools/nightly22/tts.py de csv files
+  python3 -B ${workdir}/code/tools/release22/tts.py en csv files
+  python3 -B ${workdir}/code/tools/release22/tts.py fr csv files
+  python3 -B ${workdir}/code/tools/release22/tts.py es csv files
+  python3 -B ${workdir}/code/tools/release22/tts.py it csv files
+  python3 -B ${workdir}/code/tools/release22/tts.py de csv files
 
   # Create sdcards.zips for supported platforms
   mv /tmp/SOUNDS ${workdir}/sdcard/horus/
@@ -40,8 +40,8 @@ else
   mkdir ${workdir}/sdcard/taranis-x7/SOUNDS
   cp -r ${workdir}/sdcard/horus/SOUNDS ${workdir}/sdcard/taranis-x9/
   cp -r ${workdir}/sdcard/horus/SOUNDS ${workdir}/sdcard/taranis-x7/
-  cd ${workdir}/sdcard/horus && zip -r ${output}/sdcard/sdcard-horus-${sdcard_version}.zip *
-  cd ${workdir}/sdcard/taranis-x9 && zip -r ${output}/sdcard/sdcard-taranis-x9-${sdcard_version}.zip *
-  cd ${workdir}/sdcard/taranis-x7 && zip -r ${output}/sdcard/sdcard-taranis-x7-${sdcard_version}.zip *
+  cd ${workdir}/sdcard/horus && zip -r ${output}/sdcard/x12/sdcard-horus-${sdcard_version}.zip *
+  cd ${workdir}/sdcard/taranis-x9 && zip -r ${output}/sdcard/x9/sdcard-taranis-x9-${sdcard_version}.zip *
+  cd ${workdir}/sdcard/taranis-x7 && zip -r ${output}/sdcard/x7/sdcard-taranis-x7-${sdcard_version}.zip *
   rm -Rf ${workdir}/sdcard
 fi
