bd7d6be44f406813ddf2b1343a42bd09a3c13b88
Arne Schwabe <arne@rfc2549.org>
2017-03-29 12:29:59 +0200

    Add DM002 multipl protocol

--- radio/src/gui/gui_common_arm.cpp
+++ radio/src/gui/gui_common_arm.cpp
@@ -674,7 +674,6 @@ const mm_protocol_definition multi_protocols[] = {
   { MM_RF_PROTO_Q2X2,       STR_SUBTYPE_Q2X2,     2,  nullptr             },
   { MM_RF_PROTO_WK_2X01,    STR_SUBTYPE_WK2x01,   5,  nullptr             },
   { MM_RF_PROTO_Q303,       STR_SUBTYPE_Q303,     3,  nullptr             },
-  { MM_RF_PROTO_GW08,       NO_SUBTYPE,           0,  nullptr             },
   { MM_RF_CUSTOM_SELECTED,  NO_SUBTYPE,           7,  STR_MULTI_OPTION    },
 
   //Sential and default for protocols not listed above (MM_RF_CUSTOM is 0xff()
