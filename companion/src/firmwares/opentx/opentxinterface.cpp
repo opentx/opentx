c2c70ec4834aaed52e2f4f2482e1f30f8cd0675a
3djc <lesitewebdejc@hotmail.com>
2017-03-31 18:26:03 +0200

    Use existing function

--- companion/src/firmwares/opentx/opentxinterface.cpp
+++ companion/src/firmwares/opentx/opentxinterface.cpp
@@ -254,9 +254,7 @@ unsigned long OpenTxEepromInterface::load(RadioData &radioData, const uint8_t * 
   if (version_error == OLD_VERSION) {
     errors.set(version_error);
     errors.set(HAS_WARNINGS);
-    QMessageBox::warning(NULL,
-                         QObject::tr("Warning"),
-                         QT_TRANSLATE_NOOP("EepromInterface", "- Your eeprom is from an old version of OpenTX, upgrading!\n You should 'save as' to keep the old file as a backup."));
+    ShowEepromWarnings(NULL, QObject::tr("Warning"), errors.to_ulong());
   }
   else if (version_error == NOT_OPENTX) {
     std::cout << " not open9x\n";
