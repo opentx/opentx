d35d3b5e225d47684fafb3292155b4e17958a591
Damjan Adamic <projectkk2glider@gmail.com>
2017-05-13 14:02:59 +0200

    Prepare Launchpad scripts for non-nightly release

--- launchpad/prepare.sh
+++ launchpad/prepare.sh
@@ -3,8 +3,29 @@ set -e
 set -x
 
 # $1 is distribution "trusty" OR "yakkety"
+#
+# Option "-n" or "--nightly" can be used to configure build script to produce a Companion with DALLOW_NIGHTLY_BUILDS=YES flag
+
 
 cd `dirname ${0}`
+
+nightly_build=0
+
+while [ $# -gt 0 ]
+do
+  case "$1" in
+    --nightly*)
+      nightly_build=1;;
+    -n*)
+      nightly_build=1;;
+    -*)
+      echo >&2 "usage: $0 [-n|--nightly] DISTRO"
+      exit 1;;
+    *)
+      break;;   # terminate while loop
+  esac
+  shift
+done
 
 
 distro=${1%/}
@@ -32,10 +53,19 @@ cp ${distro}/* ../debian/
 echo "Editing rules to set version suffix to \"${suffix}\""
 sed -i "s/__VERSION_SUFFIX__/${suffix}/g" ../debian/rules
 
+if [ ${nightly_build} -gt 0 ] ; then
+  echo "Setting nightly build flag"
+  sed -i "s/DALLOW_NIGHTLY_BUILDS=YES/DALLOW_NIGHTLY_BUILDS=YES/g" ../debian/rules
+else
+  sed -i "s/DALLOW_NIGHTLY_BUILDS=YES/DALLOW_NIGHTLY_BUILDS=NO/g" ../debian/rules
+fi
+
 # remove old package and prepare new one
 cd ..
 rm -f  ../opentx-companion22_*${distro}*
 debuild -S -sa -I -Ijenkins -Ilaunchpad -Idoc -Ibuild -I*.luac
 
-echo "Use this command to upload to launchpad:"
+echo "Use this command to upload to launchpad (nightly builds):"
 echo "  dput ppa:opentx-test/ppa opentx-companion22_<version>.changes"
+echo "OR releases:"
+echo "  dput ppa:opentx-test/rel opentx-companion22_<version>.changes"
