bcb1d02d715f37a60ea7fb7970ce3d1c9313fd1d
Arne Schwabe <arne@rfc2549.org>
2017-06-18 16:04:11 +0200

    Display XJT version

--- radio/src/gui/212x64/radio_diaganas.cpp
+++ radio/src/gui/212x64/radio_diaganas.cpp
@@ -45,5 +45,8 @@ void menuRadioDiagAnalogs(event_t event)
   if((IS_MODULE_XJT(INTERNAL_MODULE) && IS_INTERNAL_MODULE_ON()) || (IS_MODULE_XJT(EXTERNAL_MODULE) && !IS_INTERNAL_MODULE_ON())) {
     lcdDrawTextAlignedLeft(MENU_HEADER_HEIGHT+6*FH, "RAS");
     lcdDrawNumber(10*FW-1, MENU_HEADER_HEIGHT+6*FH, telemetryData.swr.value, RIGHT);
+    lcdDrawText(LCD_W/2, MENU_HEADER_HEIGHT+6*FH, "XJTVER");
+    lcdDrawNumber(LCD_W/2 + 10*FW-1, MENU_HEADER_HEIGHT+6*FH, telemetryData.xjtVersion, RIGHT);
+
   }
 }
