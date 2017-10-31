248445781bbd8a0ee7f7da4bc74052368bda683b
RCdiy <info@rcdiy.ca>
2017-10-31 15:55:41 -0400

    Update companion_en.ts

--- companion/src/translations/companion_en.ts
+++ companion/src/translations/companion_en.ts
@@ -7607,7 +7607,7 @@ If blank then the mix is considered to be &quot;ON&quot; all the time.</source>
     </message>
     <message>
         <location filename="../firmwares/opentx/opentxinterface.cpp" line="1155"/>
-        <source>Enable Lua custom scripts screen</source>
+        <source>Support for Lua model scripts</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
