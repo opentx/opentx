6208909438306e16d234687911a66cfda4991edb
Bertrand Songis <bsongis@gmail.com>
2017-11-12 09:32:14 +0100

    Nonreg tests

--- radio/src/tests/mixer.cpp
+++ radio/src/tests/mixer.cpp
@@ -488,7 +488,7 @@ TEST_F(MixerTest, CascadedOrderedChannels)
   g_model.mixData[1].destCh = 1;
   g_model.mixData[1].srcRaw = MIXSRC_CH1;
   g_model.mixData[1].weight = 100;
-  simuSetSwitch(0, 1);
+  simuSetSwitch(1, 1);
   evalFlightModeMixes(e_perout_mode_normal, 0);
   EXPECT_EQ(chans[0], CHANNEL_MAX);
   EXPECT_EQ(chans[1], CHANNEL_MAX);
