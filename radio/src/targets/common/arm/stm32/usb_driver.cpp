0075a647309fa6ec3586d1ff0ae4387c5993730d
unknown <git@y-richter.de>
2018-04-12 18:30:59 +0200

    Changed USB Gamepad resolution to full 11 bit and fixed offset issues for some games

--- radio/src/targets/common/arm/stm32/usb_driver.cpp
+++ radio/src/targets/common/arm/stm32/usb_driver.cpp
@@ -151,10 +151,13 @@ void usbJoystickUpdate()
     //analog values
     //uint8_t * p = HID_Buffer + 1;
     for (int i = 0; i < 8; ++i) {
-      int16_t value = channelOutputs[i] / 8;
-      if ( value > 127 ) value = 127;
-      else if ( value < -127 ) value = -127;
-      HID_Buffer[i+3] = static_cast<int8_t>(value);
+
+      int16_t value = channelOutputs[i] + 1023;
+      if ( value > 2047 ) value = 2047;
+      else if ( value < 0 ) value = 0;
+      HID_Buffer[i*2 +3] = static_cast<uint8_t>(value & 0xFF);
+      HID_Buffer[i*2 +4] = static_cast<uint8_t>((value >> 8) & 0x07);
+
     }
     USBD_HID_SendReport(&USB_OTG_dev, HID_Buffer, HID_IN_PACKET);
   }
