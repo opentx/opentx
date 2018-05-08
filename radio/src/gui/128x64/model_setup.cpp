9a7c93306bb6166517201a91b2976cab35bfa0e3
Bertrand Songis <bsongis@gmail.com>
2018-05-08 22:33:23 +0200

    Previous fix broke xlite navigation

--- radio/src/gui/128x64/model_setup.cpp
+++ radio/src/gui/128x64/model_setup.cpp
@@ -1139,7 +1139,11 @@ void menuModelSetup(event_t event)
               if (s_editMode > 0) {
                 if (l_posHorz == 1) {
                   if (IS_MODULE_R9M(moduleIdx) || (IS_MODULE_XJT(moduleIdx) && g_model.moduleData[moduleIdx].rfProtocol== RF_PROTO_X16)) {
+#if defined(PCBXLITE)
+                    if (EVT_KEY_MASK(event) == KEY_ENTER) {
+#else
                     if (event == EVT_KEY_BREAK(KEY_ENTER)) {
+#endif
                       killEvents(event);
                       uint8_t default_selection;
                       if (IS_MODULE_R9M_LBT(moduleIdx)) {
