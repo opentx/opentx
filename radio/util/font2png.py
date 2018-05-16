83a603647226be1fe2fff053b09f343cc78a715a
Raphael Coeffic <rco@iptel.org>
2018-05-16 23:03:08 +0200

    try Roboto

--- radio/util/font2png.py
+++ radio/util/font2png.py
@@ -6,7 +6,7 @@ import glob
 
 from PIL import Image, ImageDraw, ImageFont
 
-chars_en = u""" !"#$%&'()*+,-./0123456789:;<=>?°ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz~|≥→←↑↓↗↘↙↖△"""
+chars_en = u""" !"#$%&'()*+,-./0123456789:;<=>?°ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz~|≥"""
 chars_fr = u"""éèàîç"""
 chars_de = u"""ÄäÖöÜüß"""
 chars_cz = u"""ěščřžýáíéňóůúďťĚŠČŘŽÝÁÍÉŇÓÚŮĎŤ"""
@@ -17,7 +17,7 @@ chars_pl = u"""ąćęłńóśżźĄĆĘŁŃÓŚŻŹ"""
 chars_pt = u"""ÁáÂâÃãÀàÇçÉéÊêÍíÓóÔôÕõÚú"""
 chars_se = u"""åäöÅÄÖ"""
 
-COUNT_EXTRA_CHARS = 12
+COUNT_EXTRA_CHARS = 21
 
 chars_extra = u"".join([chr(1+i) for i in range(COUNT_EXTRA_CHARS)])
 chars = chars_en + chars_extra + chars_fr + chars_de + chars_cz + chars_es + chars_fi + chars_it + chars_pl + chars_se
@@ -25,9 +25,12 @@ chars = chars_en + chars_extra + chars_fr + chars_de + chars_cz + chars_es + cha
 
 def createFontBitmap(filename, fontname, fontsize, fontbold, foreground, background, coordsfile=True):
 
-    font_filename = 'fonts/DejaVuSansCondensed'
+    font_filename = 'fonts/' + fontname + '/' + fontname
     if fontbold:
         font_filename = font_filename + '-Bold'
+    else:
+        font_filename = font_filename + '-Regular'
+
     font_filename = font_filename + '.ttf'
 
     coords = []
@@ -50,14 +53,14 @@ def createFontBitmap(filename, fontname, fontsize, fontbold, foreground, backgro
 
     def getCharWidth(c):
         if c in chars_extra:
-            return extraWidth
+            if ord(c) <= 9:
+                return 13
+            else:
+                return 15
         elif c == " ":
             return 4
         else:
             w, h = font.getsize(c)
-            #if fontsize >= 24:
-            #    return w + 3
-            #else:
             return w + 1
 
     def getFontWidth():
@@ -66,16 +69,7 @@ def createFontBitmap(filename, fontname, fontsize, fontbold, foreground, backgro
             width += getCharWidth(c)
         return width
 
-    # def getFontTopBottom():
-    #     top, bottom = 0, 0
-    #     for i, c in enumerate(chars):
-    #         w, h = font.getsize(chars[i])
-    #         top = min(top, r.top())
-    #         bottom = max(bottom, r.bottom())
-    #     return top, bottom
-
     width = getFontWidth()
-    # top, bottom = getFontTopBottom()
     image = Image.new('RGB', (width, fontsize + 4), background)
     draw  = ImageDraw.Draw(image)
 
@@ -85,14 +79,10 @@ def createFontBitmap(filename, fontname, fontsize, fontbold, foreground, backgro
 
         if c in chars_extra:
             if extraImage and c == chars_extra[0]:
-                for x in range(extraWidth * COUNT_EXTRA_CHARS):
-                    for y in range(extraHeight):
-                        rgb = extraImage.getpixel((x, y))
-                        draw.point([x+width, y], fill=rgb)
+                image.paste(extraImage.copy(),(width,1))
         elif c == " ":
             pass
         else:
-            #(0,0), chars, fill=(0,0,0), font=font
             w, h = font.getsize(c)
             draw.text( (width + 1, 1) , c, fill=foreground, font=font)
 
@@ -105,7 +95,7 @@ def createFontBitmap(filename, fontname, fontsize, fontbold, foreground, backgro
         with open(filename + ".specs", "w") as f:
             f.write(",".join(str(tmp) for tmp in coords))
 
-    return coords
 
+# Main
 if __name__ == "__main__":
     createFontBitmap(sys.argv[4], sys.argv[1], int(sys.argv[2]), sys.argv[3] == "True", (0,0,0), (255,255,255))
