526f460cd41e8b7c32c50d63f11a3eb136a9623d
Raphael Coeffic <raphaelcoeffic@users.noreply.github.com>
2018-05-21 10:45:13 +0200

    Generate font bitmaps without Qt (using Python Pillow) (#5891)
    * Generate font bitmaps without Qt (using Python Pillow)
    * Roboto font now default

--- cmake/Bitmaps.cmake
+++ cmake/Bitmaps.cmake
@@ -14,10 +14,10 @@ macro(add_bitmaps_target targetname filter width format)
   add_custom_target(${targetname} DEPENDS ${bitmaps_files})
 endmacro(add_bitmaps_target)
 
-macro(add_truetype_font_target radio name font size bold)
+macro(add_truetype_font_target radio name font size offset)
   set(target ${RADIO_SRC_DIRECTORY}/fonts/${radio}/font_${name})
   add_custom_target(ttf_${radio}_${name}
-    COMMAND ${PYTHON_EXECUTABLE} ${RADIO_DIRECTORY}/util/font2png.py ${font} ${size} ${bold} ${target}
+    COMMAND ${PYTHON_EXECUTABLE} ${RADIO_DIRECTORY}/util/font2png.py ${font} ${size} ${offset} ${target}
     WORKING_DIRECTORY ${RADIO_SRC_DIRECTORY}
   )
   set(ttf_${radio}_fonts_targets ${ttf_${radio}_fonts_targets} ttf_${radio}_${name})
