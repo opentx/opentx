793e971460d09c2ba2bab8b9a09d9cd3e6b62a74
3djc <lesitewebdejc@hotmail.com>
2018-06-23 10:22:12 +0200

    early release prep

--- tools/release22/build-release.sh
+++ tools/release22/build-release.sh
@@ -5,7 +5,7 @@ set -e
 branch=2.2
 docker=release22
 workdir=/home/opentx/release22
-output=/var/www/html/2.2
+output=/var/www/html/2.2/release
 version=2.2.2
 
 cd ${workdir}
@@ -24,7 +24,7 @@ code/tools/release22/build-sdcard.sh
 docker run -dit --name companion -v /home/opentx/${docker}:/opentx ${docker}
 docker exec companion sh -c "mkdir -p build && cd build && cmake /opentx/code && cp radio/src/stamp.h /opentx/binaries/stamp-opentx.txt"
 docker exec companion rm -rf build
-if [ ! -f ${output}/companion/linux/companion22_${version}${suffix}_amd64.deb ]; then
+if [ ! -f ${output}/companion/linux/companion22_${version}_amd64.deb ]; then
   docker exec companion /opentx/code/tools/build-companion-release.sh /opentx/code /opentx/binaries/
   docker exec companion sh -c "cp /build/radio/src/lua/*.txt /opentx/binaries"
   cp -f binaries/*.deb ${output}/companion/linux/companion22_${version}_amd64.deb
@@ -34,7 +34,7 @@ docker stop companion
 docker rm companion
 
 # Request companion compilation on Windows
-if [ ! -f ${output}/companion/windows/companion-windows-${version}${suffix}.exe ]; then
+if [ ! -f ${output}/companion/windows/companion-windows-${version}.exe ]; then
   cd ${output}/companion/windows
   wget -qO- http://winbox.open-tx.org/companion-builds/compile22.php?branch=${branch}
   wget -O companion-windows-${version}.exe http://winbox.open-tx.org/companion-builds/companion-windows-${version}.exe
@@ -42,7 +42,7 @@ if [ ! -f ${output}/companion/windows/companion-windows-${version}${suffix}.exe 
 fi
 
 # Request companion compilation on Mac OS X
-if [ ! -f ${output}/companion/macosx/opentx-companion-${version}${suffix}.dmg ]; then
+if [ ! -f ${output}/companion/macosx/opentx-companion-${version}.dmg ]; then
   cd ${output}/companion/macosx
   wget -qO- http://opentx.blinkt.de:8080/~opentx/build-opentx.py?branch=${branch}
   wget -O opentx-companion-${version}.dmg http://opentx.blinkt.de:8080/~opentx/builds/opentx-companion-${version}.dmg
@@ -51,7 +51,7 @@ fi
 
 # Update stamps
 cp -f  $workdir/binaries/stamp-opentx.txt ${output}/firmware
-echo "#define VERSION  "'"2.2.0"' > ${output}/companion/companion-windows.stamp
+echo "#define VERSION  "'"2.2.2"' > ${output}/companion/companion-windows.stamp
 cp -f ${output}/companion/companion-windows.stamp ${output}/companion/companion-macosx.stamp
 cp -f ${output}/companion/companion-windows.stamp ${output}/companion/companion-linux.stamp
 
