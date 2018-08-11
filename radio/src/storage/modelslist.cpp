7c165c9b46407671b334ba215223caa292439939
Raphael Coeffic <rco@iptel.org>
2018-08-11 20:30:40 +0200

    wipe partial model header as it’s not read yet

--- radio/src/storage/modelslist.cpp
+++ radio/src/storage/modelslist.cpp
@@ -122,11 +122,13 @@ void ModelCell::loadBitmap()
   if (strncmp(modelFilename, g_eeGeneral.currModelFilename, LEN_MODEL_FILENAME) == 0) {
     memcpy(&partialmodel.header, &g_model.header, sizeof(partialmodel));
   }
-#if !defined (SDCARD_YAML)
   else {
+#if !defined (SDCARD_YAML)
     error = readModel(modelFilename, (uint8_t *)&partialmodel.header, sizeof(partialmodel));
-  }
+#else
+    memset(&partialmodel.header,0,sizeof(partialmodel));
 #endif
+  }
 
   tmp_buffer->clear(TEXT_BGCOLOR);
 
