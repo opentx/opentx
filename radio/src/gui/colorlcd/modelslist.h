54c21756656c1e7ceedef7041040b3b63f2016b5
Raphael Coeffic <rco@iptel.org>
2018-08-12 21:29:24 +0200

    fixed compilation with YAML_STORAGE=NO

--- radio/src/gui/colorlcd/modelslist.h
+++ radio/src/gui/colorlcd/modelslist.h
@@ -23,7 +23,11 @@
 
 #include <stdint.h>
 #include <list>
+
 #include "sdcard.h"
+#if !defined(SDCARD_YAML)
+#include "sdcard_raw.h"
+#endif
 
 #define MODELCELL_WIDTH                172
 #define MODELCELL_HEIGHT               59
