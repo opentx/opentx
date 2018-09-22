80bb920b8d2ea3c06af8b7cdbc80e17c49a45663
3djc <lesitewebdejc@hotmail.com>
2018-09-22 13:40:00 +0200

    More compile fixes

--- radio/src/switches.cpp
+++ radio/src/switches.cpp
@@ -22,7 +22,7 @@
 
 #define CS_LAST_VALUE_INIT -32768
 
-#if defined(PCBHORUS)
+#if defined(COLORLCD)
   #define SWITCH_WARNING_LIST_X        WARNING_LINE_X
   #define SWITCH_WARNING_LIST_Y        WARNING_LINE_Y+3*FH
   #define SWITCH_WARNING_LIST_INTERVAL 35
@@ -547,7 +547,7 @@ void checkSwitches()
   swarnstate_t last_bad_switches = 0xff;
   swarnstate_t states = g_model.switchWarningState;
 
-#if defined(PCBTARANIS) || defined(PCBHORUS)
+#if defined(PCBFRSKY) || defined(PCBFLYSKY)
   uint8_t bad_pots = 0, last_bad_pots = 0xff;
 #endif
 
@@ -634,7 +634,7 @@ void checkSwitches()
     backlightOn();
 
     // first - display warning
-#if defined(PCBTARANIS) || defined(PCBHORUS)
+#if defined(PCBFRSKY) || defined(PCBFLYSKY)
     if ((last_bad_switches != switches_states) || (last_bad_pots != bad_pots)) {
       drawAlertBox(STR_SWITCHWARN, NULL, STR_PRESSANYKEYTOSKIP);
       if (last_bad_switches == 0xff || last_bad_pots == 0xff) {
