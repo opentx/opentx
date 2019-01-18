987dbe30bec91eb6cdd6f18662f0ab46498c16ac
norulers <norulers@vip.qq.com>
2019-01-18 14:28:54 +0800

    G to F switch

--- radio/src/strhelpers.cpp
+++ radio/src/strhelpers.cpp
@@ -266,7 +266,7 @@ char * getSwitchString(char * dest, swsrc_t idx)
       if (swinfo.quot == 5)
         *s++ = 'H';
       else if (swinfo.quot == 4)
-        *s++ = 'F';
+        *s++ = 'G';
       else
         *s++ = 'A'+swinfo.quot;
 #else
