2ce2f082db3897697687bbdba4d8856e4b6d8470
Bertrand Songis <bsongis@gmail.com>
2019-03-09 10:55:15 +0100

    Cosmetics

--- radio/src/gui/128x64/popups.h
+++ radio/src/gui/128x64/popups.h
@@ -48,7 +48,7 @@ extern uint8_t warningInfoFlags;
   #define SET_WARNING_INFO(...)
 #else
   #define DISPLAY_WARNING              (*popupFunc)
-  #define POPUP_INFORMATION(s)        (warningText = s, warningType = WARNING_TYPE_INFO, warningInfoText = 0, popupFunc = runPopupWarning)
+  #define POPUP_INFORMATION(s)         (warningText = s, warningType = WARNING_TYPE_INFO, warningInfoText = 0, popupFunc = runPopupWarning)
   #define POPUP_WARNING(s)             (warningText = s, warningInfoText = 0, popupFunc = runPopupWarning)
   #define POPUP_CONFIRMATION(s)        (warningText = s, warningType = WARNING_TYPE_CONFIRM, warningInfoText = 0, popupFunc = runPopupWarning)
   #define POPUP_INPUT(s, func)         (warningText = s, popupFunc = func)
