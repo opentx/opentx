7058df482c6d3f296bd075d4d0793c2b5c21ef58
Bertrand Songis <bsongis@gmail.com>
2019-03-13 18:31:04 +0100

    [X3] Compilation fix

--- radio/src/targets/common/arm/stm32/adc_driver.cpp
+++ radio/src/targets/common/arm/stm32/adc_driver.cpp
@@ -43,7 +43,7 @@
 #elif defined(REV4a)
   const int8_t ana_direction[NUM_ANALOGS] = {1,-1,1,-1,  1,-1,0,  1,1,  1};
 #else
-  const int8_t ana_direction[NUM_ANALOGS] = {1,-1,1,-1,  1,1,0,   1,1,  1};
+  const int8_t ana_direction[NUM_ANALOGS] = {1,-1,-1,1,  1,1};
 #endif
 
 #if NUM_PWMSTICKS > 0
