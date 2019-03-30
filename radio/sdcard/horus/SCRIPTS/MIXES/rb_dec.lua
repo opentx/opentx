a819ae1013ce389863c4ccd0f4b995aac98064f1
3djc <lesitewebdejc@hotmail.com>
2019-03-30 08:43:32 +0100

    Add more sensor, make choosing a bit easier

--- radio/sdcard/horus/SCRIPTS/MIXES/rb_dec.lua
+++ radio/sdcard/horus/SCRIPTS/MIXES/rb_dec.lua
@@ -36,11 +36,19 @@ end
 
 local function run()
   local status = getValue("RBS")
-  print("RBS VALUE "..status)
-  setTelemetryValue(0xb20, 1, 2, hasbit(status, bit(4)), 0, 0, "R1FS")
-  setTelemetryValue(0xb20, 2, 2, hasbit(status, bit(6)), 0, 0, "R2FS")
-  setTelemetryValue(0xb20, 3, 2, hasbit(status, bit(5)), 0, 0, "R1FL")
-  setTelemetryValue(0xb20, 4, 2, hasbit(status, bit(7)), 0, 0, "R2FL")
+
+  --  UNCOMMENT DESIRED SENSOR (many are defaulted commented to prevent hitting to fast the 32 sensor limit)
+  -- setTelemetryValue(0xb20, 1, 2, hasbit(status, bit(1)), 0, 0, "R1OL")
+  -- setTelemetryValue(0xb20, 2, 2, hasbit(status, bit(2)), 0, 0, "R2OL")
+  -- setTelemetryValue(0xb20, 3, 2, hasbit(status, bit(3)), 0, 0, "SBOL")
+  setTelemetryValue(0xb20, 1, 4, hasbit(status, bit(4)), 0, 0, "R1FS")
+  setTelemetryValue(0xb20, 2, 5, hasbit(status, bit(6)), 0, 0, "R2FS")
+  -- setTelemetryValue(0xb20, 6, 2, hasbit(status, bit(5)), 0, 0, "R1FL")
+  -- setTelemetryValue(0xb20, 7, 2, hasbit(status, bit(7)), 0, 0, "R2FL")
+  -- setTelemetryValue(0xb20, 8, 2, hasbit(status, bit(8)), 0, 0, "RX1L")
+  -- setTelemetryValue(0xb20, 9, 2, hasbit(status, bit(9)), 0, 0, "RX2L")
+  -- setTelemetryValue(0xb20, 10, 2, hasbit(status, bit(10)), 0, 0, "R1NS")
+  -- setTelemetryValue(0xb20, 11, 2, hasbit(status, bit(11)), 0, 0, "R2NS")
   if status ~= 0 then
     return 10
   else
