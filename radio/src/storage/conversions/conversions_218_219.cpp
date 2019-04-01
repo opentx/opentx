6a01d9de713acca0e4403b56063bf98988b111ab
Bertrand Songis <bsongis@gmail.com>
2019-04-01 12:37:11 +0200

    Conversion fix

--- radio/src/storage/conversions/conversions_218_219.cpp
+++ radio/src/storage/conversions/conversions_218_219.cpp
@@ -48,7 +48,7 @@ void convertModelData_218_to_219(ModelData &model)
 
   for (int i=0; i<NUM_MODULES; i++) {
     memcpy(&newModel.moduleData[i], &oldModel.moduleData[i], 4);
-    memcpy(&newModel.moduleData[i] + 4, &oldModel.moduleData[i] + 64 + 4, 2);
+    memcpy(((uint8_t *)&newModel.moduleData[i]) + 4, ((uint8_t *)&oldModel.moduleData[i]) + 64 + 4, 2);
     if (newModel.moduleData[i].type >= MODULE_TYPE_XJT2)
       newModel.moduleData[i].type += 1;
     if (newModel.moduleData[i].type >= MODULE_TYPE_R9M2)
