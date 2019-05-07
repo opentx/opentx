f2d25e049febe85abb6d7671802b53db508bfdfb
Bertrand Songis <bsongis@gmail.com>
2019-05-07 11:32:47 +0200

    Warning removed

--- radio/src/targets/taranis/board_preinit.cpp
+++ radio/src/targets/taranis/board_preinit.cpp
@@ -22,13 +22,14 @@
 
 void boardPreInit()
 {
+#if defined(INTMODULE_BOOTCMD_GPIO)
   GPIO_InitTypeDef GPIO_InitStructure;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
 
-#if defined(INTMODULE_BOOTCMD_GPIO)
+
   GPIO_SetBits(INTMODULE_BOOTCMD_GPIO, INTMODULE_BOOTCMD_GPIO_PIN);
   GPIO_InitStructure.GPIO_Pin = INTMODULE_BOOTCMD_GPIO_PIN;
   GPIO_Init(INTMODULE_BOOTCMD_GPIO, &GPIO_InitStructure);
