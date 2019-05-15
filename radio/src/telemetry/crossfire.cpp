0d5329a17bf952e3defb079ed0c8f9bc43e557e4
3djc <lesitewebdejc@hotmail.com>
2019-05-15 09:23:36 +0200

    cosmetics

--- radio/src/telemetry/crossfire.cpp
+++ radio/src/telemetry/crossfire.cpp
@@ -181,8 +181,7 @@ bool isCrossfireOutputBufferAvailable()
 
 void processCrossfireTelemetryData(uint8_t data)
 {
-
-  #if defined(SERIAL2)
+#if defined(SERIAL2)
   if (g_eeGeneral.serial2Mode == UART_MODE_TELEMETRY_MIRROR) {
     serial2Putc(data);
   }
