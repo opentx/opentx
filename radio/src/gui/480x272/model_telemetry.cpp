a28c755d915d4812422764cd18e309aef507ef8f
3djc <lesitewebdejc@hotmail.com>
2019-05-19 10:21:12 +0200

    Multi compile fix

--- radio/src/gui/480x272/model_telemetry.cpp
+++ radio/src/gui/480x272/model_telemetry.cpp
@@ -272,7 +272,7 @@ bool menuModelTelemetryFrsky(event_t event)
 
       case ITEM_TELEMETRY_RSSI_LABEL:
 #if defined(MULTIMODULE)
-        if (g_model.moduleData[INTERNAL_MODULE].rfProtocol == RF_PROTO_OFF &&
+        if (!IS_INTERNAL_MODULE_ON() &&
           g_model.moduleData[EXTERNAL_MODULE].type == MODULE_TYPE_MULTIMODULE  &&
           g_model.moduleData[EXTERNAL_MODULE].getMultiProtocol(false) == MM_RF_PROTO_FS_AFHDS2A)
           lcdDrawText(MENUS_MARGIN_LEFT, y, "RSNR");
