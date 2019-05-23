73691595be1fa1a2ecdb64cbed16fd9c22f61cb9
3djc <lesitewebdejc@hotmail.com>
2019-05-23 07:34:06 +0200

    Please travis

--- radio/src/gui/128x64/radio_diagkeys.cpp
+++ radio/src/gui/128x64/radio_diagkeys.cpp
@@ -57,11 +57,11 @@ void menuRadioDiagKeys(event_t event)
         lcdDrawTextAtIndex(0, y, STR_VKEYS, (TRM_BASE-1-i), 0);
         displayKeyState(5*FW+4, y, KEY_MENU+(TRM_BASE-1-i));
       }
-#elif defined(PCBXLITE) && defined(RADIO_X7)
+#elif defined(KEYS_GPIO_REG_SHIFT)
       y = MENU_HEADER_HEIGHT + FH*i;
       lcdDrawTextAtIndex(0, y, STR_VKEYS, (TRM_BASE-1-i), 0);
       displayKeyState(5*FW+2, y, KEY_SHIFT+(TRM_BASE-1-i));
-#elif !defined(RADIO_T12)
+#elif defined(KEYS_GPIO_REG_MENU)
       y = MENU_HEADER_HEIGHT + FH + FH*i;
       lcdDrawTextAtIndex(0, y, STR_VKEYS, (TRM_BASE-1-i), 0);
       displayKeyState(5*FW+2, y, KEY_MENU+(TRM_BASE-1-i));
