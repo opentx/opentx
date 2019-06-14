af085f4f28387a03536b445654bdfe5049888bab
3djc <lesitewebdejc@hotmail.com>
2019-06-14 07:27:48 +0200

    Typo

--- radio/sdcard/FrSky-utilities/SxR-config.lua
+++ radio/sdcard/FrSky-utilities/SxR-config.lua
@@ -268,9 +268,9 @@ local function runFieldsPage(event)
             end
         end
     elseif edit then
-        if event == EVT_VIRTUAL_NEXT or event == EVT_VIRTUAL_NEXT_REP  then
+        if event == EVT_VIRTUAL_NEXT or event == EVT_VIRTUAL_NEXT_REPT  then
             addField(1)
-        elseif event == EVT_VIRTUAL_PREVIOUS or event == EVT_VIRTUAL_PREVIOUS_REP then
+        elseif event == EVT_VIRTUAL_PREVIOUS or event == EVT_VIRTUAL_PREVIOUS_REPT then
             addField(-1)
         end
     else
