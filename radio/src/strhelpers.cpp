1f1ccf277cbc3afe279be95bf21bd911f7fdf06e
Bertrand Songis <bsongis@gmail.com>
2019-06-17 11:59:33 +0200

    Cosmetics

--- radio/src/strhelpers.cpp
+++ radio/src/strhelpers.cpp
@@ -46,11 +46,7 @@ char zchar2char(int8_t idx)
 
 char char2lower(char c)
 {
-  if(c >= 'A' && c <= 'Z'){
-    return c+32;
-  }
-  else
-    return c;
+  return (c >= 'A' && c <= 'Z') ? c + 32 : c;
 }
 
 int8_t char2zchar(char c)
