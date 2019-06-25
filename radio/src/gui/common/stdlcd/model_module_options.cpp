f9ab0ee9addf8f92bddea28304e4107ce5458166
Bertrand Songis <bsongis@gmail.com>
2019-06-25 11:26:08 +0200

    Hardening

--- radio/src/gui/common/stdlcd/model_module_options.cpp
+++ radio/src/gui/common/stdlcd/model_module_options.cpp
@@ -87,7 +87,7 @@ void menuModelModuleOptions(event_t event)
   uint8_t modelId = reusableBuffer.hardwareAndSettings.modules[g_moduleIdx].information.modelID;
   // uint8_t variant = reusableBuffer.hardwareAndSettings.modules[g_moduleIdx].information.variant;
 
-  uint8_t optionsAvailable = moduleOptions[modelId] & ((1 << MODULE_OPTION_EXTERNAL_ANTENNA) | (1 << MODULE_OPTION_POWER));
+  uint8_t optionsAvailable = getModuleOptions(modelId) & ((1 << MODULE_OPTION_EXTERNAL_ANTENNA) | (1 << MODULE_OPTION_POWER));
 
   SUBMENU_NOTITLE(ITEM_MODULE_SETTINGS_COUNT, {
     !optionsAvailable ? (uint8_t)0 : IF_MODULE_OPTIONS(MODULE_OPTION_EXTERNAL_ANTENNA, 0),
