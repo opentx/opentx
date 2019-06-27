87a1afb748faf8a1bae8aa8e3e298913c815799d
Bertrand Songis <bsongis@gmail.com>
2019-06-27 22:30:25 +0200

    Add radio tools to all radio (#6525)
    Add radio tools to all radio

--- radio/src/gui/212x64/menu_radio.cpp
+++ radio/src/gui/212x64/menu_radio.cpp
@@ -23,9 +23,7 @@
 const MenuHandlerFunc menuTabGeneral[MENU_RADIO_PAGES_COUNT] = {
   menuRadioSetup,
   menuRadioSdManager,
-#if defined(PXX2)
   menuRadioTools,
-#endif
   menuRadioSpecialFunctions,
   menuRadioTrainer,
   menuRadioHardware,
