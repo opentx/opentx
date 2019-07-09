86c4763a9337fad118081bd236233e62242246f9
3djc <lesitewebdejc@hotmail.com>
2019-07-09 17:28:06 +0200

    X9 versions

--- radio/sdcard/taranis-x9/SCRIPTS/TOOLS/CROSSFIRE/crossfire.lua
+++ radio/sdcard/taranis-x9/SCRIPTS/TOOLS/CROSSFIRE/crossfire.lua
@@ -86,11 +86,11 @@ local function run(event)
   if event == nil then
     error("Cannot be run as a model script!")
     return 2
-  elseif event == EVT_EXIT_BREAK then
+  elseif event == EVT_VIRTUAL_EXIT then
     return 2
-  elseif event == EVT_PLUS_FIRST or event == EVT_PLUS_REPT or event == EVT_ROT_RIGHT then
+  elseif event == EVT_VIRTUAL_NEXT then
     selectDevice(1)
-  elseif event == EVT_MINUS_FIRST or event == EVT_MINUS_REPT or event == EVT_ROT_LEFT then
+  elseif event == EVT_VIRTUAL_PREVIOUS then
     selectDevice(-1)
   end
 
@@ -101,7 +101,7 @@ local function run(event)
   else
     for i=1, #devices do
       local attr = (lineIndex == i and INVERS or 0)
-      if event == EVT_ENTER_BREAK and attr == INVERS then
+      if event == EVT_VIRTUAL_ENTER and attr == INVERS then
           crossfireTelemetryPush(0x28, { devices[i].id, 0xEA })
           return "device.lua"
       end
