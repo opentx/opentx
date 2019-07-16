44a929d4461affb7df31046ecfc0ba3434d604a9
Bertrand Songis <bsongis@gmail.com>
2019-07-16 23:22:12 +0200

    Typo

--- radio/src/pulses/modules_helpers.h
+++ radio/src/pulses/modules_helpers.h
@@ -470,7 +470,7 @@ inline uint8_t getMaxRxNum(uint8_t idx)
 inline const char * getModuleDelay(uint8_t idx)
 {
   if (isModuleISRMAccess(idx))
-    return sentModuleChannels(idx) > 16 ? "(21ms)" : (sentModuleChannels(idx) > 16 ? "(14ms)" : "(7ms)");
+    return sentModuleChannels(idx) > 16 ? "(21ms)" : (sentModuleChannels(idx) > 8 ? "(14ms)" : "(7ms)");
 
   if (isModuleXJTD16(idx) || isModuleR9MNonAccess(idx))
     return sentModuleChannels(idx) > 8 ? "(18ms)" : "(9ms)";
