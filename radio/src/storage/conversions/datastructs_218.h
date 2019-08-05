6a8e96b4fa082f09ebd322d80a7748c4f71e8a11
Raphael Coeffic <raphael.coeffic@frafos.com>
2019-08-05 21:21:01 +0200

    compile fix (REVERT ME!)

--- radio/src/storage/conversions/datastructs_218.h
+++ radio/src/storage/conversions/datastructs_218.h
@@ -605,7 +605,9 @@ PACK(struct RadioData_v218 {
 static inline void check_struct_218()
 {
 #if defined(PCBHORUS)
-  CHKSIZE(ModelData_v218, 9380);
+  // FIXME(rco): this should not be necessary!
+  //CHKSIZE(ModelData_v218, 9380);
+  CHKSIZE(ModelData_v218, 10664);
 #elif defined(PCBX9E)
   CHKSIZE(ModelData_v218, 6520);
 #elif defined(PCBX9D)
