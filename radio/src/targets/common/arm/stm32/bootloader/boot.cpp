31b501383e198772a0b363221678a0ba7ac017ed
Bertrand Songis <bsongis@gmail.com>
2019-08-17 13:50:08 +0200

    Exit from bootloader was broken again

--- radio/src/targets/common/arm/stm32/bootloader/boot.cpp
+++ radio/src/targets/common/arm/stm32/bootloader/boot.cpp
@@ -213,15 +213,12 @@ int main()
   pwrInit();
   pwrOff();
 
+#if defined(PCBHORUS)
   // wait a bit for the inputs to stabilize...
   // ... otherwise the bootloader cannot be started
   //     when the power on delay is cut to 0.
+#endif
 
-  // avoid booting after DFU bootloader
-  if (!pwrPressed()) {
-    boardOff();
-  }
-  
   // LHR & RHL trims not pressed simultanously
   if (readTrims() != BOOTLOADER_KEYS) {
     // Start main application
