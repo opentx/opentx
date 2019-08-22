777fff354f7c80d94cd113c466a39756a9a1f912
Anton Evmenenko <anton.evmenenko.93@gmail.com>
2019-08-23 00:24:56 +0300

    code style fixed for opentx/radio/src/targets/sky9x/joystick.cpp

--- radio/src/targets/sky9x/joystick.cpp
+++ radio/src/targets/sky9x/joystick.cpp
@@ -29,60 +29,60 @@ extern void USBD_Connect(void);
 
 static void ConfigureUsbClock(void)
 {
-    /* Enable PLLB for USB */
-    PMC->CKGR_PLLBR = CKGR_PLLBR_DIVB(1)
-                    | CKGR_PLLBR_MULB(7)
-                    | CKGR_PLLBR_PLLBCOUNT_Msk;
-    while((PMC->PMC_SR & PMC_SR_LOCKB) == 0); // TODO  && (timeout++ < CLOCK_TIMEOUT));
-    /* USB Clock uses PLLB */
-    PMC->PMC_USB = PMC_USB_USBDIV(1)    /* /2   */
-                 | PMC_USB_USBS;        /* PLLB */
+  /* Enable PLLB for USB */
+  PMC->CKGR_PLLBR = CKGR_PLLBR_DIVB(1)
+                  | CKGR_PLLBR_MULB(7)
+                  | CKGR_PLLBR_PLLBCOUNT_Msk;
+  while((PMC->PMC_SR & PMC_SR_LOCKB) == 0); // TODO  && (timeout++ < CLOCK_TIMEOUT));
+  /* USB Clock uses PLLB */
+  PMC->PMC_USB = PMC_USB_USBDIV(1)    /* /2   */
+               | PMC_USB_USBS;        /* PLLB */
 }
 
 void usbJoystickUpdate()
 {
-    static bool initialized = false;
+  static bool initialized = false;
 
-    if (usbPlugged()) {
-        TRACE_DEBUG("usbJoystick\n\r");
+  if (usbPlugged()) {
+    TRACE_DEBUG("usbJoystick\n\r");
 
-        if (!initialized) {
-            ConfigureUsbClock();
+    if (!initialized) {
+      ConfigureUsbClock();
 
-            HIDDJoystickDriver_Initialize();
+      HIDDJoystickDriver_Initialize();
 
-            // VBus_Configure();
-            USBD_Connect();
+      // VBus_Configure();
+      USBD_Connect();
 
-            initialized = true;
-        }
-
-        static uint8_t HID_Buffer[11];
-
-        HID_Buffer[0] = 0;
-        HID_Buffer[1] = 0;
-        HID_Buffer[2] = 0;
-        for (int i = 0; i < 8; ++i) {
-            if ( channelOutputs[i+8] > 0 ) {
-                HID_Buffer[0] |= (1 << i);
-            }
-            if ( channelOutputs[i+16] > 0 ) {
-                HID_Buffer[1] |= (1 << i);
-            }
-            if ( channelOutputs[i+24] > 0 ) {
-                HID_Buffer[2] |= (1 << i);
-            }
-        }
-
-        //analog values
-        //uint8_t * p = HID_Buffer + 1;
-        for (int i = 0; i < 8; ++i) {
-            int16_t value = channelOutputs[i] / 8;
-            if ( value > 127 ) value = 127;
-            else if ( value < -127 ) value = -127;
-            HID_Buffer[i+3] = static_cast<int8_t>(value);
-        }
-
-        HIDDJoystickDriver_ChangeJoystickState((HIDDJoystickInputReport*)&HID_Buffer);
+      initialized = true;
     }
+
+    static uint8_t HID_Buffer[11];
+
+    HID_Buffer[0] = 0;
+    HID_Buffer[1] = 0;
+    HID_Buffer[2] = 0;
+    for (int i = 0; i < 8; ++i) {
+      if ( channelOutputs[i+8] > 0 ) {
+        HID_Buffer[0] |= (1 << i);
+      }
+      if ( channelOutputs[i+16] > 0 ) {
+        HID_Buffer[1] |= (1 << i);
+      }
+      if ( channelOutputs[i+24] > 0 ) {
+        HID_Buffer[2] |= (1 << i);
+      }
+    }
+
+    //analog values
+    //uint8_t * p = HID_Buffer + 1;
+    for (int i = 0; i < 8; ++i) {
+      int16_t value = channelOutputs[i] / 8;
+      if ( value > 127 ) value = 127;
+      else if ( value < -127 ) value = -127;
+      HID_Buffer[i+3] = static_cast<int8_t>(value);
+    }
+
+    HIDDJoystickDriver_ChangeJoystickState((HIDDJoystickInputReport*)&HID_Buffer);
+  }
 }
