465674ca689c00d7a9ac7d0f3c5a447e79e64637
Bertrand Songis <bsongis@gmail.com>
2019-09-10 14:38:39 +0200

    Fixes #6721 - Tests needed

--- radio/src/gui/480x272/view_text.cpp
+++ radio/src/gui/480x272/view_text.cpp
@@ -24,7 +24,7 @@ bool menuTextView(event_t event)
 {
   static int lines_count;
 
-  drawMenuTemplate(STR_TEXT_VIEWER, ICON_OPENTX);
+  drawMenuTemplate(STR_TEXT_VIEWER, ICON_OPENTX, nullptr, OPTION_MENU_NO_SCROLLBAR);
 
   switch (event) {
     case EVT_ENTRY:
@@ -42,7 +42,7 @@ bool menuTextView(event_t event)
       break;
 
     case EVT_ROTARY_RIGHT:
-      if (menuVerticalOffset+NUM_BODY_LINES >= lines_count)
+      if (menuVerticalOffset + NUM_BODY_LINES >= lines_count)
         break;
       else
         ++menuVerticalOffset;
