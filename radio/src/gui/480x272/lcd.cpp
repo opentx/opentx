fcea9519914217219fd64c4aa9d0f4745a548d45
3djc <lesitewebdejc@hotmail.com>
2019-09-12 09:52:17 +0200

    Fix swedish font (#6750)
    Swedish font fix

--- radio/src/gui/480x272/lcd.cpp
+++ radio/src/gui/480x272/lcd.cpp
@@ -52,7 +52,7 @@ uint8_t getMappedChar(uint8_t c)
   else if (c >= 0x80 && c <= 0x81) {
     result = 157 + c - 0x80;
   }
-#elif defined(TRANSLATIONS_FI)
+#elif defined(TRANSLATIONS_FI) || defined(TRANSLATIONS_SE)
   else if (c >= 0x80 && c <= 0x85) {
     result = 159 + c - 0x80;
   }
@@ -68,16 +68,12 @@ uint8_t getMappedChar(uint8_t c)
   else if (c >= 0x80 && c <= 0x80+21) {
     result = 185 + c - 0x80;
   }
-#elif defined(TRANSLATIONS_SE)
-  else if (c >= 0x80 && c <= 0x85) {
-    result = 207 + c - 0x80;
-  }
 #endif
   else if (c < 0xC0)
     result = c - 0x20;
   else
     result = c - 0xC0 + 96;
-  // TRACE("getMappedChar '%c' (%d) = %d", c, c, result);
+   // TRACE("getMappedChar '%c' (%d) = %d", c, c, result);
   return result;
 }
 
