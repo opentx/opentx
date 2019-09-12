34ac3f53a6a6d39599b7f8d4763877d32325cbbb
Kilrah <kilrah@andrebernet.ch>
2019-09-12 20:01:48 +0200

    Multi struct was misaligned

--- companion/src/firmwares/opentx/opentxeeprom.cpp
+++ companion/src/firmwares/opentx/opentxeeprom.cpp
@@ -2028,9 +2028,9 @@ class ModuleUnionField: public UnionField<unsigned int> {
         ModuleData::Multi& multi = module.multi;
         internalField.Append(new UnsignedField<2>(this, rfProtExtra));
         internalField.Append(new SpareBitsField<3>(this));
+        internalField.Append(new BoolField<1>(this, multi.customProto));
         internalField.Append(new BoolField<1>(this, multi.autoBindMode));
         internalField.Append(new BoolField<1>(this, multi.lowPowerMode));
-        internalField.Append(new BoolField<1>(this, multi.customProto));
         internalField.Append(new SignedField<8>(this, multi.optionValue));
       }
 
