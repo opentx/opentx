77f23765ae5469b4e5b32e7b232d4a30ec696e7b
Bertrand Songis <bsongis@gmail.com>
2019-09-24 10:20:48 +0200

    No LR12 from internal module on X9D+ 2019

--- radio/src/gui/212x64/model_setup.cpp
+++ radio/src/gui/212x64/model_setup.cpp
@@ -776,7 +776,7 @@ void menuModelSetup(event_t event)
             }
           }
           else if (isModulePXX2(INTERNAL_MODULE)) {
-            g_model.moduleData[INTERNAL_MODULE].subType = checkIncDec(event, g_model.moduleData[INTERNAL_MODULE].subType, 0, MODULE_SUBTYPE_ISRM_PXX2_ACCST_LR12, EE_MODEL, isRfProtocolAvailable);
+            g_model.moduleData[INTERNAL_MODULE].subType = checkIncDec(event, g_model.moduleData[INTERNAL_MODULE].subType, 0, MODULE_SUBTYPE_ISRM_PXX2_ACCST_D16, EE_MODEL, isRfProtocolAvailable);
           }
         }
 #else
@@ -786,7 +786,7 @@ void menuModelSetup(event_t event)
         }
         lcdDrawTextAtIndex(MODEL_SETUP_2ND_COLUMN, y, STR_ISRM_RF_PROTOCOLS, index, attr);
         if (attr) {
-          index = checkIncDec(event, index, 0, 3, EE_MODEL);
+          index = checkIncDec(event, index, 0, MODULE_SUBTYPE_ISRM_PXX2_ACCST_D16 + 1 /* because of --- */, EE_MODEL);
           if (checkIncDec_Ret) {
             memclear(&g_model.moduleData[INTERNAL_MODULE], sizeof(ModuleData));
             if (index > 0) {
