2a2f996eb4ce70b97dd389bc0c1677b796a9108f
3djc <lesitewebdejc@hotmail.com>
2019-10-01 07:32:23 +0200

    Fix bind on 9x nivigation

--- radio/src/gui/128x64/model_setup.cpp
+++ radio/src/gui/128x64/model_setup.cpp
@@ -1282,7 +1282,7 @@ void menuModelSetup(event_t event)
                   if (isModuleR9MNonAccess(moduleIdx) || isModuleD16(moduleIdx)) {
 #if defined(PCBXLITE)
                     if (EVT_KEY_MASK(event) == KEY_ENTER) {
-#elif defined(PCBSKY9X) || defined(PCBAR9X)
+#elif defined(NAVIGATION_9X)
                     if (event ==  EVT_KEY_FIRST(KEY_ENTER)) {
 #else
                     if (event == EVT_KEY_BREAK(KEY_ENTER)) {
