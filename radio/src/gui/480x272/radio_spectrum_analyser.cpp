077d298510a1eb9b7c4952496e1649ffb954488b
3djc <lesitewebdejc@hotmail.com>
2019-10-03 15:33:27 +0200

    Cosmetics

--- radio/src/gui/480x272/radio_spectrum_analyser.cpp
+++ radio/src/gui/480x272/radio_spectrum_analyser.cpp
@@ -22,7 +22,8 @@
 
 extern uint8_t g_moduleIdx;
 
-enum SpectrumFields {
+enum SpectrumFields
+{
   SPECTRUM_FREQUENCY,
   SPECTRUM_SPAN,
   SPECTRUM_TRACK,
@@ -32,10 +33,10 @@ enum SpectrumFields {
 coord_t getAverage(uint8_t number, uint8_t * value)
 {
   uint16_t sum = 0;
-  for (uint8_t i=0; i < number; i++) {
+  for (uint8_t i = 0; i < number; i++) {
     sum += value[i];
   }
-  return sum/number;
+  return sum / number;
 }
 
 #define SPECTRUM_ROW  (isModuleMultimodule(g_moduleIdx) ? READONLY_ROW : (uint8_t)0)
@@ -49,7 +50,7 @@ bool menuRadioSpectrumAnalyser(event_t event)
   });
 
   if (menuEvent) {
-    lcdDrawCenteredText(LCD_H/2, STR_STOPPING);
+    lcdDrawCenteredText(LCD_H / 2, STR_STOPPING);
     lcdRefresh();
     if (isModulePXX2(g_moduleIdx))
       moduleState[g_moduleIdx].readModuleInformation(&reusableBuffer.moduleSetup.pxx2.moduleInformation, PXX2_HW_INFO_TX_ID, PXX2_HW_INFO_TX_ID);
@@ -99,8 +100,8 @@ bool menuRadioSpectrumAnalyser(event_t event)
     moduleState[g_moduleIdx].mode = MODULE_MODE_SPECTRUM_ANALYSER;
   }
 
-  for (uint8_t i=0; i<SPECTRUM_FIELDS_MAX; i++) {
-    LcdFlags attr = (menuVerticalPosition == i ? (s_editMode>0 ? INVERS|BLINK : INVERS) : 0);
+  for (uint8_t i = 0; i < SPECTRUM_FIELDS_MAX; i++) {
+    LcdFlags attr = (menuVerticalPosition == i ? (s_editMode > 0 ? INVERS | BLINK : INVERS) : 0);
 
     switch (i) {
       case SPECTRUM_FREQUENCY: {
@@ -117,8 +118,7 @@ bool menuRadioSpectrumAnalyser(event_t event)
         break;
       }
 
-      case SPECTRUM_SPAN:
-      {
+      case SPECTRUM_SPAN: {
         uint8_t span = reusableBuffer.spectrumAnalyser.span / 1000000;
         lcdDrawText(MENUS_MARGIN_LEFT + 100, MENU_FOOTER_TOP, "S:", TEXT_INVERTED_COLOR);
         lcdDrawNumber(lcdNextPos + 2, MENU_FOOTER_TOP, reusableBuffer.spectrumAnalyser.span / 1000000, attr | TEXT_INVERTED_COLOR);
@@ -156,7 +156,7 @@ bool menuRadioSpectrumAnalyser(event_t event)
     lcdDrawVerticalLine(x, MENU_HEADER_HEIGHT, LCD_H - MENU_HEADER_HEIGHT - MENU_FOOTER_HEIGHT, STASHED, CURVE_AXIS_COLOR);
   }
 
-  for (uint8_t power = 20; ; power += 20) {
+  for (uint8_t power = 20;; power += 20) {
     int y = MENU_FOOTER_TOP - 1 - limit<int>(0, power << 1, LCD_H - MENU_HEADER_HEIGHT - MENU_FOOTER_HEIGHT);
     if (y <= MENU_HEADER_HEIGHT)
       break;
@@ -171,21 +171,21 @@ bool menuRadioSpectrumAnalyser(event_t event)
   // Draw spectrum data
   constexpr uint8_t step = 4;
 
-  for (coord_t xv=0; xv <= LCD_W-step; xv+=4) {
+  for (coord_t xv = 0; xv <= LCD_W - step; xv += 4) {
     coord_t yv = MENU_FOOTER_TOP - 1 - limit<int>(0, getAverage(step, &reusableBuffer.spectrumAnalyser.bars[xv]) << 1, LCD_H - MENU_HEADER_HEIGHT - MENU_FOOTER_HEIGHT);
     coord_t max_yv = MENU_FOOTER_TOP - 1 - limit<int>(0, getAverage(step, &reusableBuffer.spectrumAnalyser.max[xv]) << 1, LCD_H - MENU_HEADER_HEIGHT - MENU_FOOTER_HEIGHT);
 
     // Signal bar
     lcdDrawSolidFilledRect(xv, yv, step - 1, LCD_H - yv - MENU_FOOTER_HEIGHT, TEXT_INVERTED_BGCOLOR);
-    lcdDrawRect(xv, yv, step -1, LCD_H - yv - MENU_FOOTER_HEIGHT);
+    lcdDrawRect(xv, yv, step - 1, LCD_H - yv - MENU_FOOTER_HEIGHT);
 
     // Signal max
     lcdDrawLine(xv, max_yv, xv + step, max_yv);
 
     // Decay max values
     if (max_yv < yv) { // Those value are INVERTED (MENU_FOOTER_TOP - value)
-      for (uint8_t i=0; i < step;i++) {
-        reusableBuffer.spectrumAnalyser.max[xv+i]-=1;
+      for (uint8_t i = 0; i < step; i++) {
+        reusableBuffer.spectrumAnalyser.max[xv + i] -= 1;
       }
     }
   }
