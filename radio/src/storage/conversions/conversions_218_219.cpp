bac057c447b7836ad4692c57b8196c1a679632b1
3djc <lesitewebdejc@hotmail.com>
2019-10-06 14:16:29 +0200

    Cosmetics

--- radio/src/storage/conversions/conversions_218_219.cpp
+++ radio/src/storage/conversions/conversions_218_219.cpp
@@ -363,7 +363,7 @@ void convertModelData_218_to_219(ModelData &model)
   }
 
   for (uint8_t i=0; i<MAX_SCRIPTS; i++) {
-    for(uint8_t j=0; j<MAX_SCRIPT_INPUTS; j++) {
+    for (uint8_t j=0; j<MAX_SCRIPT_INPUTS; j++) {
       newModel.scriptsData[i].inputs[j].source = convertSource_218_to_219(oldModel.scriptsData[i].inputs[j].source);
     }
   }
