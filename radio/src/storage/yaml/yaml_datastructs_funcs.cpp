3c193f7e73af7708c15e389bcba7baa00efbf496
Raphael Coeffic <raphael.coeffic@frafos.com>
2019-11-10 13:14:11 +0100

    fixed output module union field

--- radio/src/storage/yaml/yaml_datastructs_funcs.cpp
+++ radio/src/storage/yaml/yaml_datastructs_funcs.cpp
@@ -123,22 +123,22 @@ static uint8_t select_mod_type(uint8_t* data, uint32_t bitoffs)
     case MODULE_TYPE_PPM:
     case MODULE_TYPE_DSM2:
     case MODULE_TYPE_CROSSFIRE:
-        return 0;
-    case MODULE_TYPE_MULTIMODULE:
         return 1;
+    case MODULE_TYPE_MULTIMODULE:
+        return 2;
     case MODULE_TYPE_XJT_PXX1:
     case MODULE_TYPE_R9M_PXX1:
     case MODULE_TYPE_R9M_LITE_PXX1:
     case MODULE_TYPE_R9M_LITE_PRO_PXX1:
-        return 2;
-    case MODULE_TYPE_SBUS:
         return 3;
+    case MODULE_TYPE_SBUS:
+        return 4;
     case MODULE_TYPE_ISRM_PXX2:
     case MODULE_TYPE_R9M_PXX2:
     case MODULE_TYPE_R9M_LITE_PXX2:
     case MODULE_TYPE_R9M_LITE_PRO_PXX2:
     case MODULE_TYPE_XJT_LITE_PXX2:
-        return 4;
+        return 5;
     }
     return 0;
 }
