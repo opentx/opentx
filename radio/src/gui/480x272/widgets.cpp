cccf2ac0231d33f8a50a857594a437ef04113f53
3djc <lesitewebdejc@hotmail.com>
2019-11-28 13:33:11 +0100

    Cosmetics

--- radio/src/gui/480x272/widgets.cpp
+++ radio/src/gui/480x272/widgets.cpp
@@ -113,15 +113,15 @@ void drawProgressScreen(const char * title, const char * message, int num, int d
   lcd->clear(TEXT_BGCOLOR);
   lcdDrawSolidFilledRect(0, 0, LCD_W, MENU_HEADER_HEIGHT, HEADER_BGCOLOR);
   if (title) {
-    lcdDrawText(LCD_W / 2 ,  (MENU_HEADER_HEIGHT - FH) / 2, title, MENU_TITLE_COLOR | CENTERED);
+    lcdDrawText(LCD_W / 2, (MENU_HEADER_HEIGHT - FH) / 2, title, MENU_TITLE_COLOR | CENTERED);
   }
   if (message) {
     lcdDrawText(MENUS_MARGIN_LEFT, LCD_H-42, message, TEXT_COLOR);
   }
-  lcdDrawRect(MENUS_MARGIN_LEFT, LCD_H-22, LCD_W-2*MENUS_MARGIN_LEFT, 15, 1, SOLID, BARGRAPH_BGCOLOR);
+  lcdDrawRect(MENUS_MARGIN_LEFT, LCD_H - 22, LCD_W - 2 * MENUS_MARGIN_LEFT, 15, 1, SOLID, BARGRAPH_BGCOLOR);
   if (num > 0 && den > 0) {
-    int width = ((LCD_W-2*MENUS_MARGIN_LEFT-4) * num) / den;
-    lcdDrawSolidFilledRect(MENUS_MARGIN_LEFT+2, LCD_H-20, width, 11, BARGRAPH1_COLOR);
+    int width = ((LCD_W - 2 * MENUS_MARGIN_LEFT - 4) * num) / den;
+    lcdDrawSolidFilledRect(MENUS_MARGIN_LEFT + 2, LCD_H - 20, width, 11, BARGRAPH1_COLOR);
   }
   lcdRefresh();
 }
