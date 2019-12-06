3bd8e829bc036a947cef4cd0f03ea630e0974f62
3djc <lesitewebdejc@hotmail.com>
2019-12-06 16:33:26 +0100

    Cosmetics

--- radio/src/targets/common/arm/stm32/bootloader/boot.h
+++ radio/src/targets/common/arm/stm32/bootloader/boot.h
@@ -24,10 +24,10 @@
 #include "stamp.h"
 
 #if LCD_W < 212
-#define BOOTLOADER_TITLE               " OTX Bootloader - " VERSION
+  #define BOOTLOADER_TITLE               " OTX Bootloader - " VERSION
 #else
+  #define BOOTLOADER_TITLE               " OpenTX Bootloader - " VERSION
 #endif
-#define BOOTLOADER_TITLE               " OpenTX Bootloader - " VERSION
 #define DISPLAY_CHAR_WIDTH             (LCD_COLS+4)
 
 #if LCD_W >= 480
