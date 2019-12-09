83ebacf90fa50a761f82c2dcfbae4277bc5c34e1
Bertrand Songis <bsongis@gmail.com>
2019-12-09 14:21:50 +0100

    Cosmetics

--- radio/src/strhelpers.h
+++ radio/src/strhelpers.h
@@ -23,7 +23,7 @@
 
 #include "opentx_types.h"
 
-char * strAppend(char * dest, const char * source, int len=0);
+char * strAppend(char * dest, const char * source, int len = 0);
 char * strAppendUnsigned(char * dest, uint32_t value, uint8_t digits=0, uint8_t radix=10);
 char * strAppendSigned(char * dest, int32_t value, uint8_t digits=0, uint8_t radix=10);
 char * strSetCursor(char * dest, int position);
