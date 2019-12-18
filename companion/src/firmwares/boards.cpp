f65c72e0cdcb5bb049dbc178e32ed941d861dd22
3djc <lesitewebdejc@hotmail.com>
2019-12-18 15:13:05 +0100

    Fix x9lite-s having only 5 switches on simu (#7215)
    Fixes #7207

--- companion/src/firmwares/boards.cpp
+++ companion/src/firmwares/boards.cpp
@@ -329,8 +329,10 @@ int Boards::getCapability(Board::Type board, Board::Capability capability)
     case Switches:
       if (IS_TARANIS_X9E(board))
         return 18;
-      else if (IS_TARANIS_X9LITE(board))
+      else if (board == Board::BOARD_TARANIS_X9LITE)
         return 5;
+      else if (board == Board::BOARD_TARANIS_X9LITES)
+        return 7;
       else if (IS_TARANIS_X7(board))
         return 8;
       else if (IS_JUMPER_T12(board))
