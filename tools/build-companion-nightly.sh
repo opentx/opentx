462ba50b0332a3cee5d36ad0cf102a2534ba7fc9
3djc <lesitewebdejc@hotmail.com>
2020-01-07 07:25:15 +0100

    Add internalmulti to T16 simu

--- tools/build-companion-nightly.sh
+++ tools/build-companion-nightly.sh
@@ -94,7 +94,7 @@ cmake ${COMMON_OPTIONS} -DPCB=X10 ${SRCDIR}
 make -j${JOBS} libsimulator
 rm CMakeCache.txt
 
-cmake ${COMMON_OPTIONS} -DPCB=X10 -DPCBREV=T16 ${SRCDIR}
+cmake ${COMMON_OPTIONS} -DPCB=X10 -DPCBREV=T16 -DINTERNAL_MODULE_MULTI=YES ${SRCDIR}
 make -j${JOBS} libsimulator
 rm CMakeCache.txt
 
