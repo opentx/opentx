02669196fd3e951f4252fcb892b4160294f0fb2f
Bertrand Songis <bsongis@gmail.com>
2020-01-13 18:39:50 +0100

    Emergency mode fix

--- radio/src/main.cpp
+++ radio/src/main.cpp
@@ -169,7 +169,8 @@ void checkEeprom()
 {
 #if defined(RTC_BACKUP_RAM) && !defined(SIMU)
   if (TIME_TO_BACKUP_RAM()) {
-    rambackupWrite();
+    if (!globalData.unexpectedShutdown)
+      rambackupWrite();
     rambackupDirtyMsk = 0;
   }
 #endif
