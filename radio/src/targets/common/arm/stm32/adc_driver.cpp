2cd685a011c8da981b7d5aa632a52aae611f2595
3djc <lesitewebdejc@hotmail.com>
2020-01-27 15:27:55 +0100

    Review anadir for tx16s, needs to be checked on more radios

--- radio/src/targets/common/arm/stm32/adc_driver.cpp
+++ radio/src/targets/common/arm/stm32/adc_driver.cpp
@@ -22,8 +22,10 @@
 
 #if defined(SIMU)
   // not needed
-#elif defined(RADIO_T16_FAMILLY)
+#elif defined(RADIO_T16)
   const int8_t adcDirection[NUM_ANALOGS] = {1,-1,1,-1,  1,1,1,  -1,1,  -1,1};
+#elif defined(RADIO_TX16S)
+const int8_t adcDirection[NUM_ANALOGS] = {-1,1,-1,1,  1,1,1,  -1,1,  -1,1};
 #elif defined(PCBX10)
   const int8_t adcDirection[NUM_ANALOGS] = {1,-1,1,-1,  -1,1,-1,   1,1,    1, -1};
 #elif defined(PCBX9E)
