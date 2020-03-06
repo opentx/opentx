9d77d34ad952deed11f7acb56d61c0a7483beb23
3djc <lesitewebdejc@hotmail.com>
2020-03-06 15:54:15 +0100

    Cosmetics

--- radio/src/targets/sky9x/board.h
+++ radio/src/targets/sky9x/board.h
@@ -212,7 +212,6 @@ void calcConsumption();
 
 // Trainer driver
 #define SLAVE_MODE()                   (pwrCheck() == e_power_trainer)
-#define TRAINER_CONNECTED()            (PIOA->PIO_PDSR & PIO_PA8)
 void init_trainer_capture();
 void stop_trainer_capture();
 #define TRAINER_DETECT_GPIO             PIOA
