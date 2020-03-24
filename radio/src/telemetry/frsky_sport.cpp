2860bce01bdc513d017629ac4835808786f3f1c2
Bertrand Songis <bsongis@gmail.com>
2020-03-24 07:49:34 +0100

    Only one test needed, not 4

--- radio/src/telemetry/frsky_sport.cpp
+++ radio/src/telemetry/frsky_sport.cpp
@@ -317,21 +317,9 @@ void sportProcessTelemetryPacketWithoutCrc(uint8_t origin, const uint8_t * packe
           }
 #endif
         }
-        else if (dataId >= RB3040_CH1_2_FIRST_ID && dataId <= RB3040_CH1_2_LAST_ID) {
+        else if (dataId >= RB3040_CH1_2_FIRST_ID && dataId <= RB3040_CH7_8_LAST_ID) {
           sportProcessTelemetryPacket(dataId, 0, instance, data & 0xffff);
-          sportProcessTelemetryPacket(dataId, 1, instance, (data >> 16) & 0xffff);
-        }
-        else if (dataId >= RB3040_CH3_4_FIRST_ID && dataId <= RB3040_CH3_4_LAST_ID) {
-          sportProcessTelemetryPacket(dataId, 0, instance, data & 0xffff);
-          sportProcessTelemetryPacket(dataId, 1, instance, (data >> 16) & 0xffff);
-        }
-        else if (dataId >= RB3040_CH5_6_FIRST_ID && dataId <= RB3040_CH5_6_LAST_ID) {
-          sportProcessTelemetryPacket(dataId, 0, instance, data & 0xffff);
-          sportProcessTelemetryPacket(dataId, 1, instance, (data >> 16) & 0xffff);
-        }
-        else if (dataId >= RB3040_CH7_8_FIRST_ID && dataId <= RB3040_CH7_8_LAST_ID) {
-          sportProcessTelemetryPacket(dataId, 0, instance, data & 0xffff);
-          sportProcessTelemetryPacket(dataId, 1, instance, (data >> 16) & 0xffff);
+          sportProcessTelemetryPacket(dataId, 1, instance, (data >> 16u) & 0xffff);
         }
         else {
           sportProcessTelemetryPacket(dataId, 0, instance, data);
