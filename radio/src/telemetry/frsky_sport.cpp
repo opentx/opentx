28d851714f88bdeb95f90e33ca0a8ccc5e41da65
Bertrand Songis <bsongis@gmail.com>
2020-03-24 15:10:17 +0100

    Revert this for now until this is confirmed as really needed

--- radio/src/telemetry/frsky_sport.cpp
+++ radio/src/telemetry/frsky_sport.cpp
@@ -41,7 +41,7 @@ const FrSkySportSensor sportSensors[] = {
   { A3_FIRST_ID, A3_LAST_ID, 0, ZSTR_A3, UNIT_VOLTS, 2 },
   { A4_FIRST_ID, A4_LAST_ID, 0, ZSTR_A4, UNIT_VOLTS, 2 },
   { BATT_ID, BATT_ID, 0, ZSTR_BATT, UNIT_VOLTS, 1 },
-  { R9_PWR_ID, R9_PWR_ID, 0, ZSTR_R9PW, UNIT_DB, 0 },
+  { R9_PWR_ID, R9_PWR_ID, 0, ZSTR_R9PW, UNIT_MILLIWATTS, 0 },
   { T1_FIRST_ID, T1_LAST_ID, 0, ZSTR_TEMP1, UNIT_CELSIUS, 0 },
   { T2_FIRST_ID, T2_LAST_ID, 0, ZSTR_TEMP2, UNIT_CELSIUS, 0 },
   { RPM_FIRST_ID, RPM_LAST_ID, 0, ZSTR_RPM, UNIT_RPMS, 0 },
@@ -220,7 +220,8 @@ void sportProcessTelemetryPacketWithoutCrc(uint8_t origin, const uint8_t * packe
 #endif
     }
     else if (dataId == R9_PWR_ID) {
-      data = SPORT_DATA_U8(packet);
+      uint32_t r9pwr[] = {100, 200, 500, 1000};
+      data = r9pwr[SPORT_DATA_U8(packet) & 0x03];
     }
     else if (dataId == XJT_VERSION_ID) {
       telemetryData.xjtVersion = HUB_DATA_U16(packet);
