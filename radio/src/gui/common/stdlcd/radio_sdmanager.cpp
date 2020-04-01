3bff803238ad4178efb3a5b48d3a4c0a7095eba7
Bertrand Songis <bsongis@gmail.com>
2020-04-01 08:14:32 +0200

    OTA Update back on Horus (#7477)

--- radio/src/gui/common/stdlcd/radio_sdmanager.cpp
+++ radio/src/gui/common/stdlcd/radio_sdmanager.cpp
@@ -286,12 +286,14 @@ void menuRadioSdManager(event_t _event)
   int lastPos = menuVerticalPosition;
 #endif
 
-  if (moduleState[reusableBuffer.sdManager.otaUpdateInformation.module].mode == MODULE_MODE_BIND && EVT_KEY_MASK(_event) == KEY_EXIT) {
+#if defined(PXX2)
+  if (EVT_KEY_MASK(_event) == KEY_EXIT && moduleState[reusableBuffer.sdManager.otaUpdateInformation.module].mode == MODULE_MODE_BIND) {
     moduleState[reusableBuffer.sdManager.otaUpdateInformation.module].mode = MODULE_MODE_NORMAL;
     CLEAR_POPUP();
     killEvents(KEY_EXIT);
     _event = 0;
   }
+#endif
 
   event_t event = (EVT_KEY_MASK(_event) == KEY_ENTER ? 0 : _event);
   SIMPLE_MENU(SD_IS_HC() ? STR_SDHC_CARD : STR_SD_CARD, menuTabGeneral, MENU_RADIO_SD_MANAGER, HEADER_LINE + reusableBuffer.sdManager.count);
@@ -299,13 +301,13 @@ void menuRadioSdManager(event_t _event)
   switch (_event) {
     case EVT_ENTRY:
       f_chdir(ROOT_PATH);
-      REFRESH_FILES();
 #if LCD_DEPTH > 1
       lastPos = -1;
 #endif
-      break;
+      // no break
 
     case EVT_ENTRY_UP:
+      memclear(&reusableBuffer.sdManager, sizeof(reusableBuffer.sdManager));
       REFRESH_FILES();
       break;
 
