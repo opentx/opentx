cd465529d78a04b341403069a02d2dbf9582393b
Tom van Dijk <tomvand@users.noreply.github.com>
2020-04-08 12:39:03 +0200

    Specify correct gcc version in bash script

--- build-lua-serialread.bash
+++ build-lua-serialread.bash
@@ -1,3 +1,4 @@
+export PATH=/opt/gcc-arm-none-eabi-4_7-2014q2/bin:$PATH
 mkdir -p build-lua-serialread
 cd build-lua-serialread
 cmake -DPCB=X9D+ -DGVARS=YES -DLUA=YES -DUSB_SERIAL=YES -DCMAKE_BUILD_TYPE=Debug ..
