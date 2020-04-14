ad2cbc4d8c44a0f423ea3a36955ae67135a0d90b
Tom van Dijk <tomvand@users.noreply.github.com>
2020-04-14 11:10:11 +0200

    Fix #6589

--- build-lua-serialread.bash
+++
@@ -1,6 +0,0 @@
-export PATH=/opt/gcc-arm-none-eabi-4_7-2014q2/bin:$PATH
-mkdir -p build-lua-serialread
-cd build-lua-serialread
-cmake -DPCB=X9D+ -DGVARS=YES -DLUA=YES -DUSB_SERIAL=YES -DCMAKE_BUILD_TYPE=Debug ..
-make firmware
-make libsimulator && make companion23
