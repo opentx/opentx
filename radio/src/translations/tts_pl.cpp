622ab67ea3b0503732b8aae601fa1d125957fd40
ectogens <rafalski878@o2.pl>
2020-06-03 13:24:12 +0200

    fix tts for polish

--- radio/src/translations/tts_pl.cpp
+++ radio/src/translations/tts_pl.cpp
@@ -18,49 +18,32 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
+ * Fixing polish tts for OpenTX 2.3.2: Rafał Michalski
  */
 
 #include "opentx.h"
 
 enum PolishPrompts {
   PL_PROMPT_NUMBERS_BASE = 0,
-  PL_PROMPT_ZERO = PL_PROMPT_NUMBERS_BASE+0,    //0-99
-  PL_PROMPT_STO = PL_PROMPT_NUMBERS_BASE+100,   //100,200 .. 900
-  PL_PROMPT_TYSIAC = PL_PROMPT_NUMBERS_BASE+109, //1000
-  PL_PROMPT_TYSIACE = PL_PROMPT_NUMBERS_BASE+110,
-  PL_PROMPT_TYSIECY = PL_PROMPT_NUMBERS_BASE+111,
-  PL_PROMPT_JEDNA = PL_PROMPT_NUMBERS_BASE+112,
-  PL_PROMPT_JEDNO = PL_PROMPT_NUMBERS_BASE+113,
-  PL_PROMPT_DWIE = PL_PROMPT_NUMBERS_BASE+114,
-  PL_PROMPT_CALA = PL_PROMPT_NUMBERS_BASE+115,
-  PL_PROMPT_CALE = PL_PROMPT_NUMBERS_BASE+116,
-  PL_PROMPT_CALYCH = PL_PROMPT_NUMBERS_BASE+117,
-  PL_PROMPT_MINUS = PL_PROMPT_NUMBERS_BASE+118,
-  PL_PROMPT_DZIESIATKI_ZENSKIE=PL_PROMPT_NUMBERS_BASE+120, // 22(0122.wav),32(123.wav),42,52,62,72,82,92(129.wav) - this are special female numbers when the unit is female
+  PL_PROMPT_ZERO = PL_PROMPT_NUMBERS_BASE+0,    		// 0-99
+  PL_PROMPT_STO = PL_PROMPT_NUMBERS_BASE+100,   		// 100,200 .. 900
+  PL_PROMPT_TYSIAC = PL_PROMPT_NUMBERS_BASE+109, 		// 1000
+  PL_PROMPT_TYSIACE = PL_PROMPT_NUMBERS_BASE+110,		// TYSIĄCE - thousands variety
+  PL_PROMPT_TYSIECY = PL_PROMPT_NUMBERS_BASE+111,		// TYSIĘCY - thousands variety
+  PL_PROMPT_JEDNA = PL_PROMPT_NUMBERS_BASE+112,			// JEDNA - one variety
+  //PL_PROMPT_JEDNO = PL_PROMPT_NUMBERS_BASE+113,		// JEDNO - one variety
+  PL_PROMPT_DWIE = PL_PROMPT_NUMBERS_BASE+114,			// DWIE - two variety
+  //PL_PROMPT_CALA = PL_PROMPT_NUMBERS_BASE+115,		// CAŁA - whole
+  //PL_PROMPT_CALE = PL_PROMPT_NUMBERS_BASE+116,		// CAŁE - whole
+  //PL_PROMPT_CALYCH = PL_PROMPT_NUMBERS_BASE+117,		// CAŁYCH
+  PL_PROMPT_MINUS = PL_PROMPT_NUMBERS_BASE+118,			// MINUS
 
+  PL_PROMPT_PRZECINEK = PL_PROMPT_NUMBERS_BASE+119,		// PRZECINEK - comma
+
+  PL_PROMPT_DZIESIATKI_ZENSKIE = PL_PROMPT_NUMBERS_BASE+120, 	// 22(0122.wav),32(123.wav),42,52,62,72,82,92(129.wav) - this are special female numbers when the unit is female
+
+  PL_PROMPT_POINT_BASE = 130, 					//,0 - ,9 (0130.wav - 0139.wav)
   PL_PROMPT_UNITS_BASE = 160,
-  PL_PROMPT_VOLTS = PL_PROMPT_UNITS_BASE+UNIT_VOLTS, //(jeden)wolt,(dwa)wolty,(piec+)woltów i tak dla wszytkich unitów
-  PL_PROMPT_AMPS = PL_PROMPT_UNITS_BASE+(UNIT_AMPS*4),
-  PL_PROMPT_METERS_PER_SECOND = PL_PROMPT_UNITS_BASE+(UNIT_METERS_PER_SECOND*4),
-  PL_PROMPT_SPARE1 = PL_PROMPT_UNITS_BASE+(UNIT_RAW*4),
-  PL_PROMPT_KMH = PL_PROMPT_UNITS_BASE+(UNIT_SPEED*4),
-  PL_PROMPT_METERS = PL_PROMPT_UNITS_BASE+(UNIT_DIST*4),
-  PL_PROMPT_DEGREES = PL_PROMPT_UNITS_BASE+(UNIT_TEMPERATURE*4),
-  PL_PROMPT_PERCENT = PL_PROMPT_UNITS_BASE+(UNIT_PERCENT*4),
-  PL_PROMPT_MILLIAMPS = PL_PROMPT_UNITS_BASE+(UNIT_MILLIAMPS*4),
-  PL_PROMPT_MAH = PL_PROMPT_UNITS_BASE+(UNIT_MAH*4),
-  PL_PROMPT_WATTS = PL_PROMPT_UNITS_BASE+(UNIT_WATTS*4),
-  PL_PROMPT_DB = PL_PROMPT_UNITS_BASE+(UNIT_DB*4),
-  PL_PROMPT_FEET = PL_PROMPT_UNITS_BASE+(UNIT_FEET*4),
-  PL_PROMPT_KTS = PL_PROMPT_UNITS_BASE+(UNIT_KTS*4),
-  PL_PROMPT_HOURS = PL_PROMPT_UNITS_BASE+(UNIT_HOURS*4),
-  PL_PROMPT_MINUTES = PL_PROMPT_UNITS_BASE+(UNIT_MINUTES*4),
-  PL_PROMPT_SECONDS = PL_PROMPT_UNITS_BASE+(UNIT_SECONDS*4),
-  PL_PROMPT_RPMS = PL_PROMPT_UNITS_BASE+(UNIT_RPMS*4),
-  PL_PROMPT_G = PL_PROMPT_UNITS_BASE+(UNIT_G*4),
-  PL_PROMPT_MILLILITERS = PL_PROMPT_UNITS_BASE+(UNIT_MILLILITERS*4),
-  PL_PROMPT_FLOZ = PL_PROMPT_UNITS_BASE+(UNIT_FLOZ*4),
-  PL_PROMPT_FEET_PER_SECOND = PL_PROMPT_UNITS_BASE+(UNIT_FEET_PER_SECOND*4),
 };
 
 
@@ -70,22 +53,33 @@ enum PolishPrompts {
 #define ZENSKI 0x81
 #define NIJAKI 0x82
 
+
 I18N_PLAY_FUNCTION(pl, pushUnitPrompt, uint8_t unitprompt, int16_t number)
 {
-  if (number == 1)
-    PUSH_UNIT_PROMPT(unitprompt, 0);
-  else if (number > 1 && number < 5)
-    PUSH_UNIT_PROMPT(unitprompt, 1);
-  else {
-    int test_2 =0;
-    test_2 =number % 10;
-    int ten=0;
-    ten=(number - (number % 10))/10;
-    if ((test_2 > 1 && test_2 < 5) && ten >=2)
-	PUSH_UNIT_PROMPT(unitprompt, 1);
-    else
-	PUSH_UNIT_PROMPT(unitprompt, 2);
+
+    if (number == 1)			        // If value is 1
+    	PUSH_UNIT_PROMPT(unitprompt, 0);
+    
+    else if (number > 1 && number < 5)		// If we have number from 2 to 4
+    	PUSH_UNIT_PROMPT(unitprompt, 1);
+    
+    else if (number < 0)			// if negative number, we have to use 2 units form (for example value = 1.3)
+	PUSH_UNIT_PROMPT(unitprompt, 3);
+
+    else {
+	int mod10 = 0;
+	mod10 = number % 10;
+	int ten=0;
+	ten = (number - (number % 10))/10;
+
+    	if ((mod10 > 1 && mod10 < 5) && ten >=2)
+	    PUSH_UNIT_PROMPT(unitprompt, 1);	//For polish female tens 22,23,24,32,33,34 ...
+	
+    	else
+	    PUSH_UNIT_PROMPT(unitprompt, 2);	//reszta
     }
+
+
 }
 
 I18N_PLAY_FUNCTION(pl, playNumber, getvalue_t number, uint8_t unit, uint8_t att)
@@ -99,53 +93,43 @@ I18N_PLAY_FUNCTION(pl, playNumber, getvalue_t number, uint8_t unit, uint8_t att)
 
   int8_t mode = MODE(att);
   if (mode > 0) {
+#if defined(CPUARM)
     if (mode == 2) {
       number /= 10;
     }
+#else
+    // we assume that we are PREC1
+#endif
     div_t qr = div((int)number, 10);
-    if (qr.rem) {
-      PLAY_NUMBER(qr.quot, 0, ZENSKI);
-      if (qr.quot == 0)
-        PUSH_NUMBER_PROMPT(PL_PROMPT_CALA);
-      else
-        PL_PUSH_UNIT_PROMPT(PL_PROMPT_CALA, qr.quot);
-      PLAY_NUMBER(qr.rem, 0, ZENSKI);
-      PUSH_NUMBER_PROMPT(PL_PROMPT_UNITS_BASE+((unit-1)*4)+3);
-      return;
+    if (qr.rem) {						//if there is rest from dividing
+      PLAY_NUMBER(qr.quot, 0, 0);
+      PUSH_NUMBER_PROMPT(PL_PROMPT_PRZECINEK);			//PUSH_NUMBER_PROMPT(PL_PROMPT_POINT_BASE + qr.rem); //Tutaj potrzeba 9 dźwięków ,0 ,1 ,2 ...
+      PUSH_NUMBER_PROMPT(PL_PROMPT_NUMBERS_BASE + qr.rem);
+      number = -1;
     }
     else {
       number = qr.quot;
     }
   }
 
+
   int16_t tmp = number;
 
   switch(unit) {
-    case 0:
+    case UNIT_RAW:		// case 0
       break;
-// ft/s
-    case 6:
-// mph
-    case 8:
-// feet
-    case 10:
-//mAh
-    case 14:
-//flOz
-    case 21:
-//Hours
-    case 22:
-//Minutes
-    case 23:
-//Seconds
-    case 24:
-      att = ZENSKI;
-      break;
-    case 100:
-      att = NIJAKI;
+    case UNIT_FEET_PER_SECOND: 	// case 6
+    case UNIT_MPH:		// case 8
+    case UNIT_FEET:		// case 10
+    case UNIT_MAH:		// case 14
+    case UNIT_FLOZ:		// case 21
+    case UNIT_HOURS:		// case 22
+    case UNIT_MINUTES:		// case 23
+    case UNIT_SECONDS:		// case 24
+      att = ZENSKI; 		//JEDNA, DWIE
       break;
     default:
-      att = MESKI;
+      att = MESKI;		//JEDEN
       break;
   }
 
@@ -154,16 +138,12 @@ I18N_PLAY_FUNCTION(pl, playNumber, getvalue_t number, uint8_t unit, uint8_t att)
     number = -1;
   }
 
-  if ((number == 1) && (att == NIJAKI)) {
-    PUSH_NUMBER_PROMPT(PL_PROMPT_JEDNO);
-    number = -1;
-  }
-
   if ((number == 2) && (att == ZENSKI)) {
     PUSH_NUMBER_PROMPT(PL_PROMPT_DWIE);
     number = -1;
   }
 
+// ==== TYSIĄCE =======
   if (number >= 1000) {
     if (number >= 2000)
       PLAY_NUMBER(number / 1000, 0, 0);
@@ -186,6 +166,7 @@ I18N_PLAY_FUNCTION(pl, playNumber, getvalue_t number, uint8_t unit, uint8_t att)
       number = -1;
   }
 
+// === For polish female tens (22,23,23,32,33,33 ...) ===
   if (number >= 0) {
     int test_2 =0;
     test_2 =number % 10;
@@ -197,6 +178,7 @@ I18N_PLAY_FUNCTION(pl, playNumber, getvalue_t number, uint8_t unit, uint8_t att)
     }else
        PUSH_NUMBER_PROMPT(PL_PROMPT_ZERO+number);
   }
+// =========
 
   if (unit) {
     PL_PUSH_UNIT_PROMPT(unit, tmp);
@@ -230,4 +212,3 @@ I18N_PLAY_FUNCTION(pl, playDuration, int seconds PLAY_DURATION_ATT)
 }
 
 LANGUAGE_PACK_DECLARE(pl, "Polish");
-
