4cca618f1ec0b8a90f54fa4a2d544a97183f82f2
Bertrand Songis <bsongis@gmail.com>
2020-06-15 16:03:20 +0200

    Fixes #7739

--- radio/src/audio.h
+++ radio/src/audio.h
@@ -18,8 +18,8 @@
  * GNU General Public License for more details.
  */
 
-#ifndef _AUDIO_ARM_H_
-#define _AUDIO_ARM_H_
+#ifndef _AUDIO_H_
+#define _AUDIO_H_
 
 #include <stddef.h>
 #include "ff.h"
@@ -64,7 +64,11 @@ template <unsigned int NUM_BITS> class BitField {
 #define INDEX_LOGICAL_SWITCH_AUDIO_FILE(index, event) (2*(index)+(event))
 #define INDEX_PHASE_AUDIO_FILE(index, event)          (2*(index)+(event))
 
-constexpr uint8_t AUDIO_FILENAME_MAXLEN = (sizeof("/SOUNDS/fr/") - 1) + LEN_MODEL_NAME + 1 + LEN_FLIGHT_MODE_NAME + (sizeof("-off.wav") - 1); // max length (example: /SOUNDS/fr/123456789012/1234567890-off.wav)
+// max length (example: /SOUNDS/fr/123456789012/1234567890-off.wav)
+constexpr uint8_t AUDIO_MODEL_FILENAME_MAXLEN = (sizeof("/SOUNDS/fr/") - 1) + LEN_MODEL_NAME + 1 + LEN_FLIGHT_MODE_NAME + (sizeof("-off.wav") - 1);
+constexpr uint8_t AUDIO_LUA_FILENAME_MAXLEN = 42;
+constexpr uint8_t AUDIO_FILENAME_MAXLEN = (AUDIO_LUA_FILENAME_MAXLEN > AUDIO_MODEL_FILENAME_MAXLEN ? AUDIO_LUA_FILENAME_MAXLEN : AUDIO_MODEL_FILENAME_MAXLEN);
+
 #define AUDIO_QUEUE_LENGTH             (16) // must be a power of 2!
 
 #define AUDIO_SAMPLE_RATE              (32000)
@@ -652,4 +656,4 @@ void referenceModelAudioFiles();
 
 bool isAudioFileReferenced(uint32_t i, char * filename/*at least AUDIO_FILENAME_MAXLEN+1 long*/);
 
-#endif // _AUDIO_ARM_H_
+#endif // _AUDIO_H_
