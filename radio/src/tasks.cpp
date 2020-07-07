71ce79bad78c2b416c58a7522b9c379b714cc880
3djc <lesitewebdejc@hotmail.com>
2020-07-07 15:55:08 +0200

    Improve

--- radio/src/tasks.cpp
+++ radio/src/tasks.cpp
@@ -107,7 +107,7 @@ TASK_FUNCTION(mixerTask)
   s_pulses_paused = true;
 
   while (true) {
-#if (defined(PCBTARANIS) || defined(RADIO_TX16S)) && defined(SBUS)
+#if defined(SBUS) && !defined(PCBSKY9X)
     // SBUS trainer
     processSbusInput();
 #endif
