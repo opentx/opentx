dcdc44e29d532a35bbc0b0cc843fa20340323efa
3djc <lesitewebdejc@hotmail.com>
2020-08-12 22:57:00 +0200

    Missing files

--- companion/src/firmwares/opentx/opentxeeprom.h
+++ companion/src/firmwares/opentx/opentxeeprom.h
@@ -107,6 +107,7 @@ class ProtocolsConversionTable: public ConversionTable
       addConversion(PULSES_XJT_LITE_X16, val);
       addConversion(PULSES_XJT_LITE_D8, val);
       addConversion(PULSES_XJT_LITE_LR12, val++);
+
       addConversion(PULSES_GHOST, val++);
     }
 };
