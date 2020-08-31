661604340de07a559340d687affcf149c21dfde6
elecpower <neilh713@tpg.com.au>
2020-08-31 19:47:11 +1000

    Allow external module on all ACCESS radios

--- companion/src/firmwares/opentx/opentxinterface.cpp
+++ companion/src/firmwares/opentx/opentxinterface.cpp
@@ -763,7 +763,7 @@ bool OpenTxFirmware::isAvailable(PulsesProtocol proto, int port)
           case PULSES_GHOST:
             return true;
           case PULSES_ACCESS_R9M:
-            return IS_TARANIS_XLITE(board) || IS_TARANIS_X7_ACCESS(board) || IS_TARANIS_X9LITE(board) || board == BOARD_TARANIS_X9DP_2019 || board == BOARD_X10_EXPRESS || (IS_FAMILY_HORUS_OR_T16(board) && id.contains("internalaccess"));
+            return IS_ACCESS_RADIO(board, id);
           case PULSES_PXX_R9M_LITE:
           case PULSES_ACCESS_R9M_LITE:
           case PULSES_ACCESS_R9M_LITE_PRO:
