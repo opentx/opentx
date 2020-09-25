3715669b9155ec8abac703252c90f1b8644b87c2
3djc <lesitewebdejc@hotmail.com>
2020-09-25 20:48:14 +0200

    x9lites also can't draw to many h lines

--- radio/src/gui/128x64/widgets.cpp
+++ radio/src/gui/128x64/widgets.cpp
@@ -67,10 +67,10 @@ void drawVerticalScrollbar(coord_t x, coord_t y, coord_t h, uint16_t offset, uin
 
 void drawGauge(coord_t x, coord_t y, coord_t w, coord_t h, int32_t val, int32_t max)
 {
-#if !defined(PCBX7)  // X7 LCD doesn't like too many horizontal lines
-  lcdDrawRect(x, y, w+1, h);
-#else
+#if defined(PCBX7) || defined(PCBX9LITES) // X7 LCD doesn't like too many horizontal lines
   h++;
+#else
+  lcdDrawRect(x, y, w+1, h);
 #endif
   lcdDrawFilledRect(x+1, y+1, w-1, 4, SOLID, ERASE);
   coord_t len = limit((uint8_t)1, uint8_t((abs(val) * w/2 + max/2) / max), uint8_t(w/2));
