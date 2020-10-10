2ecb049e0e8368442dac7f3666a73b21860a618e
3djc <lesitewebdejc@hotmail.com>
2020-10-10 19:29:40 +0200

    Please travis

--- radio/src/targets/taranis/board.h
+++ radio/src/targets/taranis/board.h
@@ -660,6 +660,7 @@ void pwrResetHandler();
 void backlightInit();
 void backlightDisable();
 #define BACKLIGHT_DISABLE()             backlightDisable()
+#define BACKLIGHT_OLD_MODE              101
 uint8_t isBacklightEnabled();
 #if !defined(__cplusplus)
   #define backlightEnable(...)
