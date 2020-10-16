21e8814961292f673410d58e8ebe4006e436d5b9
3djc <lesitewebdejc@hotmail.com>
2020-10-16 16:22:51 +0200

    cosmetics

--- radio/src/targets/horus/board.h
+++ radio/src/targets/horus/board.h
@@ -547,7 +547,7 @@ void backlightInit();
 void backlightEnable(uint8_t dutyCycle = 0);
 #endif
 #define BACKLIGHT_LEVEL_MAX     100
-#define BACKLIGHT_FORCED_ON   BACKLIGHT_LEVEL_MAX + 1
+#define BACKLIGHT_FORCED_ON     BACKLIGHT_LEVEL_MAX + 1
 #if defined(PCBX12S)
 #define BACKLIGHT_LEVEL_MIN   5
 #elif defined(RADIO_FAMILY_T16)
