8afb48eee2b4700514d7bc800cf36d6fc23706a2
3djc <lesitewebdejc@hotmail.com>
2020-11-07 13:00:33 +0100

    Stop trainers before changing model (#8075)
    Turn off SBUS trainer to allow model switch

--- radio/src/trainer.cpp
+++ radio/src/trainer.cpp
@@ -48,6 +48,11 @@ void checkTrainerSignalWarning()
 }
 
 #if defined(PCBSKY9X)
+void stopTrainer()
+{
+  stop_trainer_capture();
+}
+
 void checkTrainerSettings()
 {
   uint8_t requiredTrainerMode = SLAVE_MODE();
@@ -55,50 +60,59 @@ void checkTrainerSettings()
   if (requiredTrainerMode != currentTrainerMode) {
     currentTrainerMode = requiredTrainerMode;
     if (requiredTrainerMode)
-      stop_trainer_capture();
+      stopTrainer();
     else
       init_trainer_capture();
   }
 }
 #else
+void stopTrainer()
+{
+  switch (currentTrainerMode) {
+    case TRAINER_MODE_MASTER_TRAINER_JACK:
+      stop_trainer_capture();
+      break;
+
+    case TRAINER_MODE_SLAVE:
+      stop_trainer_ppm();
+      break;
+
+#if defined(TRAINER_MODULE_CPPM)
+    case TRAINER_MODE_MASTER_CPPM_EXTERNAL_MODULE:
+      stop_trainer_module_cppm();
+      break;
+#endif
+
+#if defined(TRAINER_MODULE_SBUS)
+    case TRAINER_MODE_MASTER_SBUS_EXTERNAL_MODULE:
+      stop_trainer_module_sbus();
+      break;
+#endif
+
+#if defined(TRAINER_BATTERY_COMPARTMENT)
+    case TRAINER_MODE_MASTER_BATTERY_COMPARTMENT:
+#if defined(AUX_SERIAL)
+      if (g_eeGeneral.auxSerialMode == UART_MODE_SBUS_TRAINER)
+        auxSerialStop();
+#endif
+#if defined(AUX2_SERIAL)
+      if (g_eeGeneral.aux2SerialMode == UART_MODE_SBUS_TRAINER)
+        aux2SerialStop();
+#endif
+      break;
+#endif
+  }
+
+  currentTrainerMode = 0xFF;
+}
+
 void checkTrainerSettings()
 {
   uint8_t requiredTrainerMode = g_model.trainerData.mode;
 
   if (requiredTrainerMode != currentTrainerMode) {
-    switch (currentTrainerMode) {
-      case TRAINER_MODE_MASTER_TRAINER_JACK:
-        stop_trainer_capture();
-        break;
-
-      case TRAINER_MODE_SLAVE:
-        stop_trainer_ppm();
-        break;
-
-#if defined(TRAINER_MODULE_CPPM)
-      case TRAINER_MODE_MASTER_CPPM_EXTERNAL_MODULE:
-        stop_trainer_module_cppm();
-        break;
-#endif
-
-#if defined(TRAINER_MODULE_SBUS)
-      case TRAINER_MODE_MASTER_SBUS_EXTERNAL_MODULE:
-        stop_trainer_module_sbus();
-        break;
-#endif
-
-#if defined(TRAINER_BATTERY_COMPARTMENT)
-      case TRAINER_MODE_MASTER_BATTERY_COMPARTMENT:
-#if defined(AUX_SERIAL)
-        if (g_eeGeneral.auxSerialMode == UART_MODE_SBUS_TRAINER)
-          auxSerialStop();
-#endif
-#if defined(AUX2_SERIAL)
-        if (g_eeGeneral.aux2SerialMode == UART_MODE_SBUS_TRAINER)
-          aux2SerialStop();
-#endif
-        break;
-#endif
+    if (currentTrainerMode != 0xFF) {
+      stopTrainer();
     }
 
     currentTrainerMode = requiredTrainerMode;
