5e3bfc221f1a45af5ecbefd02de7742139dc892c
3djc <lesitewebdejc@hotmail.com>
2020-11-12 10:53:14 +0100

    Add b&w lcd support

---
+++ radio/src/gui/common/stdlcd/radio_ghost_menu.cpp
@@ -0,0 +1,107 @@
+/*
+ * Copyright (C) OpenTX
+ *
+ * Based on code named
+ *   th9x - http://code.google.com/p/th9x
+ *   er9x - http://code.google.com/p/er9x
+ *   gruvin9x - http://code.google.com/p/gruvin9x
+ *
+ * License GPLv2: http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include "opentx.h"
+
+void menuRadioGhostMenu(event_t event)
+{
+  switch (event) {
+    case EVT_ENTRY:
+      memclear(&reusableBuffer.ghostMenu, sizeof(reusableBuffer.ghostMenu));
+      reusableBuffer.ghostMenu.buttonAction = GHST_BTN_None;
+      reusableBuffer.ghostMenu.menuAction = GHST_MENU_CTRL_Open;
+      moduleState[EXTERNAL_MODULE].counter = GHST_MENU_CONTROL;
+      break;
+
+#if defined(ROTARY_ENCODER_NAVIGATION)
+    case EVT_ROTARY_LEFT:
+#elif defined(KEY_UP)
+    case EVT_KEY_BREAK(KEY_UP):
+#endif
+      reusableBuffer.ghostMenu.buttonAction = GHST_BTN_JoyUp;
+      reusableBuffer.ghostMenu.menuAction = GHST_MENU_CTRL_None;
+      moduleState[EXTERNAL_MODULE].counter = GHST_MENU_CONTROL;
+      break;
+
+#if defined(ROTARY_ENCODER_NAVIGATION)
+    case EVT_ROTARY_RIGHT:
+#elif defined(KEY_DOWN)
+      case EVT_KEY_BREAK(KEY_DOWN):
+#endif
+      reusableBuffer.ghostMenu.buttonAction = GHST_BTN_JoyDown;
+      reusableBuffer.ghostMenu.menuAction = GHST_MENU_CTRL_None;
+      moduleState[EXTERNAL_MODULE].counter = GHST_MENU_CONTROL;
+      break;
+
+
+    case EVT_KEY_FIRST(KEY_ENTER):
+      reusableBuffer.ghostMenu.buttonAction = GHST_BTN_JoyPress;
+      reusableBuffer.ghostMenu.menuAction = GHST_MENU_CTRL_None;
+      moduleState[EXTERNAL_MODULE].counter = GHST_MENU_CONTROL;
+      break;
+
+    case EVT_KEY_BREAK(KEY_EXIT):
+      reusableBuffer.ghostMenu.buttonAction = GHST_BTN_JoyLeft;
+      reusableBuffer.ghostMenu.menuAction = GHST_MENU_CTRL_None;
+      moduleState[EXTERNAL_MODULE].counter = GHST_MENU_CONTROL;
+      break;
+
+    case EVT_KEY_LONG(KEY_EXIT):
+      menuVerticalOffset = 0;
+      memclear(&reusableBuffer.ghostMenu, sizeof(reusableBuffer.ghostMenu));
+      reusableBuffer.ghostMenu.buttonAction = GHST_BTN_None;
+      reusableBuffer.ghostMenu.menuAction = GHST_MENU_CTRL_Close;
+      moduleState[EXTERNAL_MODULE].counter = GHST_MENU_CONTROL;
+      RTOS_WAIT_MS(10);
+      popMenu();
+      break;
+  }
+
+  if (reusableBuffer.ghostMenu.menuAction ==  GHST_MENU_CTRL_Close)
+    popMenu();
+
+  lcdDrawText(0, 0, "GHOST MENU", INVERS);
+
+  constexpr coord_t xOffset = LCD_W / 3 - 5;
+  constexpr coord_t xOffset2 = xOffset + LCD_W / 4;
+  constexpr coord_t yOffset = 12;
+  LcdFlags flags = 0;
+
+  for (uint8_t line = 0; line < GHST_MENU_LINES; line++) {
+    flags = 0;
+    if (reusableBuffer.ghostMenu.line[line].splitLine) {
+      if (reusableBuffer.ghostMenu.line[line].lineFlags & GHST_LINE_FLAGS_LabelSelect)
+        flags = INVERS;
+      lcdDrawText(xOffset, yOffset + line * FH, reusableBuffer.ghostMenu.line[line].menuText, flags);
+
+      flags = 0;
+      if (reusableBuffer.ghostMenu.line[line].lineFlags & GHST_LINE_FLAGS_ValueSelect)
+        flags |= INVERS;
+      if (reusableBuffer.ghostMenu.line[line].lineFlags & GHST_LINE_FLAGS_ValueEdit)
+        flags |= BLINK;
+      lcdDrawText(xOffset2, yOffset + line * FH, &reusableBuffer.ghostMenu.line[line].menuText[reusableBuffer.ghostMenu.line[line].splitLine], flags);
+    }
+    else {
+      if (reusableBuffer.ghostMenu.line[line].lineFlags & GHST_LINE_FLAGS_LabelSelect)
+        flags = INVERS;
+      lcdDrawText(xOffset, yOffset + line * FH, reusableBuffer.ghostMenu.line[line].menuText, flags);
+    }
+  }
+}
