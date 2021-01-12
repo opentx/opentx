82074137479de19b707cd774e2fd8186a3b0ce74
3djc <Th1sisfmyofmyo>
2021-01-12 18:00:36 +0100

    Compile fix

--- radio/src/targets/common/arm/stm32/usbd_desc.c
+++ radio/src/targets/common/arm/stm32/usbd_desc.c
@@ -62,9 +62,9 @@
 #define USBD_SERIALNUMBER_FS_STRING         "00000000001B"
 
 #if defined(BOOT)
-  #define USBD_MSC_PRODUCT_FS_STRING          USB_NAME " Bootloader"
+#define USBD_MSC_PRODUCT_FS_STRING          USB_NAME " Bootloader"
 #else
-  #define USBD_MSC_PRODUCT_FS_STRING          USB_NAME " Mass Storage"
+#define USBD_MSC_PRODUCT_FS_STRING          USB_NAME " Mass Storage"
 #endif
 
 #define USBD_MSC_VID                        USBD_VID_STM
@@ -85,40 +85,40 @@
 #define USBD_CDC_INTERFACE_FS_STRING        "VSP Interface"
 
 const USBD_DEVICE USR_desc =
-{
-  USBD_USR_DeviceDescriptor,
-  USBD_USR_LangIDStrDescriptor,
-  USBD_USR_ManufacturerStrDescriptor,
-  USBD_USR_ProductStrDescriptor,
-  USBD_USR_SerialStrDescriptor,
-  USBD_USR_ConfigStrDescriptor,
-  USBD_USR_InterfaceStrDescriptor,
-};
+  {
+    USBD_USR_DeviceDescriptor,
+    USBD_USR_LangIDStrDescriptor,
+    USBD_USR_ManufacturerStrDescriptor,
+    USBD_USR_ProductStrDescriptor,
+    USBD_USR_SerialStrDescriptor,
+    USBD_USR_ConfigStrDescriptor,
+    USBD_USR_InterfaceStrDescriptor,
+  };
 
 
 /* USB Standard Device Descriptor */
 __ALIGN_BEGIN const uint8_t USBD_DeviceQualifierDesc[USB_LEN_DEV_QUALIFIER_DESC] __ALIGN_END =
-{
-  USB_LEN_DEV_QUALIFIER_DESC,
-  USB_DESC_TYPE_DEVICE_QUALIFIER,
-  0x00,
-  0x02,
-  0x00,
-  0x00,
-  0x00,
-  0x40,
-  0x01,
-  0x00,
-};
+  {
+    USB_LEN_DEV_QUALIFIER_DESC,
+    USB_DESC_TYPE_DEVICE_QUALIFIER,
+    0x00,
+    0x02,
+    0x00,
+    0x00,
+    0x00,
+    0x40,
+    0x01,
+    0x00,
+  };
 
 /* USB Standard Device Descriptor */
 __ALIGN_BEGIN const  uint8_t USBD_LangIDDesc[USB_SIZ_STRING_LANGID] __ALIGN_END =
-{
-     USB_SIZ_STRING_LANGID,
-     USB_DESC_TYPE_STRING,
-     LOBYTE(USBD_LANGID_STRING),
-     HIBYTE(USBD_LANGID_STRING),
-};
+  {
+    USB_SIZ_STRING_LANGID,
+    USB_DESC_TYPE_STRING,
+    LOBYTE(USBD_LANGID_STRING),
+    HIBYTE(USBD_LANGID_STRING),
+  };
 
 __ALIGN_BEGIN uint8_t USBD_StrDesc[USB_MAX_STR_DESC_SIZ] __ALIGN_END ;	// modified by OpenTX
 
@@ -228,10 +228,8 @@ uint8_t *  USBD_USR_ProductStrDescriptor( uint8_t speed , uint16_t *length)
 * @param  length : pointer to data length variable
 * @retval pointer to descriptor buffer
 */
-static const char USBD_MANUFACTURER_STRING[] = { USB_MANUFACTURER };
 uint8_t *  USBD_USR_ManufacturerStrDescriptor( uint8_t speed , uint16_t *length)
 {
-
   USBD_GetString ((uint8_t*)USBD_MANUFACTURER_STRING, USBD_StrDesc, length);
   return USBD_StrDesc;
 }
