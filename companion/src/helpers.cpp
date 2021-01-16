5b2c091a276590a2064e1b0f56e7418bfc44f0f6
sfjuocekr <sfjuocekr@gmail.com>
2021-01-17 00:02:23 +0100

    Reverted not yet deprecated QStringList::fromSet

--- companion/src/helpers.cpp
+++ companion/src/helpers.cpp
@@ -275,7 +275,8 @@ void Helpers::populateFileComboBox(QComboBox * b, const QSet<QString> & set, con
 
   bool added = false;
   // Convert set into list and sort it alphabetically case insensitive
-  QStringList list = QList<QString>(set.begin(), set.end());
+  //QStringList list = QList<QString>(set.begin(), set.end());
+  QStringList list = QStringList::fromSet(set);
   std::sort(list.begin(), list.end(), caseInsensitiveLessThan);
   foreach (QString entry, list) {
     b->addItem(entry);
