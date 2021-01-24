92479949a9bb4ea9570f058886896a2d7395423c
Bertrand Songis <bsongis@gmail.com>
2021-01-24 22:25:33 +0100

    Warning removed

--- radio/src/lua/api_general.cpp
+++ radio/src/lua/api_general.cpp
@@ -1608,7 +1608,9 @@ Set baudrate for serial port(s) affected to LUA
 */
 static int luaSetSerialBaudrate(lua_State * L)
 {
+#if defined(AUX_SERIAL) || defined(AUX2_SERIAL)
   unsigned int baudrate = luaL_checkunsigned(L, 1);
+#endif
 
 #if defined(AUX_SERIAL)
   if (auxSerialMode == UART_MODE_LUA) {
