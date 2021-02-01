20ff2328342da7c73855e148b385cc1e95add78d
Bertrand Songis <bsongis@gmail.com>
2021-02-01 23:19:04 +0100

    All my comments from the PR fixed. Still missing the Companion part

--- radio/src/datastructs.h
+++ radio/src/datastructs.h
@@ -451,7 +451,7 @@ PACK(struct ModuleData {
       int8_t refreshRate;  // definition as framelength for ppm (* 5 + 225 = time in 1/10 ms)
     } sbus);
     NOBACKUP(PACK(struct {
-      uint8_t receiver:7; // 4 bits spare
+      uint8_t receivers:7; // 4 bits spare
       uint8_t racingMode:1;
       char receiverName[PXX2_MAX_RECEIVERS_PER_MODULE][PXX2_LEN_RX_NAME];
     }) pxx2);
