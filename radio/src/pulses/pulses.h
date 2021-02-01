c5f3445f04ec56cba90da0022750ff9ad86033fe
Bertrand Songis <bsongis@gmail.com>
2021-02-01 23:03:15 +0100

    All my comments from the PR fixed. Still missing the Companion part

--- radio/src/pulses/pulses.h
+++ radio/src/pulses/pulses.h
@@ -142,8 +142,7 @@ PACK(struct ModuleState {
   uint8_t protocol:4;
   uint8_t mode:4;
   uint8_t paused:1;
-  uint8_t racingMode:1;
-  uint8_t spare:6;
+  uint8_t spare:7;
   uint16_t counter;
   union
   {
