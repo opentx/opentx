8b61ce5ce75836fd8a081fbce46b7d65bcdc4228
elecpower <neilh713@tpg.com.au>
2021-05-12 06:12:27 +1000

    New autolabel widget

---
+++ companion/src/shared/autolabel.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) OpenTX
+ *
+ * Based on code named
+ *   th9x - http://code.google.com/p/th9x
+ *   er9x - http://code.google.com/p/er9x
+ *   gruvin9x - http://code.google.com/p/gruvin9x
+ *
+ * License GPLv2: http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#pragma once
+
+#include "autowidget.h"
+
+#include <QLabel>
+
+class AutoLabel: public QLabel, AutoWidget
+{
+  Q_OBJECT
+
+  public:
+    explicit AutoLabel(QWidget * parent = nullptr);
+    virtual ~AutoLabel();
+
+    virtual void updateValue() override;
+
+    void setField(char * field, GenericPanel * panel = nullptr);
+    void setField(QString & field, GenericPanel * panel = nullptr);
+    void setWidth(int numChars);
+
+  private:
+    char *m_charField;
+    QString *m_strField;
+};
