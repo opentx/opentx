cfc7c0ea86242b5c10dc18cf4560246b706111e3
3djc <lesitewebdejc@hotmail.com>
2021-05-11 18:15:25 +0200

    Improved sync

--- radio/src/mixer_scheduler.h
+++ radio/src/mixer_scheduler.h
@@ -43,6 +43,9 @@ void mixerSchedulerResetTimer();
 // Set the scheduling period for a given module
 void mixerSchedulerSetPeriod(uint8_t moduleIdx, uint16_t periodUs);
 
+// Clear the flag before waiting
+void mixerSchedulerClearTrigger();
+
 // Wait for the scheduler timer to trigger
 // returns true if timeout, false otherwise
 bool mixerSchedulerWaitForTrigger(uint8_t timeoutMs);
@@ -66,6 +69,7 @@ void mixerSchedulerISRTrigger();
 #define mixerSchedulerStop()
 #define mixerSchedulerResetTimer()
 #define mixerSchedulerSetPeriod(m,p)
+#define mixerSchedulerClearTrigger()
 
 static inline bool mixerSchedulerWaitForTrigger(uint8_t timeout)
 {
