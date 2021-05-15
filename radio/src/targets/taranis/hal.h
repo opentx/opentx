2d36ef626c3ead532a1e4a0c7c77ee4a5883fcee
3djc <lesitewebdejc@hotmail.com>
2021-05-15 08:30:59 +0200

    Fix #8510

--- radio/src/targets/taranis/hal.h
+++ radio/src/targets/taranis/hal.h
@@ -2034,7 +2034,7 @@
   #define AUDIO_SPEAKER_ENABLE_GPIO_PIN GPIO_Pin_14 // PD.14
   #define HEADPHONE_TRAINER_SWITCH_GPIO      GPIOD
   #define HEADPHONE_TRAINER_SWITCH_GPIO_PIN  GPIO_Pin_9 // PD.09
-#elif defined(AUDIO_MUTE_GPIO)
+#elif defined(RADIO_TX12) || defined(RADIO_T8) || defined(RADIO_TLITE)
   #define AUDIO_RCC_AHB1Periph          (RCC_AHB1Periph_GPIOE | RCC_AHB1Periph_GPIOA | RCC_AHB1Periph_DMA1)
   #define AUDIO_MUTE_GPIO               GPIOE
   #define AUDIO_MUTE_GPIO_PIN           GPIO_Pin_12  // PE.12
