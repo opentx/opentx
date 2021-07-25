10d34e5f29efea2f6ea574219c427c1725db5fda
3djc <lesitewebdejc@hotmail.com>
2021-07-25 17:47:06 +0200

    Still 1k missing on x7

--- radio/src/FreeRTOSConfig.h
+++ radio/src/FreeRTOSConfig.h
@@ -1,8 +1,7 @@
 /*
- * Copyright (C) EdgeTx
+ * Copyright (C) OpenTX
  *
  * Based on code named
- *   opentx - https://github.com/opentx/opentx
  *   th9x - http://code.google.com/p/th9x
  *   er9x - http://code.google.com/p/er9x
  *   gruvin9x - http://code.google.com/p/gruvin9x
@@ -21,9 +20,7 @@
 
 #pragma once
 
-#if !defined(PCBSKY9X)
 #include "board_common.h"
-#endif
 
 #define configUSE_PREEMPTION            1
 #define configUSE_IDLE_HOOK             0
@@ -33,17 +30,17 @@
 #define configMAX_PRIORITIES            ( 5 )
 #define configMINIMAL_STACK_SIZE        ( ( unsigned short ) 130 )
 //#define configTOTAL_HEAP_SIZE           ( ( size_t ) ( 75 * 1024 ) )
-#define configMAX_TASK_NAME_LEN         ( 10 )
-#define configUSE_TRACE_FACILITY        1
+#define configMAX_TASK_NAME_LEN         1
+#define configUSE_TRACE_FACILITY        0
 #define configUSE_16_BIT_TICKS          0
 #define configIDLE_SHOULD_YIELD         1
-#define configUSE_MUTEXES               1 // ???
-#define configQUEUE_REGISTRY_SIZE       8
+#define configUSE_MUTEXES               0
+#define configQUEUE_REGISTRY_SIZE       0
 #define configCHECK_FOR_STACK_OVERFLOW  0
-#define configUSE_RECURSIVE_MUTEXES     1
+#define configUSE_RECURSIVE_MUTEXES     0
 #define configUSE_MALLOC_FAILED_HOOK    0
 #define configUSE_APPLICATION_TASK_TAG  0
-#define configUSE_COUNTING_SEMAPHORES   1
+#define configUSE_COUNTING_SEMAPHORES   0
 #define configGENERATE_RUN_TIME_STATS   0
 
 #define configSUPPORT_DYNAMIC_ALLOCATION 0
@@ -54,27 +51,27 @@
 #define configMAX_CO_ROUTINE_PRIORITIES ( 2 )
 
 /* Software timer definitions. */
-#define configUSE_TIMERS                1
+#define configUSE_TIMERS                0
 #define configTIMER_TASK_PRIORITY       ( 2 )
 #define configTIMER_QUEUE_LENGTH        10
 #define configTIMER_TASK_STACK_DEPTH    ( configMINIMAL_STACK_SIZE * 2 )
 
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
-#define INCLUDE_vTaskPrioritySet        1
-#define INCLUDE_uxTaskPriorityGet       1
-#define INCLUDE_vTaskDelete             1
-#define INCLUDE_vTaskCleanUpResources   1
+//#define INCLUDE_vTaskPrioritySet        1
+//#define INCLUDE_uxTaskPriorityGet       1
+//#define INCLUDE_vTaskDelete             1
+//#define INCLUDE_vTaskCleanUpResources   1
 #define INCLUDE_vTaskSuspend            1
 #define INCLUDE_vTaskDelayUntil         1
 #define INCLUDE_vTaskDelay              1
 
 /* Cortex-M specific definitions. */
 #ifdef __NVIC_PRIO_BITS
-	/* __BVIC_PRIO_BITS will be specified when CMSIS is being used. */
-	#define configPRIO_BITS       		__NVIC_PRIO_BITS
+/* __BVIC_PRIO_BITS will be specified when CMSIS is being used. */
+#define configPRIO_BITS       		__NVIC_PRIO_BITS
 #else
-	#define configPRIO_BITS       		4        /* 15 priority levels */
+#define configPRIO_BITS       		4        /* 15 priority levels */
 #endif
 
 /* The lowest interrupt priority that can be used in a call to a "set priority"
@@ -93,11 +90,11 @@ to all Cortex-M ports, and do not rely on any particular library functions. */
 /* !!!! configMAX_SYSCALL_INTERRUPT_PRIORITY must not be set to zero !!!!
 See http://www.FreeRTOS.org/RTOS-Cortex-M3-M4.html. */
 #define configMAX_SYSCALL_INTERRUPT_PRIORITY 	( configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY << (8 - configPRIO_BITS) )
-	
+
 /* Normal assert() semantics without relying on the provision of an assert.h
 header file. */
-#define configASSERT( x ) if( ( x ) == 0 ) { taskDISABLE_INTERRUPTS(); for( ;; ); }	
-	
+#define configASSERT( x ) if( ( x ) == 0 ) { taskDISABLE_INTERRUPTS(); for( ;; ); }
+
 /* Definitions that map the FreeRTOS port interrupt handlers to their CMSIS
 standard names. */
 #define vPortSVCHandler SVC_Handler
