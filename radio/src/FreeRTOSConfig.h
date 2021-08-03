41d2a99457a7c6c1bcab7a0cc9a215d4a4d772ef
3djc <lesitewebdejc@hotmail.com>
2021-08-03 11:43:56 +0200

    We do not delete tasks

--- radio/src/FreeRTOSConfig.h
+++ radio/src/FreeRTOSConfig.h
@@ -72,7 +72,7 @@ extern uint32_t SystemCoreClock;
 
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
-#define INCLUDE_vTaskDelete             1
+#define INCLUDE_vTaskDelete             0
 #define INCLUDE_vTaskSuspend            1
 #define INCLUDE_vTaskDelayUntil         1
 #define INCLUDE_vTaskDelay              1
