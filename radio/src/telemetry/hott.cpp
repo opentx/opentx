35771dc9109caafd9851849bbd83ffbb9b664099
Michael <101.37584@germanynet.de>
2021-10-29 19:29:24 +0200

    refined code to suppress working with empty packets

--- radio/src/telemetry/hott.cpp
+++ radio/src/telemetry/hott.cpp
@@ -15,36 +15,90 @@
 
 #include "opentx.h"
 
-/* Telemetry 
-packet[0]    = TX RSSI value
-packet[1]    = TX LQI value
-packet[2]    = Telemetry type
-packet[3]    = Telemetry page
-packet[4-13] = Telemetry data from device
+/* HoTT Telemetry 
 
-Telemetry devive IDs: 
+HoTT telemetry comprises of one and up to eight devices connected to the receiver's one-wire serial bus. The devices
+have unique ID's and each decive ID may be present on the bus only once. 
+
+HoTT telemetry knows to operating modes, Binary mode (normal operating mode) and Text mode (configuration mode). 
+Telemetry data is passed from the receiver via MPM to OpenTx in pages. The pages can be present or not, they 
+also do not have to follow each others.
+
+Telemetry device IDs: 
   0x00 RX, 0x09 Vario, 0x0A GPS, 0x0B not seen, 0x0C ESC, 0x0D GAM, 0x0E EAM, 0x80 Text config mode
-Telemetry page: 
-  0x00..0x04 for all telemetry except Text config mode which is 0x00..0x12 and rx which only has pahe 0x00  
-  The pages can be present or not, they also do not have to follow each others.
 
-Description of the RX telemetry page 0x00:
- [0] = TX RSSI value, downlink signal strength (rx --> tx as seen by tx)
-       CC2500 formated (a<128:a/2-71dBm, a>=128:(a-256)/2-71dBm)
- [1] = TX LQI value, downlink signal quality (rx --> tx as seen by tx)
+Binary mode:
+  Each page consists of a four byte header followed by ten bytes data plus a single byte warning code. 
+
+  Generic telemetry page layout in binary mode:
+    [0] = TX RSSI value, downlink signal strength (rx --> tx as seen by tx)
+          CC2500 formated (a<128:a/2-71dBm, a>=128:(a-256)/2-71dBm)
+    [1] = TX LQI value, downlink signal quality (rx --> tx as seen by tx)
+    [2] = ID of device that sent the packet, see telemetry devices ID above 
  [2] = ID of device that sent the packet, see telemetry devices ID above 
+    [2] = ID of device that sent the packet, see telemetry devices ID above 
+ [2] = ID of device that sent the packet, see telemetry devices ID above 
+    [2] = ID of device that sent the packet, see telemetry devices ID above 
+ [2] = ID of device that sent the packet, see telemetry devices ID above 
+    [2] = ID of device that sent the packet, see telemetry devices ID above 
+ [2] = ID of device that sent the packet, see telemetry devices ID above 
+    [2] = ID of device that sent the packet, see telemetry devices ID above 
+    [3] = telemetry page number. 
  [3] = telemetry page number. 
- [4] = ?? looks to be a copy of [7]
- [5] = RX_Voltage*10 in V
- [6] = Temperature-20 in °C
- [7] = Uplink signal strength (tx --> rx as seen by the rx): 
-       CC2500 formated (a<128:a/2-71dBm, a>=128:(a-256)/2-71dBm)
- [8] = Uplink signal quality (tx --> rx as seen by the tx) in percent
- [9] = RX_MIN_Voltage*10 in V
- [10,11] = [11]*256+[10]=max lost packet time in ms, max value is 2s=0x7D0
- [12] = RX Event (1 = low battery voltage, 2 low/high temp warning, other = general event)
- [13] = ?? looks to be a copy of [7]
- [14] = GAM, EAM, ESC, GPS, VARIO warnings
+    [3] = telemetry page number. 
+ [3] = telemetry page number. 
+    [3] = telemetry page number. 
+ [3] = telemetry page number. 
+    [3] = telemetry page number. 
+ [3] = telemetry page number. 
+    [3] = telemetry page number. 
+    [4]..[13] = 10 data bytes
+    [14] = RX, GAM, EAM, ESC, GPS, VARIO warnings
+
+  Telemetry pages in binary mode: 
+    0x00 for RX binary data
+    0x01..0x04 for GAM, EAM, ESC, GPS, VARIO binary data 
+
+  Every binary data frame sent by the RX has 10 data bytes.
+  Page layout of the RX telemetry page 0 in binary mode
+    [0] = TX RSSI value, downlink signal strength (rx --> tx as seen by tx)
+          CC2500 formated (a<128:a/2-71dBm, a>=128:(a-256)/2-71dBm)
+    [1] = TX LQI value, downlink signal quality (rx --> tx as seen by tx)
+    [2] = ID of device that sent the packet, see telemetry devices ID above 
+    [3] = telemetry page number. 
+    [4]  = ?? looks to be a copy of [7]
+    [5]  = RX_Voltage*10 in V
+    [6]  = Temperature-20 in °C
+    [7]  = Uplink signal strength (tx --> rx as seen by the rx): 
+           CC2500 formated (a<128:a/2-71dBm, a>=128:(a-256)/2-71dBm)
+    [8]  = Uplink signal quality (tx --> rx as seen by the tx) in percent
+    [9]  = RX_MIN_Voltage*10 in V
+    [10,11] = [11]*256+[10]=max lost packet time in ms, max value is 2s=0x7D0
+    [12] = RX Event (1 = low battery voltage, 2 low/high temp warning, other = general event)
+    [13] = ?? looks to be a copy of [7]
+    [14] = RX, GAM, EAM, ESC, GPS, VARIO warnings
+
+  Every binary data frame sent by GAM, EAM, ESC, GPS, VARIO has a length of 45 bytes with 40 bytes
+  of device specific data, see e.g. 
+  https://github.com/betaflight/betaflight/blob/1d8a0e9fd61cf01df7b34805e84365df72d9d68d/src/main/telemetry/hott.h#L240
+  The 40 data bytes are sent in 10 byte segments through pages 1 to 4. 
+  
+  Page layout for GAM, EAM, ESC, GPS, VARIO telemetry pages 1 to 4:
+   [0] = TX RSSI value, downlink signal strength (rx --> tx as seen by tx)
+         CC2500 formated (a<128:a/2-71dBm, a>=128:(a-256)/2-71dBm)
+   [1] = TX LQI value, downlink signal quality (rx --> tx as seen by tx)
+   [2] = ID of device that sent the packet, see telemetry devices ID above 
+   [3] = telemetry page number. 
+   [4]..[13] = data bytes 4-13, data bytes 14-23, data bytes 24, 33, data bytes 34-42
+   [14] = RX, GAM, EAM, ESC, GPS, VARIO warnings
+  
+Text Mode:
+  Telemetry pages in text mode:
+    0x00..0x12   
+
+  Every text mode data frame sent by GAM, EAM, ESC, GPS, VARIO has a length of 173 bytes with 168 bytes
+  of data representing 8 lines of 21 characters to display. The 168 bytes of data are sent through pages 
+  0 to 19. 
 
 RSSI:
 The packet[0] TX RSSI and packet[7] RX RSSI values describe estimates of the signal level in the
@@ -56,18 +110,18 @@ Usually TX RSSI even indicates stronger downlink signal strength compared to upl
 because the TX amplifies the signal higher due to the lower power of the downlink signal.
 
 LQI:
-LQI (link quality) values in packet[1] and packet[8] also take decodability, i.e. the possibility 
-to extract meaningful data in account and are because of this the best indicator to judge the quality of the link.
-relative numbers (0-100%). 
+LQI (link quality) values in packet[1] and packet[8] also take decodability, i.e. the effort
+to extract meaningful data in account and are because of this the best indicator to determine the quality of 
+the link. LQI values are presented asrelative numbers (0-100%). 
 
 OpenTX RSSI value:
 For Graupner HoTT OpenTX doesn't use the RX RSSI value but instead uses the more 
 meaningful RX_LQI value in packet[8]. Hence the default OpenTX RSSI warnig and critical thresholds can
-be adjusted to more appropriate values to indicate link problems, e.g. 30 for warning and 1 for critical.
+be adjusted to more appropriate values to indicate link problems.
 
 Failsafe trigger:
-Graupner receivers do not trigger failsafe based on RSSI and LQI. They do trigger
-failsafe based on the maximum time no decodable packet has been received. The default threshhold
+Graupner receivers do not trigger failsafe based on RSSI and LQI directly. They do trigger
+failsafe based on the maximum time no decodable data packet has been received. The default threshhold
 seems to be 1000ms
 */
 
@@ -256,14 +310,14 @@ const HottSensor * getHottSensor(uint16_t id)
   return nullptr;
 }
 
-// CC2500 data sheet:
-// The RSSI value read from the RSSI status register is a 2’s complement number. The following
-// procedure can be used to convert the RSSI reading to an absolute power level (RSSI_dBm).
-// 1) Read the RSSI status register
-// 2) Convert the reading from a hexadecimal number to a decimal number (RSSI_dec)
-// 3) If RSSI_dec ≥ 128 then RSSI_dBm = (RSSI_dec - 256)/2 – RSSI_offset
-// 4) Else if RSSI_dec < 128 then RSSI_dBm = (RSSI_dec)/2 – RSSI_offset
 int16_t processHoTTdBm(int16_t value)
+  // The RSSI value read from the RSSI status register is a 2’s complement number. The following
+  // procedure implements the CC2500 data sheet specification to convert the RSSI reading to an 
+  // absolute power level (RSSI_dBm).
+  // 1) Read the RSSI status register
+  // 2) Convert the reading from a hexadecimal number to a decimal number (RSSI_dec)
+  // 3) If RSSI_dec ≥ 128 then RSSI_dBm = (RSSI_dec - 256)/2 – RSSI_offset
+  // 4) Else if RSSI_dec < 128 then RSSI_dBm = (RSSI_dec)/2 – RSSI_offset
 {
   if (value >= 128) {
     value -= 256;
@@ -272,90 +326,93 @@ int16_t processHoTTdBm(int16_t value)
 }
 
 uint8_t processHoTTWarnings(const uint8_t * packet) {
-  // transfers rx events to warnings and transfers GAM, EAM, GPS, VARIO, ESC warnings
+  // Translates rx events to warnings and transfers GAM, EAM, GPS, VARIO, ESC warnings
   // 
   // Two types of warnings have to be considered:
-  //
-  // 1. Rx events - these are critical events and should be treated with priority
-  // 0      = no event
-  // 1      = rx low battery warning based on the threshold set in the rx configuration
-  // 2      = rx temp warning based on the threshold set in the rx configuration
-  // others = not sure, make it known as general event
+  //  1. Rx events - these are critical events and should be treated with priority
+  //  0      = no event
+  //  1      = rx low battery warning based on the threshold set in the rx configuration
+  //  2      = rx temp warning based on the threshold set in the rx configuration
+  //  others = not sure, make it known as general event
   //
   // 2. GAM, EAM, VARIO, GPS, EAM - these may be informational but also flight critical
-  // 0     = no warning
-  // other = warnings based on device specific setting either set by HoTT device configuration
-  //         or external software, e.g. third party devices like YGE ESC's or SM Unisens/GPS Logger
+  //  0     = no warning
+  //  other = warnings based on device specific setting either set by HoTT device configuration
+  //          or external software, e.g. third party devices like YGE ESC's or SM Unisens/GPS Logger
   // 
   // For a list of warnings see HoTT_warnings.txt
   //
   // Rx events are passed from MPM in page 0, packet[12]
-  // Other device warnings are passed in packet[14] for all devices
+  // Other device warnings are passed in packet[14]
   // 
-  // As only one warning can be transferred to the user device warnigs are prioritized 
+  // As only one warning can be transferred to the user the device warnigs are prioritized 
   // by the order RX, ESC, GAM, EAM, VARIO, GPS
   //
-  // The final result is passed to the user for further processing in the HOTT_ID_RX_EVENT telemetry sensor 
+  // The final result is passed to the user for further processing in the HOTT_ID_RX_EVENT telemetry sensor. 
   // Users may process warnings with a combination of logical switches and special funtions to announce the warning
   // or lua scripts
 
   #define DEVICE  (packet[2])
+  #define PAGE    (packet[3])
   #define RXTEMP  (packet[6] - 20)
   #define RXEVENT (packet[12])
   #define WARN    (packet[14])
 
-  enum HoTTWarnDevices {                            // lists devices in order of priority
-    HOTT_WARN_RX = 0,                               // if two or more devices issue warnigs in the same le
-    HOTT_WARN_ESC,                                  // polling cyc the device with the lower enum value wins
+  enum HoTTWarnDevices {                          // lists devices in order of priority
+    HOTT_WARN_RX = 0,                             // if two or more devices issue warnigs in the same le
+    HOTT_WARN_ESC,                                // polling cyc the device with the lower enum value wins
     HOTT_WARN_GAM,
     HOTT_WARN_EAM, 
     HOTT_WARN_VARIO,
     HOTT_WARN_GPS,
-    HOTT_WARN_LAST                                  // delimiter
+    HOTT_WARN_LAST                                // delimiter
   };
 
-  static uint8_t warnings[HOTT_WARN_LAST] = {};     // to memorize the warnings status of each HoTT device
-                                                    // until it gets updated again
- 
-  switch DEVICE {
-    case HOTT_TELEM_RX:                             // sending device is RX
-      switch (RXEVENT) {                            // fetch RX event parameter from RX telemetry data
-        case 0:                                     // rx doesn't indicate an event
-          warnings[HOTT_WARN_RX] = 0;               // translate to warning 0   
-          break;     	                        
-        
-        case 1:                                     // low rx battery event
-          warnings[HOTT_WARN_RX] = 64;              //translate to warning 64
+  static uint8_t warnings[HOTT_WARN_LAST] = {};   // to memorize the warnings status of each HoTT device
+                                                  // until it gets updated again
+
+  if ( PAGE == 0 && DEVICE ==  HOTT_TELEM_RX) {	  // sending device is RX
+    switch (RXEVENT) {                            // fetch RX event parameter from RX telemetry data
+      case 0:                                     // rx doesn't indicate an event
+        warnings[HOTT_WARN_RX] = 0;               // translate to warning 0 (no warning)   
+        break;     	                          	                        
+      
+      case 1:                                     // low rx battery event
+        warnings[HOTT_WARN_RX] = 64;              // translate to warning 64
+        break;
+
+      case 2:                                     // rx temperature event, find reason
+        warnings[HOTT_WARN_RX] =                  // check rx temp
+          RXTEMP >= 50 ? 44: 43;                  // > 50 degrees, high temperature warning, translate to warning 44      
+          break;                                  // < 50 degrees, low temperature warning, translate to warning 43 
+
+      default: 
+        warnings[HOTT_WARN_RX] = 53;              // other rx events -> translate to general receiver warning
+    }  
+  } else {
+    if (PAGE > 0 && PAGE <= 4) {                  // sending device is other than rx
+      switch (DEVICE) {
+        case HOTT_TELEM_ESC:                      // sending device is ESC
+          warnings[HOTT_WARN_ESC] = WARN;
           break;
-
-        case 2:                                     // rx temperature event, find reason
-          warnings[HOTT_WARN_RX] =                  // check rx temp
-            RXTEMP >= 50 ? 44: 43;                  // > 50 degrees, high temperature warning, translate to warning 44       
-            break;                                  // < 50 degrees, low temperature warning, translate to warning 43 
-
-        default: 
-          warnings[HOTT_WARN_RX] = 53;              // other rx events -> translate to general receiver warning
+        
+        case HOTT_TELEM_GAM:                      // sending device is GAM
+          warnings[HOTT_WARN_GAM] = WARN;
+          break;
+        
+        case HOTT_TELEM_EAM:                      // sending device is EAM
+          warnings[HOTT_WARN_EAM] = WARN;
+          break;
+        
+        case HOTT_TELEM_VARIO:                    // sending device is VARIO
+          warnings[HOTT_WARN_VARIO] = WARN;
+          break;
+        
+        case HOTT_TELEM_GPS:                      // sending device is GPS
+          warnings[HOTT_WARN_GPS] = WARN; 
+          break;
+      }
     }
-
-    case HOTT_TELEM_ESC:                            // sending device is ESC
-      warnings[HOTT_WARN_ESC] = WARN;
-      break;
-    
-    case HOTT_TELEM_GAM:                            // sending device is GAM
-      warnings[HOTT_WARN_GAM] = WARN;
-      break;
-    
-    case HOTT_TELEM_EAM:                            // sending device is EAM
-      warnings[HOTT_WARN_EAM] = WARN;
-      break;
-    
-    case HOTT_TELEM_VARIO:                          // sending device is VARIO
-      warnings[HOTT_WARN_VARIO] = WARN;
-      break;
-    
-    case HOTT_TELEM_GPS:                            // sending device is GPS
-      warnings[HOTT_WARN_GPS] = WARN; 
-      break;
   }
 
   for(uint8_t i = 0; i < HOTT_WARN_LAST; i++) {   // go through list of devices and see if warning was issued 
@@ -366,7 +423,7 @@ uint8_t processHoTTWarnings(const uint8_t * packet) {
       return  deviceWarn;                         // return warning
   }                                                
 
-  return 0;                                       // no warning found
+  return 0;                                       // return 0 (no warning)
 }
 
 void processHottPacket(const uint8_t * packet)
@@ -747,7 +804,7 @@ void processHottPacket(const uint8_t * packet)
           sensor = getHottSensor(HOTT_ID_GAM_RPM2);
           setTelemetryValue(PROTOCOL_TELEMETRY_HOTT, HOTT_ID_GAM_RPM2, 0, HOTT_TELEM_GAM, value, sensor->unit, sensor->precision);
          
-          // packet[11] uint8_t general_error_number;//#41 Voice error == 12. TODO: more docu
+          // packet[11] uint8_t general_error_number;//#41 Voice error == 12 (max. speed). TODO: more docu
           // packet[12] uint8_t pressure;            //#42 Pressure up to 16bar. 0,1bar scale. 20 = 2bar
           break;
       }
