9af01e46ffbb8b2a3aadd7394e55345dd32d6d5a
3djc <lesitewebdejc@hotmail.com>
2021-11-16 16:34:35 +0100

    Start ELRS

--- radio/src/targets/common/arm/stm32/adc_driver.cpp
+++ radio/src/targets/common/arm/stm32/adc_driver.cpp
@@ -47,7 +47,7 @@
 #elif defined(RADIO_TX12)
   const int8_t adcDirection[NUM_ANALOGS] = {-1,1,-1,1,  -1,-1,  1,  1};
 #elif defined(RADIO_ZORRO)
-  const int8_t adcDirection[NUM_ANALOGS] = {-1, 1, 1, -1, -1,-1, 1, 1};
+  const int8_t adcDirection[NUM_ANALOGS] = {-1, 1, 1, -1, -1, 1, 1, 1};
 #elif defined(RADIO_TANGO)
   const int8_t adcDirection[NUM_ANALOGS] = {1,1,1,1,  1,1};
 #elif defined(RADIO_MAMBO)
